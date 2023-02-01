5aa7b176cd06263147f421de4d0bbc10bdf8cb34
Abhishek <abhishek@cogoport.com>
2023-02-01 16:54:04 +0530

    docker compose

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from database.db_session import db
 from services.fcl_freight_rate.interaction.get_fcl_freight_rate import get_fcl_freight_rate
+from celery_worker import create_task
 
 app = FastAPI(debug=True)
 
@@ -28,6 +29,14 @@ def shutdown():
 @app.get("/")
 def read_root():
     return "WELCOME TO OCEAN RMS"
+
+@app.post("/ex1")
+def run_task(data={"amount": 150, "x": 6, "y": 9}):
+    amount = int(data["amount"])
+    x = data["x"]
+    y = data["y"]
+    task = create_task.delay(amount, x, y)
+    return JSONResponse(status_code=200, content={"success": True})
 
 @app.get("/get_fcl_freight_rate_data")
 def get_fcl_freight_rate_data():
