d05f620ea13dd835663c128c9452795da43cfcae
Abhishek <abhishek@cogoport.com>
2023-03-14 18:03:18 +0530

    remove files

--- src/celery_worker.py
+++ src/celery_worker.py
@@ -1,7 +1,6 @@
 import os
 import time
 from services.fcl_freight_rate.interaction.create_fcl_freight_rate import create_fcl_freight_rate_data
-from services.freight_trend.interactions.create_freight_trend_port_pair import create_freight_trend_port_pair
 from celery import Celery
 from configs.env import *
 CELERY_CONFIG = {
@@ -21,9 +20,5 @@ def create_task(param):
     from rails_client.client import initialize_client
     initialize_client()
     rate = create_fcl_freight_rate_data(param)
-
-
-@celery.task()
-def create_freight_trend_port_pair_celery(origin_port_id,destination_port_id):
-    create_freight_trend_port_pair(origin_port_id,destination_port_id)
+    
 
--- src/redis_lock.py
+++
@@ -1,6 +0,0 @@
-from redlock import Redlock
-
-
-redis_host ={'host': 'localhost', 'port': 6379, 'db': 0}
-
-redis_lock = Redlock([redis_host])
