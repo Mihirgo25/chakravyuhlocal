0ad1e227025b4762c419e6591a07ab264c34b81b
Sarvesh <sarvesh.kumar@cogoport.com>
2023-03-18 12:06:49 +0530

    add extra models

--- src/database/db_session.py
+++ src/database/db_session.py
@@ -9,3 +9,12 @@ db = PostgresqlDatabase(
     host = DATABASE_HOST,
     port = DATABASE_PORT
     )
+
+db_nirvana = PostgresqlDatabase(
+    'cogoport_api_nirvana1',
+    autorollback = True,
+    user = DATABASE_USER,
+    password = DATABASE_PASSWORD,
+    host = DATABASE_HOST,
+    port = DATABASE_PORT
+    )
