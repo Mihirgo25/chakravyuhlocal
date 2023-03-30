fe53481de2017da256d6ae3d07abe38122ff3e51
Abhishek <abhishek@cogoport.com>
2023-03-30 23:18:32 +0530

    sf

--- src/rms_utils/auth.py
+++ src/rms_utils/auth.py
@@ -12,7 +12,6 @@ def authorize_token(
     authorization_scope: str = Header(default=None, convert_underscores=False),
     authorization_parameters: str = Header(default=None, convert_underscores=False),
 ):
-    print(type(request.method))
     if APP_ENV == "development" or "is_authorization_required" in request.query_params or (request.method == "POST" and "is_authorization_required" in json.loads(request._body)):
         return {"status_code": 200, "isAuthorized": False}
 
