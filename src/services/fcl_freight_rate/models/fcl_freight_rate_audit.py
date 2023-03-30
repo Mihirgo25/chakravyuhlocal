eb25890e274b09ffc6d629d9dd01339b9b766b2e
Abhishek <abhishek@cogoport.com>
2023-03-30 23:41:35 +0530

    sf

--- src/services/fcl_freight_rate/models/fcl_freight_rate_audit.py
+++ src/services/fcl_freight_rate/models/fcl_freight_rate_audit.py
@@ -14,7 +14,7 @@ class FclFreightRateAudit(BaseModel):
     created_at = DateTimeField(default=datetime.datetime.now, index=True)
     data = BinaryJSONField(null=True)
     id = UUIDField(constraints=[SQL("DEFAULT gen_random_uuid()")], primary_key=True)
-    object_id = UUIDField(index=True)
+    object_id = UUIDField(index=True, null=True)
     object_type = CharField(null=True)
     performed_by_id = UUIDField(index=True)
     rate_sheet_id = UUIDField(index=True, null=True)
