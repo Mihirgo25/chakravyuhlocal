0fe4b893735aedafb1a6fb5a0e2e021a45bff0fb
Santram Gurjar <santram.gurjar@cogoport.com>
2023-04-05 11:20:02 +0530

    done

--- src/database/rails_db.py
+++ src/database/rails_db.py
@@ -5,195 +5,209 @@ conn = psycopg2.connect(
     host=RAILS_DATABASE_HOST,
     user=RAILS_DATABASE_USER,
     password=RAILS_DATABASE_PASSWORD,
-    port=RAILS_DATABASE_PORT
+    port=RAILS_DATABASE_PORT 
     )
 
 print("connection successful")
 
 
 def get_shipping_line(id=None, short_name=None):
-    cur = conn.cursor()
-    if short_name:
-        sql = 'select operators.id, operators.business_name, operators.short_name, operators.logo_url,operators.operator_type, operators.status from operators where operators.short_name = %s and operators.status = %s and operators.operator_type = %s'
-        cur.execute(sql, (short_name,'active','shipping_line',))
-    else:
-        if not isinstance(id, list):
-            id = (id,)
-        else:
-            id = tuple(id)
-        sql = 'select operators.id, operators.business_name, operators.short_name, operators.logo_url,operators.operator_type, operators.status from operators where operators.id in %s'
-        cur.execute(sql, (id,))
-
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        all_result.append(
-            {
-                "id": str(res[0]),
-                "business_name": res[1],
-                "short_name": res[2],
-                "logo_url": res[3],
-                "operator_type": res[4],
-                "status": res[5]
-            }
-        )
-    cur.close()
+    with conn:
+        with conn.cursor() as cur:
+            if short_name:
+                sql = 'select operators.id, operators.business_name, operators.short_name, operators.logo_url,operators.operator_type, operators.status from operators where operators.short_name = %s and operators.status = %s and operators.operator_type = %s'
+                cur.execute(sql, (short_name,'active','shipping_line',))
+            else:
+                if not isinstance(id, list):
+                    id = (id,)
+                else:
+                    id = tuple(id)
+                sql = 'select operators.id, operators.business_name, operators.short_name, operators.logo_url,operators.operator_type, operators.status from operators where operators.id in %s'
+                cur.execute(sql, (id,))
+
+            result = cur.fetchall()
+
+            for res in result:
+                all_result.append(
+                    {
+                        "id": str(res[0]),
+                        "business_name": res[1],
+                        "short_name": res[2],
+                        "logo_url": res[3],
+                        "operator_type": res[4],
+                        "status": res[5]
+                    }
+                )
+            cur.close()    
     return all_result
 
 def get_organization(id=None, short_name=None):
-    cur = conn.cursor()
-    if short_name:
-        sql = 'select organizations.id, organizations.business_name, organizations.short_name,organizations.category_types, organizations.account_type, organizations.kyc_status, organizations.status from organizations where organizations.short_name = %s and status = %s and account_type = %s'
-        cur.execute(sql, (short_name,'active','importer_exporter',))
-    else:
-        if not isinstance(id, list):
-            id = (id,)
-        else:
-            id = tuple(id)
-        sql = 'select organizations.id, organizations.business_name, organizations.short_name,organizations.category_types, organizations.account_type, organizations.kyc_status, organizations.status from organizations where organizations.id in %s'
-        cur.execute(sql, (id,))
-
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        all_result.append(
-            {
-                "id": str(res[0]),
-                "business_name": res[1],
-                "short_name": res[2],
-                "category_types":res[3],
-                "account_type":res[4],
-                "kyc_status" : res[5],
-                "status":res[6]
-            }
-        )
-    cur.close()
+    with conn:
+        with conn.cursor() as cur:
+            if short_name:
+                sql = 'select organizations.id, organizations.business_name, organizations.short_name,organizations.category_types, organizations.account_type, organizations.kyc_status, organizations.status from organizations where organizations.short_name = %s and status = %s and account_type = %s'
+                cur.execute(sql, (short_name,'active','importer_exporter',))
+            else:
+                if not isinstance(id, list):
+                    id = (id,)
+                else:
+                    id = tuple(id)
+                sql = 'select organizations.id, organizations.business_name, organizations.short_name,organizations.category_types, organizations.account_type, organizations.kyc_status, organizations.status from organizations where organizations.id in %s'
+                cur.execute(sql, (id,))
+
+            result = cur.fetchall()
+            
+            for res in result:
+                all_result.append(
+                    {
+                        "id": str(res[0]),
+                        "business_name": res[1],
+                        "short_name": res[2],
+                        "category_types":res[3],
+                        "account_type":res[4],
+                        "kyc_status" : res[5],
+                        "status":res[6]
+                    }
+                )
+            cur.close()
     return all_result
 
 def get_user(id):
-    cur = conn.cursor()
-
-    if not isinstance(id, list):
-        id = (id,)
-    else:
-        id = tuple(id)
-    sql = 'select users.id, users.name, users.email, users.mobile_number_eformat from users where users.id in %s'
-    cur.execute(sql, (id,))
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        all_result.append(
-            {
-                "id": str(res[0]),
-                "name": res[1],
-                "email": res[2],
-                "mobile_number_eformat":res[3]
-            }
-        )
-    cur.close()
+    with conn:
+        with conn.cursor() as cur:
+            if not isinstance(id, list):
+                id = (id,)
+            else:
+                id = tuple(id)
+            sql = 'select users.id, users.name, users.email, users.mobile_number_eformat from users where users.id in %s'
+            cur.execute(sql, (id,))
+            result = cur.fetchall()
+            for res in result:
+                all_result.append(
+                    {
+                        "id": str(res[0]),
+                        "name": res[1],
+                        "email": res[2],
+                        "mobile_number_eformat":res[3]
+                    }
+                )
+            cur.close()
     return all_result
 
 def get_eligible_orgs(service):
-    cur = conn.cursor()
-    sql = 'select organization_services.organization_id from organization_services where status = %s and service = %s'
-    cur.execute(sql, ('active', service,))
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        all_result.append(str(res[0]))
-    cur.close()
+    with conn:
+        with conn.cursor() as cur:
+            cur = conn.cursor()
+            sql = 'select organization_services.organization_id from organization_services where status = %s and service = %s'
+            cur.execute(sql, ('active', service,))
+            result = cur.fetchall()
+            for res in result:
+                all_result.append(str(res[0]))
+            cur.close()
     return all_result
 
 def get_partner_user_experties(service, partner_user_id):
-    cur = conn.cursor()
-    sql = 'select partner_user_expertises.origin_location_id, partner_user_expertises.destination_location_id, partner_user_expertises.location_id, partner_user.trade_type from partner_user_expertises where status = %s and service = %s and partner_user_id = %s'
-    cur.execute(sql, ('active', service, partner_user_id,))
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        new_obj = {
-            "origin_location_id": str(res[0]),
-            "destination_location_id": str(res[1]),
-            "location_id": str(res[2]),
-            "trade_type": str(res[3]),
-        }
-        all_result.append(new_obj)
-    cur.close()
+    with conn:
+        with conn.cursor() as cur:
+            sql = 'select partner_user_expertises.origin_location_id, partner_user_expertises.destination_location_id, partner_user_expertises.location_id, partner_user.trade_type from partner_user_expertises where status = %s and service = %s and partner_user_id = %s'
+            cur.execute(sql, ('active', service, partner_user_id,))
+            result = cur.fetchall()
+            
+            for res in result:
+                new_obj = {
+                    "origin_location_id": str(res[0]),
+                    "destination_location_id": str(res[1]),
+                    "location_id": str(res[2]),
+                    "trade_type": str(res[3]),
+                }
+                all_result.append(new_obj)
+            cur.close()
     return all_result
 
 def get_partner_users_by_expertise(service, origin_location_ids = None, destination_location_ids = None, location_ids = None, trade_type = None):
-    cur = conn.cursor()
-    if origin_location_ids and destination_location_ids: 
-        origin_location_ids = tuple(origin_location_ids)   
-        destination_location_ids = tuple(destination_location_ids)
-        sql = 'select partner_user_expertises.partner_user_id, partner_user_expertises.origin_location_id, partner_user_expertises.destination_location_id, partner_user_expertises.location_id, partner_user_expertises.trade_type from partner_user_expertises where status = %s and service_type = %s and origin_location_id IN %s and destination_location_id IN %s'
-        cur.execute(sql, ('active', service, origin_location_ids, destination_location_ids,))
-    else:
-        location_ids = tuple(location_ids)
-        sql = 'select partner_user_expertises.partner_user_id, partner_user_expertises.origin_location_id, partner_user_expertises.destination_location_id, partner_user_expertises.location_id, partner_user_expertises.trade_type from partner_user_expertises where status = %s and service_type = %s and partner_user_id = %s and location_id IN %s and trade_type = %s'
-        cur.execute(sql, ('active', service, location_ids, trade_type,))
-    
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        new_obj = {
-            "partner_user_id": str(res[0]),
-            "origin_location_id": str(res[1]),
-            "destination_location_id": str(res[2]),
-            "location_id": str(res[3]),
-            "trade_type": str(res[4]),
-        }
-        all_result.append(new_obj)
-    cur.close()
-    return all_result
+    with conn:
+        with conn.cursor() as cur:
+            if origin_location_ids and destination_location_ids: 
+                origin_location_ids = tuple(origin_location_ids)   
+                destination_location_ids = tuple(destination_location_ids)
+                sql = 'select partner_user_expertises.partner_user_id, partner_user_expertises.origin_location_id, partner_user_expertises.destination_location_id, partner_user_expertises.location_id, partner_user_expertises.trade_type from partner_user_expertises where status = %s and service_type = %s and origin_location_id IN %s and destination_location_id IN %s'
+                cur.execute(sql, ('active', service, origin_location_ids, destination_location_ids,))
+            else:
+                location_ids = tuple(location_ids)
+                sql = 'select partner_user_expertises.partner_user_id, partner_user_expertises.origin_location_id, partner_user_expertises.destination_location_id, partner_user_expertises.location_id, partner_user_expertises.trade_type from partner_user_expertises where status = %s and service_type = %s and partner_user_id = %s and location_id IN %s and trade_type = %s'
+                cur.execute(sql, ('active', service, location_ids, trade_type,))
+
+            result = cur.fetchall()
+            for res in result:
+                new_obj = {
+                    "partner_user_id": str(res[0]),
+                    "origin_location_id": str(res[1]),
+                    "destination_location_id": str(res[2]),
+                    "location_id": str(res[3]),
+                    "trade_type": str(res[4]),
+                }
+                all_result.append(new_obj)
+            cur.close()
+            return all_result
 
 def get_organization_stakeholders(stakeholder_type, stakeholder_id):
-    cur = conn.cursor()
-    sql = 'select organization_stakeholders.organization_id from organization_stakeholders where status = %s and stakeholder_type = %s and stakeholder_id = %s'
-    cur.execute(sql, ('active', stakeholder_type, stakeholder_id,))
-    result = cur.fetchall()
     org_ids = []
-    for res in result:
-        org_ids.append(str(res[0]))
-    cur.close()
+    with conn:
+        with conn.cursor() as cur:
+            sql = 'select organization_stakeholders.organization_id from organization_stakeholders where status = %s and stakeholder_type = %s and stakeholder_id = %s'
+            cur.execute(sql, ('active', stakeholder_type, stakeholder_id,))
+            result = cur.fetchall()
+            
+            for res in result:
+                org_ids.append(str(res[0]))
+            cur.close()
     return org_ids
 
 def get_partner_users(ids, status = 'active'):
     if not ids:
         return []
-    cur = conn.cursor()
-    ids = tuple(ids)
-    sql = 'select partner_users.user_id, partner_users.id from partner_users where status = %s and id IN %s'
-    cur.execute(sql, ( status, ids,))
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        new_obj = {
-            "user_id": str(res[0]),
-            "id": str(res[1]),
-        }
-        all_result.append(new_obj)
-    cur.close()
+    with conn:
+        with conn.cursor() as cur:
+            ids = tuple(ids)
+            sql = 'select partner_users.user_id, partner_users.id from partner_users where status = %s and id IN %s'
+            cur.execute(sql, ( status, ids,))
+            result = cur.fetchall()
+            for res in result:
+                new_obj = {
+                    "user_id": str(res[0]),
+                    "id": str(res[1]),
+                }
+                all_result.append(new_obj)
+            cur.close()
     return all_result
 
 def get_organization_service_experties(service, supply_agent_id):
-    cur = conn.cursor()
-    org_ids = get_organization_stakeholders('supply_agent', supply_agent_id)
-    if len(org_ids) == 0:
-        return []
-    org_ids_tuple = tuple(org_ids)
-    sql = 'select organization_service_expertises.origin_location_id, organization_service_expertises.destination_location_id, organization_service_expertises.location_id, organization_service_expertises.trade_type, organization_service_expertises.organization_id from organization_service_expertises where status = %s and service = %s and organization_id IN %s'
-    cur.execute(sql, ('active', service, org_ids_tuple,))
-    result = cur.fetchall()
     all_result = []
-    for res in result:
-        new_obj = {
-            "origin_location_id": str(res[0]),
-            "destination_location_id": str(res[1]),
-            "location_id": str(res[2]),
-            "trade_type": str(res[3]),
-            "organization_id": str(res[4])
-        }
-        all_result.append(new_obj)
-    cur.close()
-    return all_result
+    with conn:
+        with conn.cursor() as cur:
+            org_ids = get_organization_stakeholders('supply_agent', supply_agent_id)
+            if len(org_ids) == 0:
+                return []
+            org_ids_tuple = tuple(org_ids)
+            sql = 'select organization_service_expertises.origin_location_id, organization_service_expertises.destination_location_id, organization_service_expertises.location_id, organization_service_expertises.trade_type, organization_service_expertises.organization_id from organization_service_expertises where status = %s and service = %s and organization_id IN %s'
+            cur.execute(sql, ('active', service, org_ids_tuple,))
+            result = cur.fetchall()
+        
+            for res in result:
+                new_obj = {
+                    "origin_location_id": str(res[0]),
+                    "destination_location_id": str(res[1]),
+                    "location_id": str(res[2]),
+                    "trade_type": str(res[3]),
+                    "organization_id": str(res[4])
+                }
+                all_result.append(new_obj)
+            cur.close()
+            return all_result
