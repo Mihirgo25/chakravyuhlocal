df92c9551dd34f13f7ad150b5218d69d2f75e772
Santram Gurjar <santram.gurjar@cogoport.com>
2023-04-07 20:36:22 +0530

    id

--- src/services/fcl_freight_rate/interaction/get_fcl_freight_rate_cards.py
+++ src/services/fcl_freight_rate/interaction/get_fcl_freight_rate_cards.py
@@ -329,12 +329,14 @@ def build_local_line_item_object(line_item, request):
 
 def add_local_objects(freight_query_result, response_object, request):
     response_object['origin_local'] = {
+        'id': freight_query_result['origin_local'].get('id'),
         'service_provider_id': freight_query_result['origin_local']['service_provider_id'] if freight_query_result['origin_local'].get('service_provider_id') else response_object['service_provider_id'],
         'source': freight_query_result['origin_local']['source'] if freight_query_result['origin_local'].get('source') else response_object['source'],
         'line_items': []
     } if 'origin_local' in freight_query_result and freight_query_result['origin_local'] else { 'line_items': [], 'service_provider_id': response_object['service_provider_id'], 'source':  response_object['source'] }
     response_object['destination_local'] = {}
     if freight_query_result.get('destination_local'):
+        response_object['destination_local']['id'] =  freight_query_result['destination_local'].get('id'),
         if freight_query_result['destination_local'].get('service_provider_id'):
             response_object['destination_local']['service_provider_id'] = freight_query_result['destination_local']['service_provider_id']
         else:
