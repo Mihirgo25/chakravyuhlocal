df9c79a36fc69e5184ef9bb5aa33044b57e081bb
Abhishek <abhishek@cogoport.com>
2023-04-07 16:54:55 +0530

    migration

--- src/clean_redis.py
+++ src/clean_redis.py
@@ -1,4 +1,7 @@
 from database.db_session import rd
+from services.fcl_freight_rate.models.fcl_freight_rate import FclFreightRate
+from celery_worker import update_multiple_service_objects
+from joblib import parallel, delayed
 
 def clean_full_redis():
     redis_keys = rd.keys('*celery-task-meta*')
@@ -15,3 +18,20 @@ def clean_full_redis():
             print('Deleted ->', total_non_celery)
         else:
             print('No Delete performed')
+            
+def fcl_freight_objects_updation():
+    rates_to_update = FclFreightRate.select().where(
+        FclFreightRate.shipping_line.is_null(True) |
+        FclFreightRate.service_provider.is_null(True) |
+        FclFreightRate.sourced_by.is_null(True) |
+        FclFreightRate.procured_by.is_null(True)
+    )
+    
+    result = parallel(n_jobs=4)(delayed(delay_func)(object) for object in rates_to_update.iterator())
+    
+    
+def delay_func(object):
+    from celery_worker import update_multiple_service_objects
+    update_multiple_service_objects.apply_async(kwargs={"object":object},queue='low')
+    
+    
