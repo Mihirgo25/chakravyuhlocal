473a9f15f25f0fa9cc48bf81920d419883bb83bc
Santram Gurjar <santram.gurjar@cogoport.com>
2023-04-12 03:57:33 +0530

    reverse key

--- src/services/envision/interaction/get_fcl_freight_predicted_rate.py
+++ src/services/envision/interaction/get_fcl_freight_predicted_rate.py
@@ -25,7 +25,6 @@ def get_final_price(min_price, price, ldh, request):
         modified_container_size = '20'
 
     key = '{}:{}:{}'.format(request['origin_country_id'], request['destination_country_id'], modified_container_size)
-    reverse_key = '{}:{}:{}'.format(request['destination_country_id'], request['origin_country_id'], modified_container_size)
     trade_key = '{}:{}:{}'.format(origin_trade_id, destination_trade_id, modified_container_size)
     reverse_trade_key = '{}:{}:{}'.format(destination_trade_id, origin_trade_id, modified_container_size)
 
