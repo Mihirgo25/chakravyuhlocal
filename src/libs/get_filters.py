856e3eed139c091d454338a580242ea1dd7f4b9e
Saksham573001 <saksham.sharma@cogoport.com>
2023-04-13 17:32:43 +0530

    get filter

--- src/libs/get_filters.py
+++ src/libs/get_filters.py
@@ -7,7 +7,9 @@ def get_filters(filters: dict, query, model):
     for filter_key in filter_keys:
         filter_value = filters[filter_key]
 
-        if isinstance(filter_value, str):
+        if isinstance(filter_value, int):
+            query = query.where(attrgetter(filter_key)(model) == filter_value)
+        elif isinstance(filter_value, str):
             if filter_value != "":
                 query = query.where(attrgetter(filter_key)(model) == filter_value)
         elif isinstance(filter_value, bool):
