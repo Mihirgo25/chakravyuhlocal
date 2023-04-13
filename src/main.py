6beea44dddb37508b3a4fafe7ae149cfb37d88cf
Abhishek <abhishek@cogoport.com>
2023-04-13 20:35:19 +0530

    sentry fix

--- src/main.py
+++ src/main.py
@@ -13,15 +13,13 @@ from libs.migration import fcl_freight_migration, create_partition_table, fcl_lo
 # from migrate import insert
 from services.fcl_freight_rate.fcl_freight_router import fcl_freight_router
 from micro_services.client import *
-
-if APP_ENV == "production":
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        environment="production",
-        traces_sample_rate=0.5,
-        attach_stacktrace=True,
-        ignore_errors=[HTTPException]
-    )
+sentry_sdk.init(
+    dsn=SENTRY_DSN if APP_ENV == "production" else None,
+    environment="production",
+    traces_sample_rate=0.5,
+    attach_stacktrace=True,
+    ignore_errors=[HTTPException]
+)
 
 docs_url = None if APP_ENV == "production" else "/docs"
 
--- src/services/rate_sheet/interactions/send_rate_sheet_notification.py
+++ src/services/rate_sheet/interactions/send_rate_sheet_notification.py
@@ -4,7 +4,6 @@ from playhouse.postgres_ext import *
 from peewee import *
 from micro_services.client import *
 from services.rate_sheet.models.rate_sheet_audits import RateSheetAudit
-from celery_worker import create_communication_background
 from configs.global_constants import PROD_DATA_OPERATIONS_ASSOCIATE_ROLE_ID
 
 def get_relevant_user_ids(params):
