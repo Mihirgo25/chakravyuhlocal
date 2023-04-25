51ab57e062414ac1e8dea0de54879652a3539089
Abhishek <abhishek@cogoport.com>
2023-04-25 12:13:25 +0530

    delete maersk predicted rates

--- src/clean_redis.py
+++ src/clean_redis.py
@@ -1,10 +1,13 @@
 from database.db_session import rd
-from to_d import a
+# from to_d import a
 from datetime import datetime
 from services.fcl_freight_rate.models.fcl_freight_rate import FclFreightRate
 from services.fcl_freight_rate.helpers.get_multiple_service_objects import get_multiple_service_objects
 # from celery_worker import delete_fcl_freight_rates_delay
 from services.fcl_freight_rate.interaction.delete_fcl_freight_rate import delete_fcl_freight_rate
+from configs.definitions import ROOT_DIR
+import os
+import pandas as pd
 
 def clean_full_redis():
     redis_keys = rd.keys('*celery-task-meta*')
@@ -40,15 +43,13 @@ def delay_func(object):
 
 def delete_rates():
     delete = False
+    file_path = os.path.join(ROOT_DIR, 'maersk_ids.csv')
+    a = pd.read_csv(file_path)['id'].to_list()
     for rate in a:
-        if rate != 'c997befa-1397-40d8-9d9e-1bbb056f7a9c' and not delete:
-            continue
-        if rate == 'c997befa-1397-40d8-9d9e-1bbb056f7a9c':
-            delete = True
         obj = {
-            "id": rate,
-            "validity_start": datetime.fromisoformat("2023-04-13"),
-            "validity_end": datetime.fromisoformat("2023-04-30"),
+            "id": str(rate),
+            "validity_start": datetime.fromisoformat("2023-04-25"),
+            "validity_end": datetime.fromisoformat("2023-05-01"),
             "performed_by_id": "15cd96ec-70e7-48f4-a4f9-57859c340ee7",
             "sourced_by_id": "15cd96ec-70e7-48f4-a4f9-57859c340ee7",
             "procured_by_id": "15cd96ec-70e7-48f4-a4f9-57859c340ee7",
