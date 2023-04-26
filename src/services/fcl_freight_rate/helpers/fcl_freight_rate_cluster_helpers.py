0ee9ada4f12593f9305620b487b9a5f9232567fd
Abhishek <abhishek@cogoport.com>
2023-04-26 11:15:43 +0530

    few changes

--- src/services/fcl_freight_rate/helpers/fcl_freight_rate_cluster_helpers.py
+++ src/services/fcl_freight_rate/helpers/fcl_freight_rate_cluster_helpers.py
@@ -147,7 +147,7 @@ def add_mandatory_line_items(param,request):
     return param
 
 def check_rate_existence(updated_param):
-    system_rate = FclFreightRate.select().where(FclFreightRate.origin_port_id == updated_param['origin_port_id'], FclFreightRate.destination_port_id == updated_param['destination_port_id'], FclFreightRate.container_size == updated_param['container_size'], FclFreightRate.commodity == updated_param['commodity'], FclFreightRate.container_type == updated_param['container_type'], FclFreightRate.service_provider_id == updated_param['service_provider_id'], FclFreightRate.shipping_line_id == updated_param['shipping_line_id'], FclFreightRate.last_rate_available_date > updated_param['validity_end']).execute()
+    system_rate = FclFreightRate.select().where(FclFreightRate.origin_port_id == updated_param['origin_port_id'], FclFreightRate.origin_main_port_id == updated_param.get('origin_main_port_id'), FclFreightRate.destination_port_id == updated_param['destination_port_id'], FclFreightRate.destination_main_port_id == updated_param.get('destination_main_port_id'), FclFreightRate.container_size == updated_param['container_size'], FclFreightRate.commodity == updated_param['commodity'], FclFreightRate.container_type == updated_param['container_type'], FclFreightRate.service_provider_id == updated_param['service_provider_id'], FclFreightRate.shipping_line_id == updated_param['shipping_line_id'], FclFreightRate.last_rate_available_date > updated_param['validity_end']).execute()
     if system_rate:
         return True
     else:
