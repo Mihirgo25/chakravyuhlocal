c098d1e3d9d5e1df2c95f0808d24326ed4e56a10
Hemanthkumar-cogoport <Hemanth.Boddikura@cogoport.com>
2023-04-26 15:54:19 +0530

    migration_locals

--- src/services/chakravyuh/migrated_estimated_local_rates.py
+++ src/services/chakravyuh/migrated_estimated_local_rates.py
@@ -16,7 +16,7 @@ def migrate_estimated_local_rates():
     for param in search_wise_params:
         dict_list = eval('add_'+param+'(estimated_rates)')
         final_create_params.extend(dict_list)
-    with concurrent.futures.ThreadPoolExecutor(max_workers = len(final_create_params)) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers = 4) as executor:
         futures = [executor.submit(insert_into_table, row) for row in final_create_params]
 
 def add_continent(estimated_rates):
@@ -28,6 +28,8 @@ def add_continent(estimated_rates):
         row_dict['location_id'] = row['Continent']
         del row_dict['Continent']
         row_dict['location_type'] = 'continent'
+        row_dict['shipping_line_id'] = row['shipping_line']
+        del row_dict['shipping_line']
         row_dict['line_items'] = eval(row_dict['line_items'])
         dict_list.append(row_dict)
     
@@ -44,6 +46,8 @@ def add_country(estimated_rates):
         row_dict['location_type'] = 'country'
         row_dict['line_items'] = eval(row_dict['line_items'])
         del row_dict['Country']
+        row_dict['shipping_line_id'] = row['shipping_line']
+        del row_dict['shipping_line']
         dict_list.append(row_dict)
 
     return dict_list
@@ -59,10 +63,16 @@ def add_trade(estimated_rates):
         row_dict['location_type'] = 'trade'
         row_dict['line_items'] = eval(row_dict['line_items'])
         del row_dict['Trade']
+        row_dict['shipping_line_id'] = row['shipping_line']
+        del row_dict['shipping_line']
         dict_list.append(row_dict)
     
     return dict_list
 
 def insert_into_table(row):
+    print(13)
     fcl_local_estimation = FclFreightRateLocalEstimation(**row)
-    fcl_local_estimation.save()
+    try:
+        fcl_local_estimation.save()
+    except Exception as e:
+        print((e))
