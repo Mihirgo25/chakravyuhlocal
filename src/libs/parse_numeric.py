b33ae9e7335884186870974477181bdf4f21db33
Saksham573001 <saksham.sharma@cogoport.com>
2023-05-01 13:37:09 +0530

    name fix

--- src/libs/parse_numeric.py
+++ src/libs/parse_numeric.py
@@ -1,13 +1,11 @@
-from string import digits
-
-def parse_numeric(string):
-    if not string:
+def parse_numeric(numeric):
+    if not numeric:
         return None
-    if isinstance(string, float) or isinstance(string, int):
-        return float(string)
-    string = string.strip()
+    if isinstance(numeric, float) or isinstance(numeric, int):
+        return float(numeric)
+    numeric = numeric.strip()
     try:
-        parsed_value = float(string)
+        parsed_value = float(numeric)
     except:
         parsed_value = None
     return parsed_value
