7da119c86ab53619a02f45922e3eb521591cfb84
Shiv <109941139+ShivCogoport@users.noreply.github.com>
2023-05-05 16:59:52 +0530

    change

--- src/services/envision/interaction/create_ftl_freight_rate_prediction_feedback.py
+++ src/services/envision/interaction/create_ftl_freight_rate_prediction_feedback.py
@@ -6,23 +6,22 @@ def create_ftl_freight_rate_feedback(result):
     with db.atomic() as transaction:
         try:
             for feedback in result:
-                    
-                        row = {
-                            'origin_location_id' : feedback.get('origin_location_id'),
-                            'destination_location_id' : feedback.get('destination_location_id'),
-                            'origin_region_id' : feedback.get('origin_region_id'),
-                            'destination_region_id' : feedback.get('destination_region_id'),
-                            'truck_type' : feedback.get('truck_type'),
-                            'validity_start' : feedback.get('validity_start'),
-                            'validity_end' : feedback.get('validity_end'),
-                            'predicted_price_currency' : 'INR',
-                            'predicted_price' : feedback.get('predicted_price') if 'predicted_price' in feedback else None,
-                            'actual_price' : feedback.get('actual_price') if 'actual_price' in feedback else None,
-                            'actual_price_currency' : feedback.get('actual_price_currency') if 'actual_price_currency' in feedback else None
-                        }
+                row = {
+                    'origin_location_id' : feedback.get('origin_location_id'),
+                    'destination_location_id' : feedback.get('destination_location_id'),
+                    'origin_region_id' : feedback.get('origin_region_id'),
+                    'destination_region_id' : feedback.get('destination_region_id'),
+                    'truck_type' : feedback.get('truck_type'),
+                    'validity_start' : feedback.get('validity_start'),
+                    'validity_end' : feedback.get('validity_end'),
+                    'predicted_price_currency' : 'INR',
+                    'predicted_price' : feedback.get('predicted_price') if 'predicted_price' in feedback else None,
+                    'actual_price' : feedback.get('actual_price') if 'actual_price' in feedback else None,
+                    'actual_price_currency' : feedback.get('actual_price_currency') if 'actual_price_currency' in feedback else None
+                }
 
-                        new_rate = FtlRatePredictionFeedback.create(**row)
-                        feedback['id'] = new_rate.id
+                new_rate = FtlRatePredictionFeedback.create(**row)
+                feedback['id'] = new_rate.id
 
             return result
         except Exception as e:
