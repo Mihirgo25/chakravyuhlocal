0144462d23686e099e8d0fdbe15f444c323586b8
Sarvesh <sarvesh.kumar@cogoport.com>
2023-05-06 15:54:01 +0530

    draft table

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -5,11 +5,13 @@ from services.chakravyuh.models.demand_transformation import DemandTransformatio
 from services.envision.models.haulage_rate_prediction_feedback import HaulageRatePredictionFeedback
 from services.envision.models.ftl_rate_prediction_feedback import FtlRatePredictionFeedback
 from services.envision.models.air_rate_prediction_feedback import AirFreightRatePredictionFeedback
+from services.nandi.models.draft_fcl_freight_rate import DraftFclFreightRate
+from services.nandi.models.draft_fcl_freight_rate_local import DraftFclFreightRateLocal
 
 def create_table():
     # db.connect()
     try:
-        db.create_tables([FclFreightRateEstimation, FclFreightRateEstimationAudit, DemandTransformation])
+        db.create_tables([DraftFclFreightRateLocal, DraftFclFreightRate])
         db.close()
         print("created table")
     except:
