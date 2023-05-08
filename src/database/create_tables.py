ef15a2f121e38d363fa8b39cf6b608223c72cd93
Santram Gurjar <santram.gurjar@cogoport.com>
2023-05-08 11:35:31 +0530

    table create

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -2,11 +2,15 @@ from database.db_session import db
 from services.chakravyuh.models.fcl_freight_rate_estimation import FclFreightRateEstimation
 from services.chakravyuh.models.fcl_freight_rate_estimation_audit import FclFreightRateEstimationAudit
 from services.chakravyuh.models.demand_transformation import DemandTransformation
+from services.chakravyuh.models.demand_transformation_audit import DemandTransformationAudit
+from services.chakravyuh.models.revenue_target import RevenueTarget
+from services.chakravyuh.models.revenue_target_audit import RevenueTargetAudit
+from services.chakravyuh.models.fcl_freight_rate_local_estimation import FclFreightRateLocalEstimation
 
 def create_table():
     # db.connect()
     try:
-        db.create_tables([FclFreightRateEstimation, FclFreightRateEstimationAudit])
+        db.create_tables([FclFreightRateEstimation, FclFreightRateEstimationAudit, DemandTransformation, RevenueTarget, RevenueTargetAudit, FclFreightRateLocalEstimation, DemandTransformationAudit])
         db.close()
         print("created table")
     except:
