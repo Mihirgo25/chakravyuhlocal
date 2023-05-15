9a036827af1cfd3761d55eea9793b32cb82d8c88
Abhishek <abhishek@cogoport.com>
2023-05-15 14:52:01 +0530

    sf

--- src/services/fcl_freight_rate/fcl_freight_router.py
+++ src/services/fcl_freight_rate/fcl_freight_router.py
@@ -518,6 +518,7 @@ def get_fcl_freight_rate_cards_data(
         'ignore_omp_dmp_sl_sps' : ignore_omp_dmp_sl_sps,
         'cogo_entity_id' : cogo_entity_id
     }
+    
     try:
         data = get_fcl_freight_rate_cards(request)
         return JSONResponse(status_code=200, content=jsonable_encoder(data))
