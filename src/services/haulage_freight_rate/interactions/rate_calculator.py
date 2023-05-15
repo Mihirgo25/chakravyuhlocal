32a7b0299bfeddbd5c9efa0971b7839feaf745ed
Saksham573001 <saksham.sharma@cogoport.com>
2023-05-15 14:51:53 +0530

    feat : adding tables and basic logic algo for india

--- src/services/haulage_freight_rate/interactions/rate_calculator.py
+++ src/services/haulage_freight_rate/interactions/rate_calculator.py
@@ -1,7 +1,7 @@
-from services.rail_rate.models.rail_rates import RailRates
+from services.haulage_freight_rate.models.haulage_freight_rate_rule_sets import HaulageFreightRateRuleSet
 import geopy.distance
 from micro_services.client import maps
-import services.rail_rate.interactions.rate_calculator_domestic as rate_calculator_domestic
+import services.haulage_freight_rate.interactions.rate_calculator as rate_calculator
 from configs.rails_constants import DEFAULT_PERMISSIBLE_CARRYING_CAPACITY
 
 POSSIBLE_LOCATION_CATEGORY = ["india", "china", "europe", "north_america"]
@@ -26,23 +26,28 @@ def rate_calculator(
     origin_location,
     destination_location,
     commodity,
-    location_category,
     load_type=None,
     container_count=None,
 ):
     response = {"success": False, "status_code": 200}
 
+    location_category =''
+
+    #check valid location
     if location_category not in POSSIBLE_LOCATION_CATEGORY:
         return response
+
+    #check intermodal
+
 
     ports_distance = get_distance(origin_location, destination_location)
 
     if not container_count:
         load_type = "Wagon Load"
 
-    query = RailRates.select(RailRates.base_rate)
+    query = HaulageFreightRateRuleSet.select(HaulageFreightRateRuleSet.base_rate)
 
-    final_data = getattr(rate_calculator_domestic, "get_{}_rates".format(location_category))(
+    final_data = getattr(rate_calculator, "get_{}_rates".format(location_category))(
         query, commodity, load_type, container_count, ports_distance
     )
 
@@ -61,16 +66,16 @@ def get_india_rates(query, commodity, load_type, container_count, ports_distance
         full_rake_count = container_count / 50
         remaining_wagons_count = container_count % 50
         rake_price = query = query.where(
-            RailRates.distance <= ports_distance,
-            RailRates.distance >= ports_distance - 50,
-            RailRates.commodity_type == commodity,
-            RailRates.load_type == "Train Load",
+            HaulageFreightRateRuleSet.distance <= ports_distance,
+            HaulageFreightRateRuleSet.distance >= ports_distance - 50,
+            HaulageFreightRateRuleSet.commodity_type == commodity,
+            HaulageFreightRateRuleSet.load_type == "Train Load",
         )
         wagon_price = query = query.where(
-            RailRates.distance <= ports_distance,
-            RailRates.distance >= ports_distance - 50,
-            RailRates.commodity_type == commodity,
-            RailRates.load_type == "Wagon Load",
+            HaulageFreightRateRuleSet.distance <= ports_distance,
+            HaulageFreightRateRuleSet.distance >= ports_distance - 50,
+            HaulageFreightRateRuleSet.commodity_type == commodity,
+            HaulageFreightRateRuleSet.load_type == "Wagon Load",
         )
         rake_price_per_tonne = rake_price.dicts().get()
         wagon_price_per_tonne = wagon_price.dicts().get()
@@ -80,13 +85,17 @@ def get_india_rates(query, commodity, load_type, container_count, ports_distance
 
     else:
         query = query.where(
-            RailRates.distance <= ports_distance,
-            RailRates.distance >= ports_distance - 50,
-            RailRates.commodity_type == commodity,
-            RailRates.load_type == load_type,
+            HaulageFreightRateRuleSet.distance <= ports_distance,
+            HaulageFreightRateRuleSet.distance >= ports_distance - 50,
+            HaulageFreightRateRuleSet.commodity_type == commodity,
+            HaulageFreightRateRuleSet.load_type == load_type,
         )
 
         price_per_tonne = query.dicts().get()
         price = price_per_tonne * container_count * DEFAULT_PERMISSIBLE_CARRYING_CAPACITY
         final_data["base_price"] = price
     return final_data
+
+
+def get_china_rates(query, commodity, load_type, container_count, ports_distance):
+    return
--- src/services/haulage_freight_rate/models/haulage_freight_rate_rule_sets.py
+++ src/services/haulage_freight_rate/models/haulage_freight_rate_rule_sets.py
@@ -2,30 +2,36 @@ from peewee import *
 from database.db_session import db
 import uuid
 import datetime
+from playhouse.postgres_ext import *
+
 
 
 class BaseModel(Model):
     class Meta:
         database = db
-class RailRates(BaseModel):
+class HaulageFreightRateRuleSet(BaseModel):
     id = UUIDField(
         primary_key=True,
         constraints=[SQL("DEFAULT uuid_generate_v4()")],
         default=uuid.uuid4,
     )
     distance = FloatField(index=True)
-    load_type = CharField(index=True)
-    class_type = CharField(index=True)
+    wagon_count = IntegerField(index=True, null=True)
+    status = CharField(index=True, default='active')
+    train_load_type = CharField(index=True)
+    commodity_class_type = CharField(index=True)
     commodity_type = CharField(index=True, null=True)
+    wagon_type = CharField(index=True, null=True)
     base_rate = IntegerField()
     country_code = CharField(index=True)
     currency = CharField(index=True)
+    transit_time = IntegerField(null=True)
     created_at = DateTimeField(default=datetime.datetime.now, index=True)
     updated_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
-        table_name = 'rail_rates'
+        table_name = 'haulage_freight_rate_rule_sets'
 
     def save(self, *args, **kwargs):
       self.updated_at = datetime.datetime.now()
-      return super(RailRates, self).save(*args, **kwargs)
+      return super(HaulageFreightRateRuleSet, self).save(*args, **kwargs)
