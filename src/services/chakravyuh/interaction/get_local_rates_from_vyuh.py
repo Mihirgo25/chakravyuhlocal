95f82cfff94c65235706007b59de2cfd26eb16a8
shreyasnakshatram <shreyas.nakshatram@cogoport.com>
2023-05-20 13:15:47 +0530

    changes

--- src/services/chakravyuh/interaction/get_local_rates_from_vyuh.py
+++ src/services/chakravyuh/interaction/get_local_rates_from_vyuh.py
@@ -1,15 +1,11 @@
-from celery_worker import fcl_freight_rates_local_creation_in_delay
 from configs.global_constants import HAZ_CLASSES
 from configs.fcl_freight_rate_constants import DEFAULT_SERVICE_PROVIDER_ID, DEFAULT_SHIPPING_LINE_ID
-from rms_utils.get_in_batches import get_in_batches
 from configs.env import DEFAULT_USER_ID
 from services.fcl_freight_rate.models.fcl_freight_rate_local import FclFreightRateLocal
 from micro_services.client import maps
-from configs.definitions import ROOT_DIR
-import pandas as pd, os, copy
 
-def get_local_rates_from_vyuh(local_rate_param, line_items = []):
-    ports_data = get_locations(local_rate_param.get('country_id'))
+def add_local_rates_on_country(request):
+    ports_data = get_batch_wise(request.get('country_id') , BATCH_SIZE = 100)
     icd_ports, combined_ids = [], []
     for data in ports_data:
         if data.get('is_icd') == True:
@@ -26,73 +22,40 @@ def get_local_rates_from_vyuh(local_rate_param, line_items = []):
     for port in main_ports_data:
         combined_ids.append(str(port['icd_port_id'])+':'+str(port['id']))
 
-    query_result = get_search_query(local_rate_param)
+    query_result = get_search_query(request)
     mapping_from_query = [str(row['port_id'])+':'+str(row['main_port_id'] or '') for row in query_result]
     final_list = list(set(combined_ids).difference(set(mapping_from_query)))
-    
     if final_list:
-        local_rates = create_fcl_freight_local_in_delay(local_rate_param, line_items, final_list)
-    else:
-        local_rates = []
-    return local_rates
+        get_params_and_create_local(request, final_list)
 
-def get_locations(location_id):
-    locations = maps.list_locations({'filters':{'country_id': location_id, 'status':'active', "type":"seaport" },'page_limit':10000})
-    if isinstance(locations, dict) and locations.get('list'):
-        return locations['list']
-    else:
-        return []
 
-def create_fcl_freight_local_in_delay(param, line_items, final_list):
+def get_params_and_create_local(request, final_list):
+    from celery_worker import fcl_freight_rates_local_creation_in_delay
     final_params = []
 
-    if len(line_items) == 0:
-        line_items = get_line_items_from_sheet(param)
+    local_freight_param = {
+        'trade_type': request.get('trade_type'), 
+        'container_size': request.get('container_size'),
+        'container_type': request.get('container_type'),
+        'commodity': request.get('commodity') if request.get('commodity') in HAZ_CLASSES else None,
+        'service_provider_id': request['service_provider_id'] if request.get('service_provider_id') else DEFAULT_SERVICE_PROVIDER_ID,
+        'performed_by_id': request['performed_by_id'] if request.get('performed_by_id') else DEFAULT_USER_ID,
+        'procured_by_id': request['procured_by_id'] if request.get('procured_by_id') else DEFAULT_USER_ID,
+        'sourced_by_id': request['sourced_by_id'] if request.get('sourced_by_id') else DEFAULT_USER_ID,
+        'shipping_line_id' : request.get('shipping_line_id') if request.get('shipping_line_id') else DEFAULT_SHIPPING_LINE_ID
+    }
 
-    if len(line_items) > 0:
-        return_params = []
-        local_freight_param = {
-        'trade_type':param.get('trade_type'), 
-        'main_port_id':param.get('main_port_id'),
-        'container_size': param.get('container_size'),
-        'container_type': param.get('container_type'),
-        'commodity': param.get('commodity') if param.get('commodity') in HAZ_CLASSES else None,
-        'service_provider_id': DEFAULT_SERVICE_PROVIDER_ID,
-        'performed_by_id': DEFAULT_USER_ID,
-        'procured_by_id': DEFAULT_USER_ID,
-        'sourced_by_id': DEFAULT_USER_ID,
-        'source':'predicted',
-        'shipping_line_id' : param.get('shipping_line_id') if param.get('shipping_line_id') else DEFAULT_SHIPPING_LINE_ID,
-        }
+    for port in final_list:
+        port_id, main_port_id = port.split(':')
+        creation_param = local_freight_param | {'port_id': port_id, 'source':'predicted','data':request.get('data')}
+        if main_port_id:
+            creation_param['main_port_id'] = main_port_id
+        else:
+            creation_param['main_port_id'] = None
+      
+        final_params.append(creation_param)
+    fcl_freight_rates_local_creation_in_delay.apply_async(kwargs={'local_rate_params':final_params}, queue = 'low')
 
-        for port in final_list:
-            port_id, main_port_id = port.split(':')
-            creation_param = local_freight_param | {'port_id': port_id, 'source':'predicted','data':{'line_items':line_items, 'plugin':None, 'detention':None, 'demurrage':None}}
-            if main_port_id:
-                creation_param['main_port_id'] = main_port_id
-            else:
-                creation_param['main_port_id'] = None
-            if port_id == param.get('port_id'):
-                return_params.append(creation_param)
-            final_params.append(creation_param)
-        fcl_freight_rates_local_creation_in_delay.apply_async(kwargs={'local_rate_params':final_params}, queue = 'low')
-        return return_params
-    else:
-        return []
-
-def get_line_items_from_sheet(param):
-    FILE_PATH = os.path.join(ROOT_DIR, "libs", "local_rates_country_level.csv")
-    estimated_rates = pd.read_csv(FILE_PATH)
-    commodity = param.get('commodity') if param.get('commodity') in HAZ_CLASSES else 'None'
-    container_size = param.get('container_size')
-    if container_size == '40HC':
-        container_size = '40'
-
-    estimated_rates['commodity'].fillna('None', inplace = True)
-    line_items = estimated_rates[(estimated_rates.location_id == param.get('country_id')) & (estimated_rates.container_type == param.get('container_type')) & (estimated_rates.container_size == int(container_size)) & (estimated_rates.trade_type == param.get('trade_type')) & (estimated_rates.commodity == commodity)]['line_items']
-    if len(line_items) > 0:
-        return set_local_line_items(line_items.iloc[0])
-    return [] 
 
 def get_search_query(local_rate_param):
     query = FclFreightRateLocal.select().where(
@@ -107,16 +70,12 @@ def get_search_query(local_rate_param):
         query = query.where(FclFreightRateLocal.shipping_line_id == local_rate_param['shipping_line_id'])
     return list(query.dicts())
 
-def set_local_line_items(local_rate):
-    line_items = []
-    rate = copy.deepcopy(eval(local_rate))
-    for line_item in rate:
-        local_price = round((line_item['upper_price'] + line_item['lower_price'])/2)
-        if local_price > 0 and (local_price%10 != 0):
-            line_item['price'] = local_price + (5 - local_price%10) if local_price%10 <= 5 else (local_price + (10 - local_price%10))
-        else:
-            line_item['price'] = local_price
-        del line_item['upper_price']
-        del line_item['lower_price']
-        line_items.append(line_item)
-    return line_items
+def get_batch_wise(location_id, BATCH_SIZE):
+    locations_data = []
+    for page_no in range(1,BATCH_SIZE):
+        locations = maps.list_locations({'filters':{'country_id': location_id, 'status':'active', "type":"seaport"},'page':page_no, 'page_limit':BATCH_SIZE})['list']
+        locations_data.extend(locations)
+        if len(locations) == 0:
+            break
+
+    return locations_data
