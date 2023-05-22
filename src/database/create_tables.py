8df7cb0682843840847eb8037831d85c924d6b30
Shiv <109941139+ShivCogoport@users.noreply.github.com>
2023-05-22 21:38:57 +0530

    main pull

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -1,11 +1,4 @@
 from database.db_session import db
-from services.chakravyuh.models.fcl_freight_rate_estimation import FclFreightRateEstimation
-from services.chakravyuh.models.fcl_freight_rate_estimation_audit import FclFreightRateEstimationAudit
-from services.chakravyuh.models.demand_transformation import DemandTransformation
-from services.chakravyuh.models.demand_transformation_audit import DemandTransformationAudit
-from services.chakravyuh.models.revenue_target import RevenueTarget
-from services.chakravyuh.models.revenue_target_audit import RevenueTargetAudit
-from services.chakravyuh.models.fcl_freight_rate_local_estimation import FclFreightRateLocalEstimation
 from services.trailer_freight_rates.models.trailer_freight_rate_estimator_constant import TrailerFreightRateCharges
 
 def create_table():
