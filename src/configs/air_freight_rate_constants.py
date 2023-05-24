474b31d5f14199ca6689aebfb25b28992adf15e6
bedadha-Ajay <bedada.kumar@cogoport.com>
2023-05-24 14:24:46 +0530

    air_cards

---
+++ src/configs/air_freight_rate_constants.py
@@ -0,0 +1,5 @@
+RATE_ENTITY_MAPPING = {
+  "6fd98605-9d5d-479d-9fac-cf905d292b88": ['6fd98605-9d5d-479d-9fac-cf905d292b88', 'b67d40b1-616c-4471-b77b-de52b4c9f2ff'],
+  "b67d40b1-616c-4471-b77b-de52b4c9f2ff": ['b67d40b1-616c-4471-b77b-de52b4c9f2ff', '6fd98605-9d5d-479d-9fac-cf905d292b88'],
+  "default": ['b67d40b1-616c-4471-b77b-de52b4c9f2ff', '6fd98605-9d5d-479d-9fac-cf905d292b88']
+}
--- src/services/air_freight_rate/interaction/get_air_freight_rate_cards.py
+++ src/services/air_freight_rate/interaction/get_air_freight_rate_cards.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from services.air_freight_rate.models.air_freight_rate import AirFreightRate
 from playhouse.postgres_ext import *
-
+from configs.air_freight_rate_constants import RATE_ENTITY_MAPPING
 def get_air_freight_rate_cards(request):
     if request['commodity'] =='general':
         request['commodity_subtype'] = 'all'
@@ -12,7 +12,57 @@ def get_air_freight_rate_cards(request):
     freight_query = initialize_freight_query(request)
 
 
-def initialize_freight_query(request):
-    return
+def initialize_freight_query(requirements,prediction_required=True):
+    freight_query = AirFreightRate.select(
+        AirFreightRate.id,
+        AirFreightRate.airline_id,
+        AirFreightRate.origin_airport_id,
+        AirFreightRate.destination_airport_id,
+        AirFreightRate.origin_country_id,
+        AirFreightRate.destination_country_id,
+        AirFreightRate.operation_type,
+        AirFreightRate.commodity,
+        AirFreightRate.commodity_type,
+        AirFreightRate.commodity_sub_type,
+        AirFreightRate.stacking_type,
+        AirFreightRate.shipment_type,
+        AirFreightRate.validities,
+        AirFreightRate.price_type,
+        AirFreightRate.service_provider_id,
+        AirFreightRate.cogo_entity_id
+    ).where(
+    AirFreightRate.origin_port_id == requirements.get('origin_airport_id'),
+    AirFreightRate.destination_port_id == requirements.get('destination_airport_id'),
+    AirFreightRate.commodity == requirements.get('commodity'),
+    AirFreightRate.commodity_type == requirements.get('commodity_type'),
+    AirFreightRate.commodity_sub_type == requirements.get('commodity_subtype'),
+    ~AirFreightRate.rate_not_available_entry,
+    AirFreightRate.handling_type == requirements.get('handling_type'),
+    AirFreightRate.shipment_type == requirements.get('shipment_type'),
+    ((AirFreightRate.importer_exporter_id == requirements.get('importer_exporter_id')) | (AirFreightRate.importer_exporter_id == None))
+    )
+
+    if requirements.get('commodity_subtype_code'):
+        freight_query.commodity_sub_type == requirements.get('commodity_subtype_code')
+
+    freight_query = freight_query.where(AirFreightRate.last_rate_available_date >= requirements['validity_start'])
+
+
+    if not prediction_required:
+        freight_query  = freight_query.where(((AirFreightRate.mode != 'predicted') | (AirFreightRate.mode.is_null(True))))
+    
+        rate_constant_mapping_key = requirements.get('cogo_entity_id')
+
+    allow_entity_ids = None
+    if rate_constant_mapping_key in RATE_ENTITY_MAPPING:
+        allow_entity_ids = RATE_ENTITY_MAPPING[rate_constant_mapping_key]
+
+    if allow_entity_ids:
+        freight_query = freight_query.where(((AirFreightRate.cogo_entity_id << allow_entity_ids) | (AirFreightRate.cogo_entity_id.is_null(True))))
+
+
+
+
+    
     
 
