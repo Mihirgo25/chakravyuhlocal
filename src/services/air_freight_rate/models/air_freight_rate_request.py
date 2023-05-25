65cb97b985aa9dfe54bb3584cad88f231f4d660b
garlapadu harshith <garlapadu.harshith@cogoport.com>
2023-05-25 14:17:26 +0530

    request_model

--- src/services/air_freight_rate/models/air_freight_rate_request.py
+++ src/services/air_freight_rate/models/air_freight_rate_request.py
@@ -10,36 +10,32 @@ class BaseModel(Model):
         only_save_dirty = True
 
 
-class AirCustomsRateRequests(BaseModel):
-    airport_id = UUIDField(null=True)
+class AirFreightRateFeedbacks(BaseModel):
+    air_freight_rate_id = UUIDField(null=True)
     booking_params = BinaryJSONField(null=True)
-    cargo_readiness_date = DateTimeField(null=True)
-    city_id = UUIDField(null=True)
     closed_by_id = UUIDField(null=True)
-    closing_remarks = ArrayField(field_class=CharField, null=True)
-    commodity = CharField(null=True)
-    continent_id = UUIDField(null=True)
-    country_id = UUIDField(null=True)
+    closing_remarks = ArrayField(constraints=[SQL("DEFAULT '{}'::character varying[]")], field_class=CharField, null=True)
     created_at = DateTimeField()
+    feedback_type = CharField(null=True)
+    feedbacks = ArrayField(field_class=CharField, null=True)
     id = UUIDField(constraints=[SQL("DEFAULT gen_random_uuid()")], primary_key=True)
+    outcome = CharField(null=True)
+    outcome_object_id = UUIDField(null=True)
     performed_by_id = UUIDField(null=True)
-    performed_by_org_id = CharField(null=True)
+    performed_by_org_id = UUIDField(null=True)
     performed_by_type = CharField(null=True)
-    preferred_customs_rate = DoubleField(null=True)
-    preferred_customs_rate_currency = CharField(null=True)
-    preferred_detention_free_days = IntegerField(null=True)
+    preferred_airline_ids = ArrayField(field_class=UUIDField, null=True)
+    preferred_freight_rate = DoubleField(null=True)
+    preferred_freight_rate_currency = CharField(null=True)
     preferred_storage_free_days = IntegerField(null=True)
     remarks = ArrayField(field_class=CharField, null=True)
-    request_type = CharField(null=True)
-    serial_id = BigIntegerField(constraints=[SQL("DEFAULT nextval('air_customs_rate_requests_serial_id_seq'::regclass)")])
+    serial_id = BigIntegerField(constraints=[SQL("DEFAULT nextval('air_freight_rate_feedbacks_serial_id_seq'::regclass)")])
     source = CharField(null=True)
     source_id = UUIDField(null=True)
     status = CharField(null=True)
-    trade_id = UUIDField(null=True)
     trade_type = CharField(null=True)
     updated_at = DateTimeField()
-    volume = DoubleField(null=True)
-    weight = DoubleField(null=True)
+    validity_id = UUIDField(null=True)
 
     class Meta:
-        table_name = 'air_customs_rate_requests'
+        table_name = 'air_freight_rate_feedbacks'
