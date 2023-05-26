a6933efc155bd575b0d0197f31e70c5f69c2d17a
shreyasnakshatram <shreyas.nakshatram@cogoport.com>
2023-05-26 18:31:35 +0530

    changes

--- src/services/fcl_customs_rate/models/fcl_customs_rate.py
+++ src/services/fcl_customs_rate/models/fcl_customs_rate.py
@@ -1,7 +1,8 @@
 from peewee import *
 from database.db_session import db
 from playhouse.postgres_ext import *
-import datetime
+import datetime, uuid
+from micro_services.client import maps, common
 
 class BaseModel(Model):
     class Meta:
@@ -47,3 +48,67 @@ class FclCustomsRate(BaseModel):
 
     class Meta:
         table_name = 'fcl_customs_rates'
+
+    def set_platform_price(self):
+      line_items = self.get_mandatory_line_items()
+      if line_items:
+        total_price = self.get_line_items_total_price()
+
+        rates_query = FclCustomsRate.select().where(
+          FclCustomsRate.location_id == self.location_id,
+          FclCustomsRate.trade_type == self.trade_type,
+          FclCustomsRate.container_size == self.container_size,
+          FclCustomsRate.container_type == self.container_type,
+          FclCustomsRate.commodity == self.commodity,
+          FclCustomsRate.is_customs_line_items_error_messages_present == False,
+          FclCustomsRate.service_provider_id != self.service_provider_id,
+          ((FclCustomsRate.importer_exporter_id == self.importer_exporter_id) | (FclCustomsRate.importer_exporter_id.is_null(True)))
+        )
+
+        rates = list(rates_query.dicts())
+
+        for rate in rates:
+          min_price = rate.customs_line_items.select{ |line_item|
+            mandatory_charge_codes.include?(line_item.code.upcase)
+          }.map{|t| GetMoneyExchange.run!(price: t.price, from_currency: t.currency, to_currency: currency)[:price].to_i}.sum
+
+          if min_price and total_price > min_price:
+            platform_price = min_price
+
+        if platform_price:
+          self.is_best_price = (total_price <= platform_price)
+
+        self.platform_price = platform_price
+
+    def get_mandatory_line_items(self):
+      for item in self.customs_line_items:
+        
+      return None
+
+    def get_line_items_total_price(self):
+      line_items = self.customs_line_items
+      currency = self.customs_line_items[0].get('currency')
+      total_price = 0
+      for line_item in line_items:
+        total_price += common.get_money_exchange_for_fcl({"price": line_item.get('price'), "from_currency": line_item.get('currency'), "to_currency": currency })['price']
+      return total_price
+
+    def set_location(self):
+        if self.port:
+            return
+
+        if not self.port_id:
+            return
+
+        location_ids = [str(self.location_id)]
+
+        ports = maps.list_locations({'filters':{'id': location_ids}})['list']
+        for port in ports:
+            if str(port.get('id')) == str(self.port_id):
+                self.country_id = port.get('country_id', None)
+                self.trade_id = port.get('trade_id', None)
+                self.continent_id = port.get('continent_id', None)
+                self.location_ids = [uuid.UUID(str(x)) for x in [self.port_id, self.country_id, self.trade_id, self.continent_id] if x is not None]
+                self.port = port
+            elif self.main_port_id and str(port.get('id')) == str(self.main_port_id):
+                self.main_port = port
