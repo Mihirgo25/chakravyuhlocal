a83804ad76bf272623f9475e09565e0b39991992
bedadha-Ajay <bedada.kumar@cogoport.com>
2023-05-26 17:17:07 +0530

    request

---
+++ src/services/air_freight_rate/interaction/delete_air_freight_rate_request.py
@@ -0,0 +1,97 @@
+from database.db_session import db
+from services.air_freight_rate.models.air_freight_rate_request import AirFreightRateRequest
+from fastapi import HTTPException
+from configs.global_constants import MAX_SERVICE_OBJECT_DATA_PAGE_LIMIT
+from micro_services.client import shipment
+def delete_air_freight_rate_requests(request):
+    with db.atomic():
+        return execute_transaction_code(request)
+
+
+def execute_transaction_code(request):
+    request_objects = AirFreightRateRequest.where(
+        AirFreightRateRequest.id << request.get('air_freight_rate_request_ids'),
+        AirFreightRateRequest.status == 'active'
+    ).execute()
+
+    if not request_objects:
+        raise HTTPException(status_code=400, detail='air_freight_rate_request_ids are invalid')
+    
+    for request_object in request_objects:
+        request_object.status = 'inactive'
+        request_object.closed_by_id = request.get('performed_by_id')
+        if request.get('closing_remarks'):
+            request_object.closing_remarks = request.get('closing_remarks')
+        
+        # if request_object.q
+        # here
+
+
+def is_valid_params(air_freight_rate, air_freight_rate_validity, air_freight_rate_request):
+    return air_freight_rate_validity and air_freight_rate.air_line_id == air_freight_rate_request.airline_id and air_freight_rate.service_provider_id == air_freight_rate_request.service_provider_id and air_freight_rate.price_type == air_freight_rate_request.price_type and air_freight_rate.operation_type == air_freight_rate_request.operation_type
+
+def collection_parties_present(request_object):
+
+    params = {
+        'filters':{ 'service_provider_id':request_object.service_provider_id,'shipment_id':request_object.shipment_id},
+        'pagination_data_required':False,
+        'page_limit': MAX_SERVICE_OBJECT_DATA_PAGE_LIMIT
+    }
+
+    req_collection_party = shipment.list_shipment_collection_party(params)['list']
+
+    if req_collection_party[0]['collection_parties']:
+        return False
+    return True
+
+# This method updates the buy line items for the shipment that is associated with the air freight rate request that is being deleted.
+def update_buy_line_items(air_freight_rate_validity,obj):
+    params ={
+        'shipment_id':obj.source_id,
+        'service_detail_required':True,
+        'performed_by_user_type':'agent'
+    }
+    shipment_quotations = shipment.get_shipment_services_quotation(params)["service_charges"]
+
+    if shipment_quotations:
+        air_freight_service_quotation = [shipment_quotation for shipment_quotation in shipment_quotations if shipment_quotation['service_type']=='air_freight_service'][0]
+        BAS_CHARGE_CODES = ['BAS', 'BASNO']
+        buy_line_items = air_freight_service_quotation['line_items']
+        for line_item in buy_line_items:
+            if line_item['code'] in BAS_CHARGE_CODES:
+                bas_buy_line_item = line_item
+                break
+        air_freight_service_detail = air_freight_service_quotation["service_detail"][0]
+        chargeable_weight = air_freight_service_detail["chargeable_weight"]
+        required_weight_slab = {}
+        for weight_slab in air_freight_rate_validity['weight_slabs']:
+                if weight_slab['lower_limit'] <= chargeable_weight and weight_slab['upper_limit'] > chargeable_weight:
+                    required_weight_slab = weight_slab
+                    break
+        
+        if required_weight_slab:
+            bas_buy_line_item['price'] = required_weight_slab.get('tariff_price')
+            bas_buy_line_item['currency'] = required_weight_slab.get('currency')
+            params ={
+                'quotations':[
+                    {
+                        'id':air_freight_service_quotation.get('id'),
+                        "service_id":air_freight_service_quotation.get('service_id'),
+                        'line_items':[bas_buy_line_item]
+                    }
+                ]
+            }
+
+            response = shipment.update_shipment_buy_quotations(params)
+
+            # error catcing
+        else:
+            raise HTTPException(status_code=404,detail='Weight Slab not consistent')
+
+        
+
+
+
+
+
+
