f141df66129ecabb666cd69c5520d0d86478ed64
mithilCogo <mithil.anchan@cogoport.com>
2023-05-26 14:39:07 +0530

    changes

--- src/services/ftl_freight_rate/interaction/create_truck.py
+++ src/services/ftl_freight_rate/interaction/create_truck.py
@@ -4,66 +4,78 @@ from database.db_session import db
 from services.ftl_freight_rate.models.ftl_services_audit import FtlServiceAudit
 import pandas as pd
 
+
 def create_audit(request, truck_id):
-    data = {key:str(value) for key, value in request.items() if key not in ['performed_by_id'] and not value == None}
+    data = {
+        key: str(value)
+        for key, value in request.items()
+        if key not in ["performed_by_id"] and not value == None
+    }
     FtlServiceAudit.create(
-        action_name = 'create',
-        performed_by_id = request['performed_by_id'],
-        data = data,
-        object_id = truck_id,
-        object_type = 'Truck'
+        action_name="create",
+        performed_by_id=request["performed_by_id"],
+        data=data,
+        object_id=truck_id,
+        object_type="Truck",
     )
+
 
 def create_truck_data(request):
     with db.atomic():
         return execute_transaction_code(request)
 
 
-
 def execute_transaction_code(request):
-    row={
-        'truck_company': request.get('truck_company'),
-        'truck_name': request.get('truck_name'),
-        'mileage': request.get('mileage'),
-        'mileage_unit': request.get('mileage_unit'),
-        'capacity': request.get('capacity'),
-        'capacity_unit': request.get('capacity_unit'),
-        'vehicle_weight': request.get('vehicle_weight'),
-        'vehicle_weight_unit': request.get('vehicle_weight_unit'),
-        'fuel_type': request.get('fuel_type'),
-        'avg_speed': request.get('avg_speed'),
-        'no_of_wheels': request.get('no_of_wheels'),
-        'engine_type': request.get('engine_type'),
-        'country_id': request.get('country_id'),
-        'axels': request.get('axels'),
-        'truck_type': request.get('truck_type'),
-        'body_type': request.get('body_type'),
-        'status': request.get('status') or 'active',
-        'horse_power': request.get('horse_power'),
-        'data':request.get('data')
+    row = {
+        "truck_company": request.get("truck_company"),
+        "truck_name": request.get("truck_name"),
+        "mileage": request.get("mileage"),
+        "mileage_unit": request.get("mileage_unit"),
+        "capacity": request.get("capacity"),
+        "capacity_unit": request.get("capacity_unit"),
+        "vehicle_weight": request.get("vehicle_weight"),
+        "vehicle_weight_unit": request.get("vehicle_weight_unit"),
+        "fuel_type": request.get("fuel_type"),
+        "avg_speed": request.get("avg_speed"),
+        "no_of_wheels": request.get("no_of_wheels"),
+        "engine_type": request.get("engine_type"),
+        "country_id": request.get("country_id"),
+        "axels": request.get("axels"),
+        "truck_type": request.get("truck_type"),
+        "body_type": request.get("body_type"),
+        "status": request.get("status") or "active",
+        "horse_power": request.get("horse_power"),
+        "data": request.get("data"),
     }
-    row['display_name'] = "{} {} ton".format(row['truck_company'], row['vehicle_weight'])
-    
-    truck = Truck.select().where((Truck.truck_name == request.get('truck_name')),
-                                 (Truck.horse_power == request.get('horse_power')),
-                                 (Truck.country_id == request.get('country_id')),
-                                 (Truck.engine_type == request.get('engine_type')),
-                                 (Truck.capacity == request.get('capacity')),
-                                 (Truck.fuel_type == request.get('fuel_type')),
-                                 (Truck.body_type == request.get('body_type')),
-                                 (Truck.no_of_wheels == request.get('no_of_wheels'))).first()
+    row["display_name"] = "{} {} ton".format(
+        row["truck_company"], row["vehicle_weight"]
+    )
+
+    truck = (
+        Truck.select()
+        .where(
+            (Truck.truck_name == request.get("truck_name")),
+            (Truck.horse_power == request.get("horse_power")),
+            (Truck.country_id == request.get("country_id")),
+            (Truck.engine_type == request.get("engine_type")),
+            (Truck.capacity == request.get("capacity")),
+            (Truck.fuel_type == request.get("fuel_type")),
+            (Truck.body_type == request.get("body_type")),
+            (Truck.no_of_wheels == request.get("no_of_wheels")),
+        )
+        .first()
+    )
 
     if not truck:
         truck = Truck(**row)
     else:
-        raise HTTPException(status_code=500, detail='truck already exist')
+        raise HTTPException(status_code=500, detail="truck already exist")
 
     try:
-      truck.save()
+        truck.save()
     except:
-      raise HTTPException(status_code=500, detail='truck did not save')
+        raise HTTPException(status_code=500, detail="truck did not save")
 
-    create_audit(request,truck.id)
+    create_audit(request, truck.id)
 
-    return {'id' : truck.id}
-
+    return {"id": truck.id}
--- src/services/ftl_freight_rate/interaction/list_trucks.py
+++ src/services/ftl_freight_rate/interaction/list_trucks.py
@@ -10,12 +10,42 @@ from libs.get_applicable_filters import get_applicable_filters
 
 
 # define filters
-POSSIBLE_DIRECT_FILTERS = ['id','truck_company','truck_name','created_at','mileage','mileage_unit','capacity','capacity_unit','vehicle_weight','vehicle_weight_unit','fuel_type','avg_speed','no_of_wheels','engine_type','country_id','axels','truck_type','body_type','status','horse_power','updated_at','display_name']
-POSSIBLE_INDIRECT_FILTERS = ['q','capacity_greater_equal_than', 'vehicle_weight_greater_equal_than']
+POSSIBLE_DIRECT_FILTERS = [
+    "id",
+    "truck_company",
+    "truck_name",
+    "created_at",
+    "mileage",
+    "mileage_unit",
+    "capacity",
+    "capacity_unit",
+    "vehicle_weight",
+    "vehicle_weight_unit",
+    "fuel_type",
+    "avg_speed",
+    "no_of_wheels",
+    "engine_type",
+    "country_id",
+    "axels",
+    "truck_type",
+    "body_type",
+    "status",
+    "horse_power",
+    "updated_at",
+    "display_name",
+]
+POSSIBLE_INDIRECT_FILTERS = [
+    "q",
+    "capacity_greater_equal_than",
+    "vehicle_weight_greater_equal_than",
+]
 TS_MATCH = "@@"
 TS_FULL_MATCH = "<->"
 
-def list_trucks_data(filters, page_limit, page, sort_by, sort_type, pagination_data_required):
+
+def list_trucks_data(
+    filters, page_limit, page, sort_by, sort_type, pagination_data_required
+):
     # make sql query
     query = get_query(sort_by, sort_type)
 
@@ -25,7 +55,9 @@ def list_trucks_data(filters, page_limit, page, sort_by, sort_type, pagination_d
             filters = json.loads(filters)
 
         # get applicable filters
-        direct_filters, indirect_filters = get_applicable_filters(filters, POSSIBLE_DIRECT_FILTERS, POSSIBLE_INDIRECT_FILTERS)
+        direct_filters, indirect_filters = get_applicable_filters(
+            filters, POSSIBLE_DIRECT_FILTERS, POSSIBLE_INDIRECT_FILTERS
+        )
         # direct filters
         query = get_filters(direct_filters, query, Truck)
         # indirect filters
@@ -34,16 +66,20 @@ def list_trucks_data(filters, page_limit, page, sort_by, sort_type, pagination_d
     query, total_count = apply_pagination(query, page, page_limit)
 
     # apply pagination
-    pagination_data = get_pagination_data(query, page, page_limit, pagination_data_required, total_count)
+    pagination_data = get_pagination_data(
+        query, page, page_limit, pagination_data_required, total_count
+    )
 
     data = jsonable_encoder(list(query.dicts()))
 
-    return {'list': data } | (pagination_data)
+    return {"list": data} | (pagination_data)
+
 
 # sql query
 def get_query(sort_by, sort_type):
     query = Truck.select().order_by(eval("Truck.{}.{}()".format(sort_by, sort_type)))
     return query
+
 
 def apply_pagination(query, page, page_limit):
     offset = (page - 1) * page_limit
@@ -51,26 +87,29 @@ def apply_pagination(query, page, page_limit):
     query = query.offset(offset).limit(page_limit)
     return query, total_count
 
+
 # split data into pages
 def get_pagination_data(query, page, page_limit, pagination_data_required, total_count):
     if not pagination_data_required:
         return {}
 
     params = {
-      'page': page,
-      'total': ceil(total_count/page_limit),
-      'total_count': total_count,
-      'page_limit': page_limit
+        "page": page,
+        "total": ceil(total_count / page_limit),
+        "total_count": total_count,
+        "page_limit": page_limit,
     }
     return params
+
 
 # indirect filters
 def apply_indirect_filters(query, filters):
     for key, val in filters.items():
         if key in POSSIBLE_INDIRECT_FILTERS:
-            apply_filter_function = f'apply_{key}_filter'
-            query = eval(f'{apply_filter_function}(query, val)')
+            apply_filter_function = f"apply_{key}_filter"
+            query = eval(f"{apply_filter_function}(query, val)")
     return query
+
 
 def set_and_constraints(val):
     val = val.strip()
@@ -83,11 +122,13 @@ def set_and_constraints(val):
     fields = "".join(fields)
     return fields
 
+
 def Match(field, query, language=None):
     params = (language, query) if language is not None else (query,)
     field_params = (language, field) if language is not None else (field,)
     params = fn.to_tsquery(*params)
-    return Expression(field_params,TS_MATCH,params)
+    return Expression(field_params, TS_MATCH, params)
+
 
 def set_or_constraints(val):
     val = val.strip()
@@ -100,16 +141,14 @@ def set_or_constraints(val):
     fields = "".join(fields)
     return fields
 
+
 # Q filter
 def apply_q_filter(query, val):
-    val = val.translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
+    val = val.translate({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
     if not val:
         return query
     search_field = set_and_constraints(val)
-    # print(search_field)
-    search_query = query.where(
-        Match(Truck.search_vector, search_field)
-    )
+    search_query = query.where(Match(Truck.search_vector, search_field))
     if search_query.count() == 0:
         search_field = set_or_constraints(val)
         search_query = query.where(Match(Truck.search_vector, search_field))
@@ -120,9 +159,11 @@ def apply_q_filter(query, val):
     query = search_query.order_by(SQL("id desc"))
     return query
 
+
 # filter greater than or equal to capacity
 def apply_capacity_greater_equal_than_filter(query, val):
     return query.where(Truck.capacity >= val)
+
 
 def apply_vehicle_weight_greater_equal_than_filter(query, val):
     return query.where(Truck.vehicle_weight >= val)
