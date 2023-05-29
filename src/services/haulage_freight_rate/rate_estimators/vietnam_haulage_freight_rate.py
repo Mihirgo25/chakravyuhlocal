ca6832a14bf70cd0ddd95ad9aca41f4128ec9609
Saksham573001 <saksham.sharma@cogoport.com>
2023-05-29 13:25:18 +0530

    feat : vietnam rates

---
+++ src/services/haulage_freight_rate/rate_estimators/vietnam_haulage_freight_rate.py
@@ -0,0 +1,13 @@
+
+from fastapi import HTTPException
+
+
+class VietnamHaulageFreightRateEstimator:
+    def __init__(self, *_, **__):
+        pass
+
+    def estimate(self):
+        """
+        Primary Function to estimate generalized prices
+        """
+        raise HTTPException(status_code=400, detail="rates not present")
