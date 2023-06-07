78e6fd79fa56efefb1ca7a221d9efa37c767d670
Abhishek <abhishek@cogoport.com>
2023-06-07 16:12:26 +0530

    create table

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -1,11 +1,10 @@
 from database.db_session import db
-from services.trailer_freight_rates.models.trailer_freight_rate_estimator_constant import TrailerFreightRateCharges
-from services.haulage_freight_rate.models.haulage_freight_rate_rule_sets import HaulageFreightRateRuleSet
-from services.haulage_freight_rate.models.wagon_types import WagonTypes
+from services.chakravyuh.models.cost_booking_estimation import CostBookingEstimation
+from services.chakravyuh.models.cost_booking_estimation_audit import CostBookingEstimationAudit
 def create_table():
     # db.connect()
     try:
-        db.create_tables([TrailerFreightRateCharges, HaulageFreightRateRuleSet, WagonTypes])
+        db.create_tables([CostBookingEstimation, CostBookingEstimationAudit])
         db.close()
         print("created table")
     except:
