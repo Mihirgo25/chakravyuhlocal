ed3ec4b4323ecb3ccd089e8f618d54df9fac303e
Sarvesh Kumar <sarvesh.kumar@cogoport.com>
2023-06-09 17:29:10 +0530

    fix

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -1,12 +1,10 @@
 from database.db_session import db
 from services.chakravyuh.models.cost_booking_estimation import CostBookingEstimation
 from services.chakravyuh.models.cost_booking_estimation_audit import CostBookingEstimationAudit
-from services.fcl_freight_rate.models.fcl_freight_rate_feedback import FclFreightRateFeedback
-
 def create_table():
     # db.connect()
     try:
-        db.create_tables([FclFreightRateFeedback])
+        db.create_tables([CostBookingEstimation, CostBookingEstimationAudit])
         db.close()
         print("created table")
     except:
