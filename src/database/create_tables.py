7edaf353fa0d96d433dd24ac383001a138c99f63
Santram Gurjar <santram.gurjar@cogoport.com>
2023-06-09 20:06:49 +0530

    create

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -1,10 +1,13 @@
 from database.db_session import db
-from services.chakravyuh.models.cost_booking_estimation import CostBookingEstimation
-from services.chakravyuh.models.cost_booking_estimation_audit import CostBookingEstimationAudit
+from services.chakravyuh.models.air_freight_rate_estimation import AirFreightRateEstimation
+from services.chakravyuh.models.air_freight_rate_estimation_audit import AirFreightRateEstimationAudit
+from services.air_freight_rate.models.air_freight_location_cluster_factor import AirFreightLocationClusterFactor
+from services.air_freight_rate.models.air_freight_location_cluster_mapping import AirFreightLocationClusterMapping
+from services.air_freight_rate.models.air_freight_location_clusters import AirFreightLocationClusters
 def create_table():
     # db.connect()
     try:
-        db.create_tables([CostBookingEstimation, CostBookingEstimationAudit])
+        db.create_tables([AirFreightRateEstimation, AirFreightRateEstimationAudit, AirFreightLocationClusterFactor, AirFreightLocationClusterMapping, AirFreightLocationClusters])
         db.close()
         print("created table")
     except:
