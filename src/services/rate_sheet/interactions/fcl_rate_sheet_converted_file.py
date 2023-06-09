2451073026c13da06850dc82eb4659e5d276c630
Santram Gurjar <santram.gurjar@cogoport.com>
2023-06-09 18:03:55 +0530

    deploy

--- src/services/rate_sheet/interactions/fcl_rate_sheet_converted_file.py
+++ src/services/rate_sheet/interactions/fcl_rate_sheet_converted_file.py
@@ -902,7 +902,7 @@ def process_fcl_freight_freight(params, converted_file, update):
         input_file = csv.DictReader(file)
         headers = input_file.fieldnames
 
-        if len(set(valid_headers) & set(headers)) != len(valid_headers):
+        if len(set(valid_headers) & set(headers)) != len(headers):
             error_file = ['invalid header']
             csv_writer.writerow(error_file)
             invalidated = True
@@ -935,6 +935,7 @@ def process_fcl_freight_freight(params, converted_file, update):
             if valid_hash(row, present_field, blank_field):
                 if rows:
                     last_row = list(row.values())
+                    print(is_previous_rate_valid, '1', params)
                     # Create previous rate if previous rate was valid
                     if is_previous_rate_valid:
                         create_fcl_freight_freight_rate(
@@ -1186,6 +1187,7 @@ def process_fcl_freight_freight(params, converted_file, update):
                 csv_writer.writerow(list_opt)
             else:
                 list_opt = []
+                print(is_previous_rate_valid, '2', params)
                 if rows and is_previous_rate_valid and is_main_rate_row:
                     last_row = list(row.values())
                     create_fcl_freight_freight_rate(
@@ -1228,7 +1230,7 @@ def process_fcl_freight_freight(params, converted_file, update):
         update.status = 'partially_complete'
         converted_file['status'] = 'partially_complete'
     
-    print(update.status,converted_file)
+    print(update.status,converted_file, '3')
 
     set_processed_percent(percent_completed, params)
     try:
@@ -1243,7 +1245,7 @@ def create_fcl_freight_freight_rate(
     from celery_worker import create_fcl_freight_rate_delay, celery_extend_create_fcl_freight_rate_data
     keys_to_extract = ['container_size', 'container_type', 'commodity', 'validity_start', 'validity_end', 'schedule_type', 'payment_term', 'rate_type']
     object = dict(filter(lambda item: item[0] in keys_to_extract, rows[0].items()))
-    print(object)
+    print(object, '4')
     object['validity_start'] = convert_date_format(object.get('validity_start'))
     object['validity_end'] = convert_date_format(object.get('validity_end'))
     for port in [
