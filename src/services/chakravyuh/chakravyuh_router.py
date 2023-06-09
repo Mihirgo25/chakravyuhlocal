30e9603a82b19c58bf8c1dac7d5c2ddb6f42b2e1
Bedada Ajay Kumar <bedadaajay@Bedadas-MacBook-Air.local>
2023-06-09 14:23:16 +0530

    router_change

--- src/services/chakravyuh/chakravyuh_router.py
+++ src/services/chakravyuh/chakravyuh_router.py
@@ -114,7 +114,7 @@ def apply_dynamic_pricing_for_air(weight:float,freight_rates: Json= Query(None),
         consumer = AirFreightVyuhConsumer(freight_rates = freight_rates,weight=weight)
         freight_rates = consumer.apply_dynamic_pricing()
         response = {'list':freight_rates}
-        return JSONResponse(status_code=200, content=freight_rates)
+        return JSONResponse(status_code=200, content=response)
     except HTTPException as e:
         raise
     except Exception as e:
