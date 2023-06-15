f575d7c6eb9a21f44b3bf9eaf1d125d84979b4b2
Muchipalli Abhishikth <125270594+abhicogoport@users.noreply.github.com>
2023-06-15 19:48:10 +0530

    changes

--- src/services/fcl_freight_rate/models/fcl_freight_rate_bulk_operation.py
+++ src/services/fcl_freight_rate/models/fcl_freight_rate_bulk_operation.py
@@ -71,8 +71,9 @@ class FclFreightRateBulkOperation(BaseModel):
         if data['validity_end'] < data['validity_start']:
             raise HTTPException(status_code=400, detail='validity_end cannot be less than validity start')
         
-        if data['rates_greater_than_price'] > data['rates_less_than_price']:
+        if data.get('rates_greater_than_price')!=None and data.get('rates_greater_than_price')!=None and data['rates_greater_than_price'] > data['rates_less_than_price']:
             raise HTTPException(status_code=400, detail='lower_limit cannot be less than upper_limit')
+        
         
         data['validity_start'] = data['validity_start'].strftime('%Y-%m-%d')
         data['validity_end'] = data['validity_end'].strftime('%Y-%m-%d')
@@ -96,7 +97,7 @@ class FclFreightRateBulkOperation(BaseModel):
         if data['validity_end'].date() < data['validity_start'].date():
             raise HTTPException(status_code=400, detail='validity_end cannot be less than validity start')
         
-        if data['rates_greater_than_price'] > data['rates_less_than_price']:
+        if data.get('rates_greater_than_price')!=None and data.get('rates_greater_than_price')!=None and data['rates_greater_than_price'] > data['rates_less_than_price']:
             raise HTTPException(status_code=400, detail='lower_limit cannot be less than upper_limit')
         
         fcl_freight_charges_dict = FCL_FREIGHT_CHARGES
