2368402866288e796126339d3ceb5526b67a42f3
Sahil-Cogo <sahil.ghule@cogoport.com>
2023-06-17 12:05:03 +0530

    minor changes

--- src/services/fcl_freight_rate/helpers/fcl_freight_rate_bulk_operation_helpers.py
+++ src/services/fcl_freight_rate/helpers/fcl_freight_rate_bulk_operation_helpers.py
@@ -1,6 +1,6 @@
 from services.fcl_freight_rate.models.fcl_freight_rate_audit import FclFreightRateAudit
 
-def get_rate_ids(select_field, id):
+def get_relevant_rate_ids_from_audits(select_field, id):
     reference_object_ids = FclFreightRateAudit.select(getattr(FclFreightRateAudit, select_field)).where(FclFreightRateAudit.object_id == id)
     reference_object_ids = [audit_data[select_field] for audit_data in list(reference_object_ids.dicts())] 
     
