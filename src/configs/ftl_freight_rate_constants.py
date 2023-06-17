2eb89eeaaee0a10f54d6c5a38daae6742eacfab4
work <mithil.anchan@cogoport.com>
2023-06-17 14:49:32 +0530

    code clean up

--- src/configs/ftl_freight_rate_constants.py
+++ src/configs/ftl_freight_rate_constants.py
@@ -233,8 +233,8 @@ DISTANCE_FACTOR = {
 }
 # distance factor for rate limiting basis on ranges of distances.
 DISTANCE_RATE_FACTOR = {
-    0:[{'lower_limit':0,'upper_limit':50,'rate_factor':3.5},{'lower_limit':50,'upper_limit':500,'rate_factor':2.2,'linear_decreasing':True},{'lower_limit':500,'upper_limit':1100,'rate_factor':1},{'lower_limit':1100,'upper_limit':3000,'rate_factor':0.94}],
-    1:[{'lower_limit':0,'upper_limit':50,'rate_factor':1.5},{'lower_limit':50,'upper_limit':500,'rate_factor':0.4,'linear_decreasing':True},{'lower_limit':500,'upper_limit':1100,'rate_factor':1},{'lower_limit':1100,'upper_limit':3000,'rate_factor':0.95}]
+    0:[{'lower_limit':0,'upper_limit':50,'rate_factor':3.5},{'lower_limit':50,'upper_limit':500,'rate_factor':1.2,'linear_decreasing':True},{'lower_limit':500,'upper_limit':1100,'rate_factor':1},{'lower_limit':1100,'upper_limit':3000,'rate_factor':0.94}],
+    1:[{'lower_limit':0,'upper_limit':50,'rate_factor':1.5},{'lower_limit':50,'upper_limit':500,'rate_factor':1.1,'linear_decreasing':True},{'lower_limit':500,'upper_limit':1100,'rate_factor':1},{'lower_limit':1100,'upper_limit':3000,'rate_factor':0.95}]
 } 
 
 # overall rate factor basis on capacity of the truck
