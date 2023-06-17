3b67509de364f5b8a96ccba1fc7bab8bc682e696
Sarvesh Kumar <sarvesh.kumar@cogoport.com>
2023-06-17 11:10:48 +0530

    add audit for draft fcl and local

--- src/services/nandi/interactions/update_draft_fcl_freight_rate_local.py
+++ src/services/nandi/interactions/update_draft_fcl_freight_rate_local.py
@@ -1,4 +1,5 @@
 from services.nandi.models.draft_fcl_freight_rate_local import DraftFclFreightRateLocal
+from services.nandi.models.draft_fcl_freight_rate_local_audit import DraftFclFreightRateLocalAudit
 from database.db_session import db
 from fastapi import HTTPException
 
@@ -23,4 +24,19 @@ def execute_transaction_code(request):
     except Exception as e:
         raise HTTPException(status_code=500, detail="rate did not update")
 
+    create_audit(request, draft_freight_local.id)
     return {'id': draft_freight_local.id}
+
+def create_audit(request, draft_fcl_local_id):
+    audit_data = {"status": request.get("status")}
+    try:
+        DraftFclFreightRateLocalAudit.create(
+        action_name = 'update',
+        performed_by_id = request['performed_by_id'],
+        data = audit_data,
+        source = request.get("source"),
+        object_id = draft_fcl_local_id,
+        object_type = 'DraftFclFreightRateLocal'
+      )
+    except:
+      raise HTTPException(status_code=500, detail='draft fcl freight local audit did not save')
