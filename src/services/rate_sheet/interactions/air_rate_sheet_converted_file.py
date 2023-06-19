6d818562eb39571594d6aec29ea4b7d442a08d75
Saksham573001 <saksham.sharma@cogoport.com>
2023-06-19 17:17:44 +0530

    fix : air_freight

--- src/services/rate_sheet/interactions/air_rate_sheet_converted_file.py
+++ src/services/rate_sheet/interactions/air_rate_sheet_converted_file.py
@@ -6,13 +6,8 @@ from services.rate_sheet.interactions.upload_file import upload_media_file
 from services.rate_sheet.interactions.validate_air_freight_object import (
     validate_air_freight_object,
 )
-from database.db_session import rd
-
 from fastapi.encoders import jsonable_encoder
-
-from datetime import datetime
-import dateutil.parser as parser
-from database.rails_db import get_shipping_line, get_organization
+from database.rails_db import get_shipping_line
 from services.rate_sheet.helpers import *
 import chardet
 from libs.parse_numeric import parse_numeric
@@ -20,8 +15,29 @@ from services.rate_sheet.interactions.fcl_rate_sheet_converted_file import (
     set_processed_percent,
     valid_hash,
 )
+from micro_services.client import maps
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+def get_airport_id(port_code, country_code):
+    input = {"filters": {"type": "seaport", "port_code": port_code, "status": "active"}}
+    locations_data = maps.list_locations(input)
+    if "list" in locations_data and len(locations_data["list"]) > 0:
+        airport_ids = locations_data["list"][0]
+    else:
+        airport_ids = None
+    return airport_ids
+
+
+def get_airline_id(airline_name):
+    try:
+        airline_id = get_shipping_line(
+            short_name=airline_name, operator_type="airline"
+        )[0]["id"]
+    except:
+        airline_id = None
+    return airline_id
 
 
 def process_air_freight_freight(params, converted_file, update):
@@ -129,7 +145,6 @@ def process_air_freight_freight(params, converted_file, update):
             if valid_hash(row, present_field, blank_field):
                 if rows:
                     last_row = list(row.values())
-                    print(is_previous_rate_valid, "1", params)
                     # Create previous rate if previous rate was valid
                     if is_previous_rate_valid:
                         create_air_freight_freight_rate(
@@ -154,243 +169,36 @@ def process_air_freight_freight(params, converted_file, update):
                     list_opt = list(row.values())
                     csv_writer.writerow(list_opt)
                 rows = [row]
-            elif (
-                valid_hash(
-                    row,
-                    ["lower_limit", "upper_limit", "tariff_price"],
-                    [
-                        "origin_airport",
-                        "origin_country",
-                        "destination_airport",
-                        "destination_country",
-                        "commodity",
-                        "commodity_type",
-                        "commodity_sub_type",
-                        "airline",
-                        "operation_type",
-                        "min_price",
-                        "currency",
-                        "weight_lower_limit",
-                        "weight_upper_limit",
-                        "weight_limit_price",
-                        "weight_limit_currency",
-                        "destination_detention_free_limit",
-                        "destination_detention_lower_limit",
-                        "destination_detention_upper_limit",
-                        "destination_detention_price",
-                        "destination_detention_currency",
-                        "schedule_type",
-                        "payment_term",
-                        "rate_type",
-                    ],
-                )
-                or valid_hash(
-                    row,
-                    ["weight_free_limit"],
-                    [
-                        "origin_port",
-                        "origin_main_port",
-                        "destination_port",
-                        "destination_main_port",
-                        "container_size",
-                        "container_type",
-                        "commodity",
-                        "shipping_line",
-                        "code",
-                        "unit",
-                        "price",
-                        "currency",
-                        "validity_start",
-                        "validity_end",
-                        "weight_lower_limit",
-                        "weight_upper_limit",
-                        "weight_limit_price",
-                        "weight_limit_currency",
-                        "destination_detention_free_limit",
-                        "destination_detention_lower_limit",
-                        "destination_detention_upper_limit",
-                        "destination_detention_price",
-                        "destination_detention_currency",
-                        "schedule_type",
-                        "payment_term",
-                        "rate_type",
-                    ],
-                )
-                or valid_hash(
-                    row,
-                    [
-                        "weight_free_limit",
-                        "weight_lower_limit",
-                        "weight_upper_limit",
-                        "weight_limit_price",
-                        "weight_limit_currency",
-                    ],
-                    [
-                        "origin_port",
-                        "origin_main_port",
-                        "destination_port",
-                        "destination_main_port",
-                        "container_size",
-                        "container_type",
-                        "commodity",
-                        "shipping_line",
-                        "code",
-                        "unit",
-                        "price",
-                        "currency",
-                        "validity_start",
-                        "validity_end",
-                        "destination_detention_free_limit",
-                        "destination_detention_lower_limit",
-                        "destination_detention_upper_limit",
-                        "destination_detention_price",
-                        "destination_detention_currency",
-                        "schedule_type",
-                        "payment_term",
-                        "rate_type",
-                    ],
-                )
-                or valid_hash(
-                    row,
-                    [
-                        "weight_lower_limit",
-                        "weight_upper_limit",
-                        "weight_limit_price",
-                        "weight_limit_currency",
-                    ],
-                    [
-                        "origin_port",
-                        "origin_main_port",
-                        "destination_port",
-                        "destination_main_port",
-                        "container_size",
-                        "container_type",
-                        "commodity",
-                        "shipping_line",
-                        "code",
-                        "unit",
-                        "price",
-                        "currency",
-                        "validity_start",
-                        "validity_end",
-                        "weight_free_limit",
-                        "destination_detention_free_limit",
-                        "destination_detention_lower_limit",
-                        "destination_detention_upper_limit",
-                        "destination_detention_price",
-                        "destination_detention_currency",
-                        "schedule_type",
-                        "payment_term",
-                        "rate_type",
-                    ],
-                )
-                or valid_hash(
-                    row,
-                    ["destination_detention_free_limit"],
-                    [
-                        "origin_port",
-                        "origin_main_port",
-                        "destination_port",
-                        "destination_main_port",
-                        "container_size",
-                        "container_type",
-                        "commodity",
-                        "shipping_line",
-                        "code",
-                        "unit",
-                        "price",
-                        "currency",
-                        "validity_start",
-                        "validity_end",
-                        "weight_free_limit",
-                        "weight_lower_limit",
-                        "weight_upper_limit",
-                        "weight_limit_price",
-                        "weight_limit_currency",
-                        "destination_detention_lower_limit",
-                        "destination_detention_upper_limit",
-                        "destination_detention_price",
-                        "destination_detention_currency",
-                        "schedule_type",
-                        "payment_term",
-                        "rate_type",
-                    ],
-                )
-                or valid_hash(
-                    row,
-                    [
-                        "destination_detention_free_limit",
-                        "destination_detention_lower_limit",
-                        "destination_detention_upper_limit",
-                        "destination_detention_price",
-                        "destination_detention_currency",
-                    ],
-                    [
-                        "origin_port",
-                        "origin_main_port",
-                        "destination_port",
-                        "destination_main_port",
-                        "container_size",
-                        "container_type",
-                        "commodity",
-                        "shipping_line",
-                        "code",
-                        "unit",
-                        "price",
-                        "currency",
-                        "validity_start",
-                        "validity_end",
-                        "weight_free_limit",
-                        "weight_lower_limit",
-                        "weight_upper_limit",
-                        "weight_limit_price",
-                        "weight_limit_currency",
-                        "schedule_type",
-                        "payment_term",
-                        "rate_type",
-                    ],
-                )
-                or valid_hash(
-                    row,
-                    [
-                        "destination_detention_lower_limit",
-                        "destination_detention_upper_limit",
-                        "destination_detention_price",
-                        "destination_detention_currency",
-                    ],
-                    [
-                        "origin_port",
-                        "origin_main_port",
-                        "destination_port",
-                        "destination_main_port",
-                        "container_size",
-                        "container_type",
-                        "commodity",
-                        "shipping_line",
-                        "code",
-                        "unit",
-                        "price",
-                        "currency",
-                        "validity_start",
-                        "validity_end",
-                        "weight_free_limit",
-                        "weight_lower_limit",
-                        "weight_upper_limit",
-                        "weight_limit_price",
-                        "weight_limit_currency",
-                        "destination_detention_free_limit",
-                        "schedule_type",
-                        "payment_term",
-                        "rate_type",
-                    ],
-                )
+
+            elif valid_hash(
+                row,
+                ["lower_limit", "upper_limit", "tariff_price"],
+                [
+                    "origin_airport",
+                    "origin_country",
+                    "destination_airport",
+                    "destination_country",
+                    "commodity",
+                    "commodity_type",
+                    "commodity_sub_type",
+                    "airline",
+                    "operation_type",
+                    "min_price",
+                    "currency",
+                    "validity_start",
+                    "validity_end",
+                    "packing_type",
+                    "handling_type",
+                    "price_type",
+                    "density_category",
+                    "density_ratio",
+                ],
             ):
                 rows.append(row)
                 list_opt = list(row.values())
                 csv_writer.writerow(list_opt)
             else:
                 list_opt = []
-                print(is_previous_rate_valid, "2", params)
                 if rows and is_previous_rate_valid and is_main_rate_row:
                     last_row = list(row.values())
                     create_air_freight_freight_rate(
@@ -474,28 +282,55 @@ def create_air_freight_freight_rate(
     from celery_worker import (
         create_air_freight_rate_delay,
     )
-    keys_to_extract = ["origi   n_airport", "origin_country", "destination_airport", "destination_country", "commodity", "commodity_type", "commodity_sub_type", "airline", "operation_type", "min_price", "currency", "validity_start", "validity_end", "packing_type", "handling_type", "price_type", "density_category", "density_ratio"]
+
+    keys_to_extract = [
+        "commodity",
+        "operation_type",
+        "min_price",
+        "currency",
+        "validity_start",
+        "validity_end",
+        "commodity_type",
+        "commodity_sub_type",
+        "packing_type",
+        "handling_type",
+        "price_type",
+        "density_category",
+        "density_ratio",
+    ]
     object = dict(filter(lambda item: item[0] in keys_to_extract, rows[0].items()))
-    rows["origin_airport"] = object["origin_airport"].upper().strip()
-    rows["origin_country"] = object["origin_country"].upper().strip()
-    rows["destination_airport"] = object["destination_airport"].upper().strip()
-    rows["destination_country"] = object["destination_country"].upper().strip()
-    rows["commodity"] = object["commodity"].lower().strip()
-    rows["commodity_type"] = object["commodity_type"].lower().strip()
-    rows["commodity_sub_type"] = object["commodity_sub_type"].lower().strip()
-    rows["airline"] = object["airline"].strip()
-    rows["currency"] = object["currency"].upper().strip()
-    rows["price_type"] = object["price_type"].lower().strip()
-    rows["density_category"] = object["density_category"].lower().strip()
-    rows["density_ratio"] = object["density_ratio"].strip()
+    object["commodity"] = object["commodity"].lower().strip()
+    object["commodity_type"] = object["commodity_type"].lower().strip()
+    object["commodity_sub_type"] = object["commodity_sub_type"].lower().strip()
+    object["currency"] = object["currency"].upper().strip()
+    object["price_type"] = object["price_type"].lower().strip()
+    object["density_category"] = object["density_category"].lower().strip()
     object["density_ratio"] = "1:{}".format(object["density_ratio"].strip())
-    object['validity_start'] = convert_date_format(object.get('validity_start'))
-    object['validity_end'] = convert_date_format(object.get('validity_end'))
-    object["rate_type"] = 'general'
+
+    object["validity_start"] = convert_date_format(object.get("validity_start"))
+    object["validity_end"] = convert_date_format(object.get("validity_end"))
+
+    object["length"] = 300
+    object["bredth"] = 300
+    object["height"] = 300
+
+    object["rate_type"] = "general"
     object["initial_volume"] = None
     object["available_volume"] = None
     object["initial_gross_weight"] = None
     object["available_gross_weight"] = None
+    object["weight_slabs"] = []
+
+    object["origin_airport_id"] = get_airport_id(
+        rows[0]["origin_airport"].upper().strip(),
+        rows[0]["origin_country"].upper().strip(),
+    )
+    object["destination_airport_id"] = get_airport_id(
+        rows[0]["destination_airport"].upper().strip(),
+        rows[0]["destination_country"].upper().strip(),
+    )
+    object["airline_id"] = get_airline_id(rows[0]["airline"].strip())
+    object["shipment_type"] = object["packing_type"]
     object["weight_slabs"] = []
 
     for slab in rows:
@@ -504,41 +339,43 @@ def create_air_freight_freight_rate(
         weight_slab["upper_limit"] = slab["upper_limit"].strip()
         weight_slab["tariff_price"] = slab["tariff_price"].strip()
         weight_slab["currency"] = object["currency"]
+        weight_slab["unit"] = object["unit"]
         object["weight_slabs"].append(weight_slab)
-    
-    object["service_provider_id"] = params.get('service_provider_id')
-    object["cogo_entity_id"] = params.get('cogo_entity_id')
+
+    # object["service_provider_id"] = params.get('service_provider_id')
+    # object["cogo_entity_id"] = params.get('cogo_entity_id')
     object["source"] = "rate_sheet"
 
-    operation_types = object["operation_type"].lower().split(',')
-    packing_types = object["packing_types"].lower().split(',')
-    handling_types = object["handling_types"].lower().split(',')
+    operation_types = object["operation_type"].lower().split(",")
+    packing_types = object["packing_types"].lower().split(",")
+    handling_types = object["handling_types"].lower().split(",")
 
     for operation in operation_types:
-        object['operation_type'] = operation
+        object["operation_type"] = operation
         for packing in packing_types:
-            object['packing_type'] = packing
+            object["packing_type"] = packing
             for handling in handling_types:
-                object['handling_type'] = handling
-                
-    request_params = 0
-    validation = write_air_freight_freight_object(
-        request_params, csv_writer, params, converted_file, last_row
-    )
-    if validation.get("valid"):
-        object["rate_sheet_validation"] = True
-        create_air_freight_rate_delay.apply_async(
-            kwargs={"request": object}, queue="air_freight_rate"
-        )
-    else:
-        print(validation.get("error"))
+                object["handling_type"] = handling
+                request_params = object
+                validation = write_air_freight_freight_object(
+                    request_params, csv_writer, params, converted_file, last_row
+                )
+                if validation.get("valid"):
+                    object["rate_sheet_validation"] = True
+                    create_air_freight_rate_delay.apply_async(
+                        kwargs={"request": object}, queue="air_freight_rate"
+                    )
+                else:
+                    print(validation.get("error"))
 
 
 def write_air_freight_freight_object(rows, csv, params, converted_file, last_row):
-    object_validity = validate_air_freight_object(converted_file.get('module'), rows)
+    object_validity = validate_air_freight_object(converted_file.get("module"), rows)
     if object_validity["valid"]:
         csv.writerow(last_row)
-        converted_file['valid_rates_count'] = int(converted_file['valid_rates_count'])+1
+        converted_file["valid_rates_count"] = (
+            int(converted_file["valid_rates_count"]) + 1
+        )
     else:
         try:
             error = ["".join(str(object_validity.get("error")))]
@@ -547,6 +384,6 @@ def write_air_freight_freight_object(rows, csv, params, converted_file, last_row
             csv.writerow([])
             csv.writerow(last_row)
         except:
-            print('no csv')
-    converted_file['rates_count'] = int(converted_file['rates_count'])+1
+            print("no csv")
+    converted_file["rates_count"] = int(converted_file["rates_count"]) + 1
     return object_validity
