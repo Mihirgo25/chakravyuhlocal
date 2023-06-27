17de29ddc41ab9dffa350882c154c9502bb03e29
Muchipalli Abhishikth <125270594+abhicogoport@users.noreply.github.com>
2023-06-27 13:38:22 +0530

    changes

--- src/services/haulage_freight_rate/interactions/get_haulage_freight_rate_frequency_addition.py
+++ src/services/haulage_freight_rate/interactions/get_haulage_freight_rate_frequency_addition.py
@@ -38,5 +38,6 @@ def get_data(query, sort_type, group_by):
     return jsonable_encoder(list(data.dicts()))
 
 def apply_procured_by_id_filter(query, filters):
-    return query.join(HaulageFreightRateAudit, on=(HaulageFreightRateAudit.object_id == HaulageFreightRate.id)).where((HaulageFreightRateAudit.object_type == 'HaulageFreightRate') &
+   query = query.join(HaulageFreightRateAudit, on=(HaulageFreightRateAudit.object_id == HaulageFreightRate.id)).where((HaulageFreightRateAudit.object_type == 'HaulageFreightRate') &
                         (HaulageFreightRateAudit.procured_by_id == filters['procured_by_id']))
+   return query
