cd9cd7b6bdcaf9496dd496344971556665456315
harshithgarlapad098 <garlapadu.harshith@cogoport.com>
2023-07-05 20:30:01 +0530

    workers done

--- src/services/air_freight_rate/workers/send_air_freight_local_charges_update_reminder_notification.py
+++ src/services/air_freight_rate/workers/send_air_freight_local_charges_update_reminder_notification.py
@@ -1,6 +1,21 @@
-from database.rails_db import get_partner_users
+from database.rails_db import get_supply_agents
+from micro_services.client import common
 
 
 def execute():
-    supply_agents=[]
-    return
+    supply_agents=get_supply_agents()
+    if not supply_agents:
+        return  
+    
+    for supply_agent in supply_agents:
+        send_notification_to_update_local_charges(supply_agent)
+
+def send_notification_to_update_local_charges(supply_agent):
+    notification_data = {
+      'type': 'platform_notification',
+      'user_id': supply_agent['user_id'],
+      'service': 'partner_user',
+      'service_id': supply_agent['user_id'],
+      'template_name': 'reminder_air_freight_local_charges'
+    }
+    common.create_communication(notification_data)
