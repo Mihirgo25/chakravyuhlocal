43109ad6836511cd7086f005ffa409e69be6e29c
Saksham573001 <saksham.sharma@cogoport.com>
2023-07-06 16:22:41 +0530

    fix: create table fixes

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -1,12 +1,13 @@
 from database.db_session import db
 from services.haulage_freight_rate.models.haulage_freight_rate_request import HaulageFreightRateRequest
 from services.haulage_freight_rate.models.haulage_freight_rate_audit import HaulageFreightRateAudit
-from services.haulage_freight_rate.models.haulage_freight_rate import HaulageFreightRate
 from services.haulage_freight_rate.models.haulage_freight_rate_bulk_operation import HaulageFreightRateBulkOperation
+from services.haulage_freight_rate.models.haulage_freight_rate_feedback import HaulageFreightRateFeedback
+from services.haulage_freight_rate.models.haulage_freight_rate import HaulageFreightRate
 def create_table():
     # db.connect()
     try:
-        db.create_tables([HaulageFreightRateAudit, HaulageFreightRateRequest, HaulageFreightRateBulkOperation])
+        db.create_tables([HaulageFreightRateAudit, HaulageFreightRateRequest, HaulageFreightRateBulkOperation, HaulageFreightRateFeedback, HaulageFreightRate])
         db.close()
         print("created table")
     except:
