5a24dfaa5a8ce501fe618e149ef34d097ea15001
work <mithil.anchan@cogoport.com>
2023-07-21 18:56:38 +0530

    changes

--- src/micro_services/maps_client.py
+++ src/micro_services/maps_client.py
@@ -14,12 +14,13 @@ class MapsApiClient:
         })
 
     def list_locations(self, data={}):
-        if APP_ENV == "production":
-            keys = ['filters', 'includes']
-            for key in keys:
-                if key in data:
-                    data[key] = json.dumps(data[key])
-            return self.client.request('GET', 'list_locations', {}, data)
+        self.client.url.set('https://api.cogoport.com/location')
+        # if APP_ENV == "production":
+        keys = ['filters', 'includes']
+        for key in keys:
+            if key in data:
+                data[key] = json.dumps(data[key])
+        return self.client.request('GET', 'list_locations', {}, data)
         return self.client.request('GET', 'list_locations', data, {})
 
     def list_location_cluster(self,data={}):
