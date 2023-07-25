2cef0d6ecb7f5914aa42d05efff735cf4d585f70
Shiv <109941139+ShivCogoport@users.noreply.github.com>
2023-07-25 19:10:47 +0530

    audit remove

--- src/services/haulage_freight_rate/interactions/create_haulage_freight_rate.py
+++ src/services/haulage_freight_rate/interactions/create_haulage_freight_rate.py
@@ -12,8 +12,6 @@ def create_audit(request, freight_id):
     audit_data["transport_modes"] = request.get("transport_modes")
     audit_data["haulage_freight_rate_request_id"] = request.get("haulage_freight_rate_request_id")
     audit_data["performed_by_id"] = request.get("performed_by_id")
-    audit_data["procured_by_id"] = request.get("procured_by_id")
-    audit_data["sourced_by_id"] = request.get("sourced_by_id")
 
     if 'trailer' in request.get("transport_modes"):
         object_type="TrailerFreightRate"
@@ -27,9 +25,7 @@ def create_audit(request, freight_id):
         data=audit_data,
         object_id=freight_id,
         object_type=object_type, 
-        source=request.get("source"),
-        sourced_by_id = request.get('sourced_by_id'),
-        procured_by_id = request.get('procured_by_id')
+        source=request.get("source")
     )
     return audit_id
 
