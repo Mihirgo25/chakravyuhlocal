e203d3b4dc028ec168dcbee52e2cc47f7b55625f
abhishek <abhishek.anand@cogoport.com>
2023-07-29 12:38:09 +0530

    low to critical

--- src/services/air_freight_rate/interactions/delete_air_freight_rate_feedback.py
+++ src/services/air_freight_rate/interactions/delete_air_freight_rate_feedback.py
@@ -35,7 +35,7 @@ def execute_transaction_code(request):
         update_multiple_service_objects.apply_async(kwargs={'object':obj},queue='low') 
          
         if obj.source =='spot_search' and obj.performed_by_type =='user':
-         send_closed_notifications_to_user_feedback.apply_async(kwargs={'object':obj},queue='low') 
+         send_closed_notifications_to_user_feedback.apply_async(kwargs={'object':obj},queue="critical") 
 
         else:
           send_closed_notifications_to_sales_agent_feedback.apply_async(kwargs={'object':obj},queue='low')    
