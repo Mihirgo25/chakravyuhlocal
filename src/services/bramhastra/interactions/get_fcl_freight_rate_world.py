44c89c1f43a39680cc1db5be2713321323138b8f
samir-cogoport <samir.dupare@cogoport.com>
2023-08-01 21:30:13 +0530

    d

--- src/services/bramhastra/interactions/get_fcl_freight_rate_world.py
+++ src/services/bramhastra/interactions/get_fcl_freight_rate_world.py
@@ -9,14 +9,9 @@ from collections import defaultdict
 import time
 
 
-async def get_fcl_freight_rate_world():
-    past_statistics = await get_past_count()
-    live_statistics = await get_live_count()
+def get_fcl_freight_rate_world():
+    statistics = get_past_count()  
     
-    statistics = merge(past_statistics, live_statistics)
-
-    add_location_objects(statistics)
-
     return {"total_rates": sum(statistic["rate_count"] for statistic in statistics), "statistics": statistics}
 
 
@@ -41,53 +36,17 @@ def add_location_objects(statistics):
         statistic["country_name"] = locations[statistic["country_id"]]["name"]
 
 
-async def get_past_count():
+def get_past_count():
     clickhouse = ClickHouse()
 
-    query = """SELECT country_id,COUNT(*) AS rate_count FROM 
-    (SELECT origin_country_id AS country_id
+    query = """SELECT country_id, dictGet('country_rate_count', 'rate_count', country_id) + SUM(sign)/2 AS rate_count FROM 
+    (SELECT sign, origin_country_id AS country_id
     FROM brahmastra.fcl_freight_rate_statistics
     UNION ALL
-    SELECT destination_country_id AS country_id
+    SELECT sign, destination_country_id AS country_id
     FROM brahmastra.fcl_freight_rate_statistics) AS combined_countries
     GROUP BY country_id"""
 
     return jsonable_encoder(clickhouse.execute(query))
 
 
-async def get_live_count():
-    subquery = (
-        FclFreightRateStatistic.select(
-            FclFreightRateStatistic.origin_country_id.alias("country_id")
-        )
-        .union_all(
-            FclFreightRateStatistic.select(
-                FclFreightRateStatistic.destination_country_id.alias("country_id")
-            )
-        )
-        .alias("combined_countries")
-    )
-
-    return jsonable_encoder(
-        list(
-            FclFreightRateStatistic.select(
-                subquery.c.country_id,
-                fn.COUNT(subquery.c.country_id).alias("rate_count"),
-            )
-            .from_(subquery)
-            .group_by(subquery.c.country_id)
-            .dicts()
-        )
-    )
-
-
-def merge(past_statistics, live_statistics):
-    merged_dict = defaultdict(int)
-
-    for item in past_statistics:
-        merged_dict[item['country_id']] += item['rate_count']
-
-    for item in live_statistics:
-        merged_dict[item['country_id']] += item['rate_count']
-
-    return [{'country_id': k, 'rate_count': v} for k, v in merged_dict.items()]
