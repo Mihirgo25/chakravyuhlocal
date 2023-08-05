fe2f80190adfdad19503fd7a8a61a2e076e7fe21
unknown <azharhussain161999@gmail.com>
2023-08-05 11:03:06 +0530

    rate_sheet_converted file testing

---
+++ src/services/ftl_freight_rate/helpers/get_multiple_service_objects.py
@@ -0,0 +1,75 @@
+from micro_services.client import *
+from database.rails_db import get_organization,get_operators,get_user
+
+def get_multiple_service_objects(freight_object):
+    if hasattr(freight_object,'shipping_line_id') and freight_object.shipping_line_id: 
+        shipping_line = get_operators(id=str(freight_object.shipping_line_id))
+        if len(shipping_line or []) > 0:
+            try:
+                freight_object.shipping_line = shipping_line[0]
+            except:
+                freight_object.shipping_line_detail = shipping_line[0]
+
+    if hasattr(freight_object,'airline_id') and freight_object.airline_id: 
+        airline = get_operators(id=str(freight_object.airline_id))
+        if len(airline or []) > 0:
+            try:
+                freight_object.airline = airline[0]
+            except:
+                freight_object.airline_detail = airline[0]
+    
+        
+            
+    user_list =[]
+    if hasattr(freight_object,'procured_by_id') and freight_object.procured_by_id:
+        user_list.append(freight_object.procured_by_id)
+    if hasattr(freight_object,'sourced_by_id') and freight_object.sourced_by_id:
+        user_list.append(freight_object.sourced_by_id)
+    if hasattr(freight_object,'performed_by_id') and freight_object.performed_by_id:
+        user_list.append(freight_object.performed_by_id)
+    if hasattr(freight_object,'closed_by_id') and freight_object.closed_by_id:
+        user_list.append(freight_object.closed_by_id)
+    if hasattr(freight_object,'completed_by_id'):
+        user_list.append(freight_object.completed_by_id)
+
+    if user_list:
+        user_data = get_user(user_list)
+        for user in user_data:
+            if hasattr(freight_object,'procured_by_id') and user['id']==str(freight_object.procured_by_id):
+                freight_object.procured_by= user
+            if hasattr(freight_object,'sourced_by_id') and user['id']== str(freight_object.sourced_by_id):
+                freight_object.sourced_by= user
+            if hasattr(freight_object,'performed_by_id') and user['id']==str(freight_object.performed_by_id):
+                freight_object.performed_by = user
+            if hasattr(freight_object,'closed_by_id') and user['id']==str(freight_object.closed_by_id):
+                freight_object.closed_by = user
+            if hasattr(freight_object,'completed_by_id') and user['id']==str(freight_object.completed_by_id):
+                freight_object.completed_by = user
+    organization_list=[]
+    if hasattr(freight_object,'importer_exporter_id') and freight_object.importer_exporter_id:
+        organization_list.append(freight_object.importer_exporter_id)
+    if hasattr(freight_object,'service_provider_id') and freight_object.service_provider_id:
+        organization_list.append(freight_object.service_provider_id)
+    if hasattr(freight_object,'performed_by_org_id') and freight_object.performed_by_org_id:
+        organization_list.append(freight_object.performed_by_org_id)
+    if organization_list:
+        organization_data = get_organization(id=organization_list)
+        for organization in organization_data:
+            if hasattr(freight_object,'service_provider_id') and organization['id']==str(freight_object.service_provider_id):
+                freight_object.service_provider = organization
+            if hasattr(freight_object,'importer_exporter_id') and organization['id']==str(freight_object.importer_exporter_id):
+                freight_object.importer_exporter= organization
+            if hasattr(freight_object,'performed_by_org_id') and organization['id']==str(freight_object.performed_by_org_id):
+                try:
+                    freight_object.organization = organization
+                except:
+                    freight_object.performed_by_org = organization
+
+    # if hasattr(freight_object,'rate_sheet_id'):
+    #     rate_sheet_data = RateSheet.select(RateSheet.serial_id,RateSheet.file_name,RateSheet.created_at,RateSheet.updated_at).dicts().get()
+    #     rate_sheet_data['serial_id'] = str(rate_sheet_data['serial_id'])
+    #     freight_object.rate_sheet = rate_sheet_data
+
+
+    freight_object.save()
+
---
+++ src/services/rate_sheet/interactions/ftl_rate_sheet_converted_files.py
@@ -0,0 +1,239 @@
+import os, csv
+from services.rate_sheet.models.rate_sheet_audits import RateSheetAudit
+from micro_services.client import *
+from services.rate_sheet.interactions.upload_file import upload_media_file
+from services.rate_sheet.interactions.validate_ftl_freight_object import (
+    validate_ftl_freight_object,
+)
+from libs.json_encoder import json_encoder
+from services.rate_sheet.helpers import *
+import chardet
+from libs.parse_numeric import parse_numeric
+
+
+def process_ftl_freight_freight(params, converted_file, update):
+    valid_headers = ['origin_location', 'destination_location', 'truck_type', 'commodity', 'code', 'unit', 'price',
+                'currency', 'truck_body_type', 'validity_start', 'validity_end', 'transit_time', 'detention_free_time',
+                  'trip_type', 'remark1', 'remark2', 'remark3']
+    
+    total_lines = 0
+    original_path = get_original_file_path(converted_file)
+    rows = []
+    params["rate_sheet_id"] = params["id"]
+    rate_sheet = RateSheetAudit.get(
+        (RateSheetAudit.object_id == params["rate_sheet_id"])
+        & (RateSheetAudit.action_name == "update")
+    )
+    rate_sheet = json_encoder(rate_sheet)["__data__"]
+    created_by_id = rate_sheet["performed_by_id"]
+    procured_by_id = rate_sheet["procured_by_id"]
+    sourced_by_id = rate_sheet["sourced_by_id"]
+
+    index = -1
+    file_path = original_path
+    edit_file = open(get_file_path(converted_file), "w")
+    last_row = []
+    invalidated = False
+    with open(file_path, "rb") as f:
+        result = chardet.detect(f.read())
+        encoding = result["encoding"]
+
+    with open(original_path, mode="rt", encoding=encoding) as file:
+        # Read converted file for porcessing
+        csv_writer = csv.writer(edit_file)
+        input_file = csv.DictReader(file)
+        headers = input_file.fieldnames
+        if len(set(valid_headers) & set(headers)) != len(valid_headers):
+            error_file = ["invalid header"]
+            csv_writer.writerow(error_file)
+            invalidated = True
+
+        for row in input_file:
+            total_lines += 1
+        # Set Initial Rate Sheets count
+        print('here')
+        set_total_line(converted_file, total_lines)
+        print('not here')
+        set_processed_percent(0, converted_file)
+        csv_writer.writerow(headers)
+        file.seek(0)
+        next(file)
+        is_previous_rate_valid = True
+        for row in input_file:
+            if invalidated:
+                break
+            index += 1
+            if not ''.join([str(value) for value in row.values() if value is not None]).strip():
+                continue
+            for k, v in row.items():
+                if v == "":
+                    row[k] = None
+            
+            is_main_rate_row = False
+            if row["origin_location"]:
+                is_main_rate_row = True
+            if valid_hash(row, ["origin_location", "destination_location","truck_type", "code", "unit", "price", "currency", "truck_body_type", "validity_start", "validity_end", "transit_time", "detention_free_time", "trip_type"], ["lower_limit", "upper_limit"]):
+                if rows:
+                    last_row = list(row.values())
+                    # Create previous rate if previous rate was valid
+                    if is_previous_rate_valid:
+                        create_ftl_freight_freight_rate(
+                            params,
+                            converted_file,
+                            rows,
+                            created_by_id,
+                            procured_by_id,
+                            sourced_by_id,
+                            csv_writer,
+                            last_row,
+                        )
+                    else:
+                        is_previous_rate_valid = True
+                    # Set Processing percent
+                    set_current_processing_line(index - 1, converted_file)
+                    percent = (
+                        get_current_processing_line(converted_file) / total_lines
+                    ) * 100
+                    set_processed_percent(percent, params)
+                else:
+                    list_opt = list(row.values())
+                    csv_writer.writerow(list_opt)
+                rows = [row]
+
+            elif valid_hash(row, ["code", "unit", "price", "currency"], ["origin_location", "destination_location", "truck_type", "truck_body_type", "commodity", "validity_start", "validity_end", "transit_time", "detention_free_time", "trip_type"]):
+                rows.append(row)
+                list_opt = list(row.values())
+                csv_writer.writerow(list_opt)
+            else:
+                list_opt = []
+                if rows and is_previous_rate_valid and is_main_rate_row:
+                    last_row = list(row.values())
+                    create_ftl_freight_freight_rate(
+                        params,
+                        converted_file,
+                        rows,
+                        created_by_id,
+                        procured_by_id,
+                        sourced_by_id,
+                        csv_writer,
+                        last_row,
+                    )
+                    set_current_processing_line(index - 1, converted_file)
+                    percent = (
+                        get_current_processing_line(converted_file) / total_lines
+                    ) * 100
+                    set_processed_percent(percent, params)
+                else:
+                    list_opt = list(row.values())
+                list_opt.append("Invalid Row")
+                csv_writer.writerow(list_opt)
+                if is_previous_rate_valid:
+                    converted_file["rates_count"] += 1
+                is_previous_rate_valid = False
+                rows = []
+    if rows and is_previous_rate_valid and not invalidated:
+        create_ftl_freight_freight_rate(
+            params,
+            converted_file,
+            rows,
+            created_by_id,
+            procured_by_id,
+            sourced_by_id,
+            csv_writer,
+            "",
+        )
+    set_current_processing_line(total_lines, converted_file)
+    try:
+        valid = converted_file.get("valid_rates_count")
+        total = converted_file.get("rates_count")
+        percent_completed = (valid / total) * 100
+    except:
+        valid = 0
+        total = 0
+        percent_completed = 0
+    percent = (get_current_processing_line(converted_file) / total_lines) * 100
+    converted_file["percent"] = percent_completed
+    edit_file.flush()
+    converted_file["file_url"] = upload_media_file(get_file_path(converted_file))
+    edit_file.close()
+    if valid == total and total != 0:
+        update.status = "complete"
+        converted_file["status"] = "complete"
+    elif valid == 0:
+        update.status = "uploaded"
+        converted_file["status"] = "invalidated"
+    else:
+        update.status = "partially_complete"
+        converted_file["status"] = "partially_complete"
+
+    set_processed_percent(percent_completed, params)
+    try:
+        os.remove(get_original_file_path(converted_file))
+        os.remove(get_file_path(converted_file))
+    except:
+        return
+
+
+def create_ftl_freight_freight_rate(
+    params,
+    converted_file,
+    rows,
+    created_by_id,
+    procured_by_id,
+    sourced_by_id,
+    csv_writer,
+    last_row,
+):
+    from src.celery_worker import create_ftl_freight_rate_delay
+    keys_to_extract =  ["origin_location", "destination_location", "commodity", "transit_time", "detention_free_time", "validity_start", "validity_end", "trip_type", "truck_type", "truck_body_type"]
+    object = dict(filter(lambda item: item[0] in keys_to_extract, rows[0].items()))
+
+    object["origin_location_id"] = get_location_id(rows[0]['origin_location'])
+    object["destination_location_id"] = get_location_id(rows[0]['destination_location'])
+
+    object["line_items"] = []
+
+    for data in rows:
+        keys_to_extract = ['code', 'unit', 'price', 'currency']
+        line_item = dict(filter(lambda item: item[0] in keys_to_extract, data.items()))
+        line_item['remarks'] = list(set([data["remark1"], data["remark2"], data["remark3"]]))
+        object['line_items'].append(line_item)
+
+    object["rate_sheet_id"] = params["id"]
+    object["performed_by_id"] = created_by_id
+    object["service_provider_id"] = params.get("service_provider_id")
+    object["procured_by_id"] = procured_by_id
+    object["sourced_by_id"] = sourced_by_id
+
+    request_params = object
+    validation = write_ftl_freight_freight_object(
+        request_params, csv_writer, params, converted_file, last_row
+    )
+    if validation.get("valid"):
+        object["rate_sheet_validation"] = True
+        create_ftl_freight_rate_delay.apply_async(
+            kwargs={"request": object}, queue="low"
+        )
+    else:
+        print(validation.get("error"))
+
+
+def write_ftl_freight_freight_object(rows, csv, params, converted_file, last_row):
+    object_validity = validate_ftl_freight_object(converted_file.get("module"), rows)
+    if object_validity["valid"]:
+        csv.writerow(last_row)
+        converted_file["valid_rates_count"] = (
+            int(converted_file["valid_rates_count"]) + 1
+        )
+    else:
+        try:
+            error = ["".join(str(object_validity.get("error")))]
+            list_opt = error
+            csv.writerow(list_opt)
+            csv.writerow([])
+            csv.writerow(last_row)
+        except:
+            print("no csv")
+    converted_file["rates_count"] = int(converted_file["rates_count"]) + 1
+    return object_validity
+
