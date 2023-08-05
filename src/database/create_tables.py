641761ac90a774721e17dfe2a9531462d3a6ca6f
Saksham5730 <saksham.sharma@cogoport.com>
2023-08-05 17:40:08 +0530

    fix:

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -3,11 +3,7 @@ from services.fcl_freight_rate.models.fcl_freight_location_cluster import FclFre
 from services.fcl_freight_rate.models.fcl_freight_location_cluster_factor import FclFreightLocationClusterFactor
 from services.fcl_freight_rate.models.fcl_freight_location_cluster_mapping import FclFreightLocationClusterMapping
 from services.air_freight_rate.models.air_freight_airline_factor import AirFreightAirlineFactor
-from services.haulage_freight_rate.models.haulage_freight_rate import HaulageFreightRate
 from services.haulage_freight_rate.models.haulage_freight_rate_audit import HaulageFreightRateAudit
-from services.haulage_freight_rate.models.haulage_freight_rate_bulk_operation import HaulageFreightRateBulkOperation
-from services.haulage_freight_rate.models.haulage_freight_rate_feedback import HaulageFreightRateFeedback
-from services.haulage_freight_rate.models.haulage_freight_rate_request import HaulageFreightRateRequest
 def create_table():
     # db.connect()
     try:
