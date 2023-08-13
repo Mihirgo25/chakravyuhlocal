e81b6bdf44712467d0744d846161b96b3e578b80
yash2001cp <yash.suryawanshi@cogoport.com>
2023-08-14 03:04:27 +0530

    done

---
+++ src/services/bramhastra/click.sh
@@ -0,0 +1,11 @@
+sudo apt-get install -y apt-transport-https ca-certificates dirmngr
+GNUPGHOME=$(mktemp -d)
+sudo GNUPGHOME="$GNUPGHOME" gpg --no-default-keyring --keyring /usr/share/keyrings/clickhouse-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8919F6BD2B48D754
+sudo rm -r "$GNUPGHOME"
+sudo chmod +r /usr/share/keyrings/clickhouse-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main" | sudo tee \
+    /etc/apt/sources.list.d/clickhouse.list
+sudo apt-get update
+
+sudo apt-get install -y clickhouse-server clickhouse-client
