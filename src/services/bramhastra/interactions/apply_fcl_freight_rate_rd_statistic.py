44523b778711ddf9b9a4d2239bb700dbb5e465d7
yash2001cp <yash.suryawanshi@cogoport.com>
2023-08-16 20:20:14 +0530

    df

--- src/services/bramhastra/interactions/apply_fcl_freight_rate_rd_statistic.py
+++ src/services/bramhastra/interactions/apply_fcl_freight_rate_rd_statistic.py
@@ -4,21 +4,20 @@ from configs.env import APP_ENV
 
 
 def apply_fcl_freight_rate_rd_statistic(request):
-    if APP_ENV != "production":
-        return
-    revenue_desk = RevenueDesk(request)
-    if (
-        not getattr(request, "selected_for_booking")
-        and request.action == RDAction.create.value
-    ):
-        revenue_desk.update_rd_visit_count(request)
-        return
+    if APP_ENV == "production":
+        revenue_desk = RevenueDesk(request)
+        if (
+            not getattr(request, "selected_for_booking")
+            and request.action == RDAction.create.value
+        ):
+            revenue_desk.update_rd_visit_count(request)
+            return
 
-    if request.action == RDAction.update.value and getattr(
-        request, "selected_for_booking"
-    ):
-        revenue_desk.set_rate_stats()
-    elif request.action == RDAction.update.value and getattr(
-        request, "selected_for_preference"
-    ):
-        revenue_desk.update_selected_for_preference_count(request)
+        if request.action == RDAction.update.value and getattr(
+            request, "selected_for_booking"
+        ):
+            revenue_desk.set_rate_stats()
+        elif request.action == RDAction.update.value and getattr(
+            request, "selected_for_preference"
+        ):
+            revenue_desk.update_selected_for_preference_count(request)
