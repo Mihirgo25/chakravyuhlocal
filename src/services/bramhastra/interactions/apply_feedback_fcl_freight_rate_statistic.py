ba5b7dc96fa3362ed8dc62f6d296c94a527da3eb
yash2001cp <yash.suryawanshi@cogoport.com>
2023-08-21 00:57:31 +0530

    done

--- src/services/bramhastra/interactions/apply_feedback_fcl_freight_rate_statistic.py
+++ src/services/bramhastra/interactions/apply_feedback_fcl_freight_rate_statistic.py
@@ -12,8 +12,8 @@ def apply_feedback_fcl_freight_rate_statistic(request):
 def execute_transaction_code(request):
     if APP_ENV == "production":
         feedback = Feedback(action=request.action, params=request.params)
+        feedback.set_format_and_existing_rate_stats()
         if request.action == FeedbackAction.create.value:
-            feedback.set_format_and_existing_rate_stats()
             feedback.set_new_stats()
         elif (
             request.action == FeedbackAction.update.value
