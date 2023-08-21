b417c04ce51eb20e4bd940bcb84e18ff4a688211
yash2001cp <yash.suryawanshi@cogoport.com>
2023-08-21 19:06:42 +0530

    done with exrend attributer

--- src/services/bramhastra/workers/fcl_daily_attribute_updater_worker.py
+++ src/services/bramhastra/workers/fcl_daily_attribute_updater_worker.py
@@ -4,6 +4,8 @@ from services.bramhastra.client import ClickHouse
 from services.bramhastra.models.fcl_freight_rate_statistic import (
     FclFreightRateStatistic,
 )
+from fastapi.encoders import jsonable_encoder
+from playhouse.postgres_ext import ServerSide
 
 
 class FclDailyAttributeUpdaterWorker:
@@ -11,21 +13,36 @@ class FclDailyAttributeUpdaterWorker:
         self.clickhouse = ClickHouse()
 
     def execute(self):
-        if fcl_freight_rate_audits := list(
-            FclFreightRateAudit.select(
-                FclFreightRateAudit.object_id.alias("rate_id"),
-                FclFreightRateAudit.extended_from_object_id.alias("parent_rate_id"),
-                FclFreightRateAudit.rate_sheet_id,
-                FclFreightRateAudit.bulk_operation_id
+        keys = {"parent_rate_id", "rate_sheet_id", "bulk_operation_id"}
+
+        query = FclFreightRateAudit.select(
+            FclFreightRateAudit.object_id.alias("rate_id"),
+            FclFreightRateAudit.extended_from_object_id.alias("parent_rate_id"),
+            FclFreightRateAudit.rate_sheet_id,
+            FclFreightRateAudit.bulk_operation_id,
+        ).where(
+            FclFreightRateAudit.created_at > datetime.utcnow() - timedelta(hours=27),
+            FclFreightRateAudit.object_type == "FclFreightRate",
+            FclFreightRateAudit.action_name == "create",
+        )
+        for fcl_freight_rate_audit in ServerSide(query):
+            query = [
+                "ALTER TABLE brahmastra.{FclFreightRateStatistic._meta.table_name}"
+            ]
+
+            audit = dict(rate_id=fcl_freight_rate_audit.rate_id)
+            for key in keys:
+                update = []
+                if getattr(fcl_freight_rate_audit, key):
+                    update.append(f"{key} = %({key})s")
+                    audit[key] = getattr(fcl_freight_rate_audit, key)
+            if len(audit) == 1:
+                continue
+
+            query.append("UPDATE")
+
+            query.append(",".join(update))
+            query.append(
+                "WHERE (id,version) IN (SELECT id, MAX(version) AS max_version FROM brahmastra.{FclFreightRateStatistic._meta.table_name} WHERE rate_id = %(rate_id)s GROUP BY id)"
             )
-            .where(
-                FclFreightRateAudit.created_at
-                > datetime.utcnow() - timedelta(hours=27),
-                FclFreightRateAudit.object_type == "FclFreightRate",
-                FclFreightRateAudit.action_name == "create",
-            )
-            .dicts()
-        ):
-            for audit in fcl_freight_rate_audits:
-                queries = f"ALTER TABLE brahmastra.{FclFreightRateStatistic._meta.table_name} UPDATE parent_rate_id = %(parent_rate_id)s WHERE (id,version) IN (SELECT id, MAX(version) AS max_version FROM brahmastra.{FclFreightRateStatistic._meta.table_name} WHERE rate_id = %(rate_id)s GROUP BY id)"
-                self.clickhouse.execute(" ".join(queries), audit)
+            self.clickhouse.execute(f" ".join(query), jsonable_encoder(audit))
