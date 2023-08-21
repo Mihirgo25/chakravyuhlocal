04fc1872183912a3e2df02d4cb36ecc60f5632c8
yash2001cp <yash.suryawanshi@cogoport.com>
2023-08-21 21:55:41 +0530

    jh

--- src/database/migrations/018_migration_1692617520.py
+++ src/database/migrations/018_migration_1692617520.py
@@ -2,7 +2,6 @@ import peewee as pw
 from peewee_migrate import Migrator
 from services.bramhastra.models.fcl_freight_rate_statistic import FclFreightRateStatistic
 from peewee import UUIDField,CharField
-from database.create_clicks import Clicks
 
 SQL = pw.SQL
 
@@ -13,9 +12,6 @@ def migrate(migrator: Migrator, database, fake=False, **kwargs):
     migrator.add_fields(FclFreightRateStatistic, performed_by_type = CharField(null=True))
     migrator.add_fields(FclFreightRateStatistic, rate_sheet_id= UUIDField(null=True))
     migrator.add_fields(FclFreightRateStatistic, bulk_operation_id = UUIDField(null=True))
-    models = [FclFreightRateStatistic]
-    Clicks(models=models).delete()
-    Clicks(models=models,ignore_oltp=True).create()
     
 
 
