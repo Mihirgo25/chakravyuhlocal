ac63737762523b55358c39765c5dbbe706dc7f0a
bedadha-Ajay <bedada.kumar@cogoport.com>
2023-08-24 21:13:19 +0530

    change

--- src/services/air_freight_rate/models/air_freight_rate_surcharge.py
+++ src/services/air_freight_rate/models/air_freight_rate_surcharge.py
@@ -61,11 +61,8 @@ class AirFreightRateSurcharge(BaseModel):
     def detail(self):
         air_freight_surcharges_dict = AIR_FREIGHT_SURCHARGES
         new_line_items = []
-        kolkata = False
-        if str(self.origin_airport_id) == KOLKATA:
-            kolkata = True
         not_required_charges = ['EAMS','EHAMS','HAMS']
-        if kolkata and str(self.airline_id) in EAMS_AIRLINES:
+        if str(self.origin_airport_id) == KOLKATA and str(self.airline_id) in EAMS_AIRLINES:
             not_required_charges = ['AMS','EHAMS','HAMS']
         for line_item in self.line_items:
             if line_item['code'] in not_required_charges:
