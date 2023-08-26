0c7efe8669dccd5cc36f0a13e9e4dade4d3b20f9
samir-cogoport <samir.dupare@cogoport.com>
2023-08-26 13:57:01 +0530

    crontab changes

--- src/celery_worker.py
+++ src/celery_worker.py
@@ -100,7 +100,7 @@ celery.conf.update(**CELERY_CONFIG)
 celery.conf.beat_schedule = {
     'fcl_freigh_rates_to_cogo_assured': {
         'task': 'celery_worker.fcl_freight_rates_to_cogo_assured',
-        'schedule': crontab(minute=30,hour=18),
+        'schedule': crontab(hour='*/2'),
         'options': {'queue' : 'fcl_freight_rate'}
         },
     # 'update_cogo_assured_fcl_freight_rates': {
@@ -440,12 +440,20 @@ def validate_and_process_rate_sheet_converted_file_delay(self, request):
 @celery.task(bind = True, retry_backoff=True,max_retries=1)
 def fcl_freight_rates_to_cogo_assured(self):
     try:
-        from services.fcl_freight_rate.models.cogo_assured_updation_time import CogoAssuredUpdationTime
+        from services.fcl_freight_rate.models.worker_time_stamp import WorkerTimeStamp
+        
+        updation_time = WorkerTimeStamp.select().first()
+        
+        if not updation_time:
+            last_updated_at = datetime.utcnow() - timedelta(hours=24)
+            WorkerTimeStamp.create()
+        else:
+            last_updated_at = updation_time.last_updated_at
+            updation_time.save()
 
-
-        query = FclFreightRate.select(FclFreightRate.origin_port_id, FclFreightRate.origin_main_port_id, FclFreightRate.destination_port_id, FclFreightRate.destination_main_port_id, FclFreightRate.container_size, FclFreightRate.container_type, FclFreightRate.commodity).where(FclFreightRate.mode.not_in(['predicted', 'cluster_extension']), FclFreightRate.updated_at >= datetime.now()-timedelta(hours = 2), FclFreightRate.validities != '[]', ~FclFreightRate.rate_not_available_entry, FclFreightRate.container_size << ['20', '40', '40HC'], FclFreightRate.rate_type == DEFAULT_RATE_TYPE)
-
+        query = FclFreightRate.select(FclFreightRate.origin_port_id, FclFreightRate.origin_main_port_id, FclFreightRate.destination_port_id, FclFreightRate.destination_main_port_id, FclFreightRate.container_size, FclFreightRate.container_type, FclFreightRate.commodity).where(FclFreightRate.mode.not_in(['predicted', 'cluster_extension']), FclFreightRate.updated_at >= last_updated_at, FclFreightRate.validities != '[]', ~FclFreightRate.rate_not_available_entry, FclFreightRate.container_size << ['20', '40', '40HC'], FclFreightRate.rate_type == DEFAULT_RATE_TYPE)   
         grouped_set = set()
+
         for rate in ServerSide(query):
             grouped_set.add(f'{str(rate.origin_port_id)}:{str(rate.origin_main_port_id or "")}:{str(rate.destination_port_id)}:{str(rate.destination_main_port_id or "")}:{str(rate.container_size)}:{str(rate.container_type)}:{str(rate.commodity)}')
 
