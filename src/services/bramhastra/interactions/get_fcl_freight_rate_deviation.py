8e4091dfb9a59282e68628316b92ad10c79036da
yash2001cp <yash.suryawanshi@cogoport.com>
2023-08-30 16:42:20 +0530

    done with deviation

--- src/services/bramhastra/interactions/get_fcl_freight_rate_deviation.py
+++ src/services/bramhastra/interactions/get_fcl_freight_rate_deviation.py
@@ -5,14 +5,21 @@ from services.bramhastra.helpers.fcl_freight_filter_helper import (
 from fastapi.encoders import jsonable_encoder
 from math import ceil
 from micro_services.client import maps
+from services.bramhastra.models.fcl_freight_rate_statistic import (
+    FclFreightRateStatistic,
+)
 
-GROUPING_KEYS = {
+SELECT = {
     "origin_port_id",
     "origin_region_id",
     "origin_continent_id",
     "origin_trade_id",
+    "destination_port_id",
+    "destination_region_id",
+    "destination_continent_id",
+    "destination_trade_id",
     "service_provider_id",
-    "shipping_line_id"
+    "shipping_line_id",
 }
 
 LOCATION_KEYS = {
@@ -26,33 +33,47 @@ LOCATION_KEYS = {
     "destination_trade_id",
 }
 
+DEFAULT_SELECT_KEYS = {"service_provider_id"}
 
-def get_fcl_freight_deviation(filters,sort_by,sort_type, page_limit, page):
+
+def get_fcl_freight_deviation(filters, page_limit, page):
     clickhouse = ClickHouse()
 
     grouping = set()
-    
-    for k in GROUPING_KEYS:
+
+    for k in SELECT:
         if k in filters:
             grouping.add(k)
 
+    if not grouping:
+        grouping = DEFAULT_SELECT_KEYS.copy()
+
+    subquery = [
+        f"WITH AVERAGE AS (SELECT AVG(standard_price) AS average_price,count(validity_id) AS validity_count FROM brahmastra.fcl_freight_rate_statistics"
+    ]
+
     queries = [
-        f'SELECT {",".join(grouping)},MAX(standard_price) AS max_price,MIN(standard_price) as min_price,MAX(standard_price) - MIN(standard_price) as difference FROM brahmastra.fcl_freight_rate_statistics'
+        f',NORMAL AS (SELECT {",".join(grouping)},AVG(standard_price) as price FROM brahmastra.{FclFreightRateStatistic._meta.table_name}'
+    ]
+
+    final_query = [
+        f"SELECT {','.join(grouping)}, SQRT(POW(price - AVERAGE.average_price,2)/AVERAGE.validity_count + 1) AS deviation FROM NORMAL,AVERAGE"
     ]
 
     if where := get_direct_indirect_filters(filters):
+        subquery.append(" WHERE ")
+        subquery.append(where)
         queries.append(" WHERE ")
         queries.append(where)
 
-    get_add_group_and_order_by(queries, grouping,sort_by,sort_type)
-    
+    get_add_group_and_order_by(final_query, subquery, queries, grouping)
+
+    queries = subquery + queries + final_query
+
     total_count, total_pages = add_pagination_data(
         clickhouse, queries, filters, page, page_limit
     )
     statistics = jsonable_encoder(clickhouse.execute(" ".join(queries), filters))
-
-    if statistics:
-        add_location_objects(statistics)
 
     return dict(
         list=statistics,
@@ -63,12 +84,17 @@ def get_fcl_freight_deviation(filters,sort_by,sort_type, page_limit, page):
     )
 
 
-def get_add_group_and_order_by(queries, grouping,sort_by,sort_type):
+def get_add_group_and_order_by(final_query, subquery, queries, grouping):
     queries.append("GROUP BY")
     queries.append(",".join(grouping))
-    queries.append(
-        f"ORDER BY {sort_by} {sort_type}"
-    )
+    queries.append(")")
+    grouping.remove("service_provider_id")
+    if grouping:
+        subquery.append("GROUP BY")
+        subquery.append(",".join(grouping))
+    subquery.append(")")
+
+    final_query.append(f"ORDER BY deviation ASC")
 
 
 def add_pagination_data(clickhouse, queries, filters, page, page_limit):
@@ -81,47 +107,3 @@ def add_pagination_data(clickhouse, queries, filters, page, page_limit):
     total_pages = ceil(total_count / page_limit)
 
     return total_count, total_pages
-
-
-def add_location_objects(statistics):
-    location_ids = list(
-        {
-            v
-            for statistic in statistics
-            for k, v in statistic.items()
-            if k in LOCATION_KEYS
-        }
-    )
-
-    if not location_ids:
-        return
-
-    locations = {
-        location["id"]: location
-        for location in maps.list_locations(
-            dict(
-                filters=dict(id=location_ids),
-                includes=dict(
-                    id=True, name=True, type=True, latitude=True, longitude=True
-                ),
-                page_limit=len(location_ids),
-            )
-        )["list"]
-    }
-
-    for statistic in statistics:
-        update_statistic = dict()
-        remove = None
-        if not statistic:
-            continue
-        for k, v in statistic.items():
-            if k in LOCATION_KEYS:
-                remove = k
-                location = locations.get(v)
-                if location is None:
-                    continue
-                for key, value in location.items():
-                    update_statistic[f"{k[:12]}{key}"] = value
-        statistic.pop(remove)
-        statistic.update(update_statistic)
-    
