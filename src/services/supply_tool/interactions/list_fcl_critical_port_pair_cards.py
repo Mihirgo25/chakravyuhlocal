658fd28aebb2787f247b1cd8c403ac7817a05c82
Shiv <109941139+ShivCogoport@users.noreply.github.com>
2023-08-30 22:58:25 +0530

    fix:

---
+++ src/services/supply_tool/interactions/list_fcl_critical_port_pair_cards.py
@@ -0,0 +1,4 @@
+from services.supply_tool.models.fcl_freight_rate_jobs import FclFreightRateJobs
+
+def list_fcl_critical_port_pair_cards(user_id, origin=None, destination=None, shipping_line=None, commodity=None):
+    return
