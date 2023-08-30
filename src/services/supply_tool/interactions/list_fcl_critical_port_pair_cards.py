f10bbda1ec38f3355518df6bcbfd5c7fa4cbf865
Saksham5730 <saksham.sharma@cogoport.com>
2023-08-30 23:02:36 +0530

    feat: add

--- src/services/supply_tool/interactions/list_fcl_critical_port_pair_cards.py
+++
@@ -1,4 +0,0 @@
-from services.supply_tool.models.fcl_freight_rate_jobs import FclFreightRateJobs
-
-def list_fcl_critical_port_pair_cards(user_id, origin=None, destination=None, shipping_line=None, commodity=None):
-    return
