df2bda8d2172e8d8ef897e77d3d4209e27316b15
zimanshu-cogoport <himanshu.ghorwal@cogoport.com>
2023-08-30 17:08:02 +0530

    air_freight_rate_jobs and jobs_mapping

---
+++ src/services/air_freight_rate/models/air_freight_rate_jobs.py
@@ -0,0 +1,53 @@
+from peewee import *
+import datetime
+from database.db_session import db
+from playhouse.postgres_ext import *
+from micro_services.client import *
+from database.rails_db import *
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+        only_save_dirty = True
+
+
+class AirFreightRateJobs(BaseModel):
+    id = UUIDField(constraints=[SQL("DEFAULT gen_random_uuid()")], primary_key=True)
+    origin_airport = BinaryJSONField(null=True)
+    origin_airport_id = UUIDField(null=True, index=True)
+    destination_airport = BinaryJSONField(null=True)
+    destination_airport_id = UUIDField(null=True, index=True)
+    service_provider = BinaryJSONField(null=True)
+    service_provider_id = UUIDField(null=True, index=True)
+    airline = BinaryJSONField(null=True)
+    airline_id = UUIDField(null=True, index=True)
+    breadth = IntegerField(null=True)
+    height = IntegerField(null=True)
+    length = IntegerField(null=True)
+    commodity = CharField(null=True, index=True)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
+    status = CharField(index=True, null=True)
+    source = CharField(default = 'manual', null = True, index=True)
+    assigned_to_id = UUIDField(index=True, null=True)
+    assigned_to = BinaryJSONField(null=True)
+    closed_by_id = UUIDField(null=True, index=True)
+    closed_by = BinaryJSONField(null=True)
+    closing_remarks = TextField(null=True)    
+    shipment_type = CharField(null=True)
+    stacking_type = CharField(null=True, index=True)
+
+    class Meta:
+        table_name = 'air_freight_rate_jobs'
+    
+
+class AirFreightRateJobsMapping(BaseModel):
+    id = UUIDField(constraints=[SQL("DEFAULT gen_random_uuid()")], primary_key=True)
+    source = TextField(index=True, null=True)
+    source_id = UUIDField(index=True, null=True)
+    job_id = UUIDField(null=True, index=True)
+    created_at = DateField(default=datetime.datetime.now)
+    updated_at = DateField(default=datetime.datetime.now)
+    
+    class Meta:
+        table_name = 'air_freight_rate_jobs_mapping'
