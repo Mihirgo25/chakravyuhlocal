d70f30c478a759346d278e851a8fd6d2722255f2
yash2001cp <yash.suryawanshi@cogoport.com>
2023-08-31 15:51:58 +0530

    done

--- src/services/bramhastra/data/migrations/migration_1.py
+++ src/services/bramhastra/data/migrations/migration_1.py
@@ -62,7 +62,7 @@ from services.bramhastra.models.shipment_fcl_freight_rate_statistic import (
 from services.bramhastra.models.checkout_fcl_freight_rate_statistic import (
     CheckoutFclFreightRateStatistic,
 )
-from services.bramhastra.models.brahmastra_track import BrahmastraTrack
+from services.bramhastra.models.worker_log import WorkerLog
 from database.create_clicks import Clicks
 from database.rails_db import get_connection
 from playhouse.shortcuts import model_to_dict
@@ -1750,7 +1750,7 @@ class PopulateFclFreightRateStatistics(MigrationHelpers):
             FeedbackFclFreightRateStatistic,
             ShipmentFclFreightRateStatistic,
             CheckoutFclFreightRateStatistic,
-            BrahmastraTrack,
+            WorkerLog,
         ]
 
         Table().create_tables(models)
