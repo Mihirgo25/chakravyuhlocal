643e446afa2017dae53b47bbe4eee96dbbd1d4cd
Saksham5730 <saksham.sharma@cogoport.com>
2023-08-31 15:42:36 +0530

    fix:

--- src/services/supply_tool/interactions/create_fcl_freight_rate_jobs.py
+++ src/services/supply_tool/interactions/create_fcl_freight_rate_jobs.py
@@ -1,15 +1,14 @@
-from database.rails_db import get_most_searched_predicted_rates_for_fcl_freight_services
-from micro_services.client import partner,maps
-import copy
 from services.supply_tool.models.fcl_freight_rate_jobs import FclFreightRateJobs
 from services.supply_tool.models.fcl_freight_rate_jobs_mapping import FclFreightRateJobsMapping
 from datetime import datetime, timedelta
 from services.fcl_freight_rate.helpers.get_multiple_service_objects import get_multiple_service_objects
 from services.supply_tool.helpers.task_distribution_system import task_distribution_system
+from fastapi.encoders import jsonable_encoder
 
 
-def create_fcl_freight_rate_jobs(request):
+def create_fcl_freight_rate_jobs(request, source):
     updated_ids = []
+    request = jsonable_encoder(request)
     for data in request:
         params = {
             'origin_port_id' : data.get('origin_port_id'),
@@ -21,30 +20,28 @@ def create_fcl_freight_rate_jobs(request):
             'container_size' : data.get('container_size'),
             'container_type' : data.get('container_type'),
             'commodity' : data.get('commodity'),
-            'source' : data.get('source'),
+            'source' : source,
             'rate_type' : data.get('rate_type'),
+            'rate_id' : data.get('rate_id'),
         }
-        rate_id = data.get('rate_id')
-        if rate_id:
-            fcl_freight_rate_job = (FclFreightRateJobs.select().where(FclFreightRateJobs.rate_id == rate_id).first())
-        else:
+        conditions = [getattr(FclFreightRateJobs, key) == value for key, value in params.items() if value is not None]
+        fcl_freight_rate_job = FclFreightRateJobs.select().where(*conditions).first()
+        if not fcl_freight_rate_job:
+            fcl_freight_rate_job = FclFreightRateJobs()
             for key in list(params.keys()):
                 setattr(fcl_freight_rate_job, key, params[key])
-            
         if fcl_freight_rate_job.status == 'active':
-            updated_ids.append(fcl_freight_rate_job.id)
             continue
 
         if fcl_freight_rate_job.status == 'inactive' and fcl_freight_rate_job.updated_at > datetime.now()-timedelta(days=7):
             continue
 
-        fcl_freight_rate_job.assigned_to_id = task_distribution_system('fcl')
-
-        set_jobs_mapping(fcl_freight_rate_job.id, data)
-
+        user_id = task_distribution_system('FCL')
+        fcl_freight_rate_job.assigned_to_id = user_id
         fcl_freight_rate_job.status = 'active'
         fcl_freight_rate_job.set_locations()
         fcl_freight_rate_job.save()
+        set_jobs_mapping(fcl_freight_rate_job.id, data)
         get_multiple_service_objects(fcl_freight_rate_job)
         updated_ids.append(fcl_freight_rate_job.id)
 
