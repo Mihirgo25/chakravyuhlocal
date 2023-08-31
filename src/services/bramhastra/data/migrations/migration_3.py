fb7cbe69924a462aa6f942e3619f68d5a2137738
yash2001cp <yash.suryawanshi@cogoport.com>
2023-09-01 00:50:38 +0530

    done

--- src/services/bramhastra/data/migrations/migration_3.py
+++ src/services/bramhastra/data/migrations/migration_3.py
@@ -2,6 +2,7 @@ def main():
     from services.bramhastra.client import ClickHouse
     from services.bramhastra.models.air_freight_rate_statistic import AirFreightRateStatistic
     from services.bramhastra.models.fcl_freight_rate_statistic import FclFreightRateStatistic
+    from services.chakravyuh.models.worker_log import WorkerLog
     
     click = ClickHouse()
     
@@ -12,6 +13,8 @@ def main():
     from database.create_clicks import Clicks
     
     Clicks(models = [AirFreightRateStatistic,FclFreightRateStatistic],ignore_oltp = True).create()
+    
+    WorkerLog.delete().execute()
 
 if __name__ == "__main__":
     main()
