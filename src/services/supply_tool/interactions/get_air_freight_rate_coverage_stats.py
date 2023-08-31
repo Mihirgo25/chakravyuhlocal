881e3bba53f424b539290d59ad452918e8f86cce
zimanshu-cogoport <himanshu.ghorwal@cogoport.com>
2023-08-31 20:27:20 +0530

    get air coverage fix and his router

--- src/services/supply_tool/interactions/get_air_freight_rate_coverage_stats.py
+++ src/services/supply_tool/interactions/get_air_freight_rate_coverage_stats.py
@@ -1,54 +1,79 @@
 from services.supply_tool.models.air_freight_rate_jobs import AirFreightRateJobs
 import json
+from datetime import datetime, timedelta
 from libs.json_encoder import json_encoder
 from libs.get_applicable_filters import get_applicable_filters
 from libs.get_filters import get_filters
+from collections import Counter
 
 possible_direct_filters = ['origin_airport_id','destination_airport_id','airline_id','commodity','status']
 possible_indirect_filters = ['updated_at', 'user_id']
 
-def get_air_freight_rate_coverage_stats(filters = {}, page_limit = 10, page = 1, sort_by = 'updated_at', sort_type = 'desc'):
-    query = get_query(sort_by, sort_type)
+def get_air_freight_rate_coverage_stats(filters = {}, page_limit = 10, page = 1, sort_by = 'created_at', sort_type = 'desc'):
+    daily_query = get_daily_query(sort_by, sort_type)
+    weekly_query = get_weekly_query(sort_by, sort_type)
 
     if filters:
         if type(filters) != dict:
             filters = json.loads(filters)
-        
-        direct_filters, indirect_filters = get_applicable_filters(filters, possible_direct_filters ,possible_indirect_filters)
-        query = get_filters(direct_filters, query, AirFreightRateJobs)
-        query = apply_indirect_filters(query, indirect_filters)
 
-    statistics ={
-        'pending': 0,
-        'backlog': 0,
-        'completed': 0,
-        'completed_percentage': 0,
-        'spot_search' : 0,
-        'critical_port_pair' : 0,
-        'expiring_rates' : 0,
-        'monitoring_dashboard' : 0,
-        'cancelled_shipment' : 0,
-        'total' : 0
+        direct_filters, indirect_filters = get_applicable_filters(filters, possible_direct_filters, possible_indirect_filters)
+
+        daily_query = get_filters(direct_filters, daily_query, AirFreightRateJobs)
+        weekly_query = get_filters(direct_filters, weekly_query, AirFreightRateJobs)
+
+        daily_query = apply_indirect_filters(daily_query, indirect_filters)
+        weekly_query = apply_indirect_filters(weekly_query, indirect_filters)
+
+    statistics = {
+      'pending': 0,
+      'backlog': 0,
+      'completed': 0,
+      'completed_percentage': 0,
+      'spot_search' : 0,
+      'critical_port_pair' : 0,
+      'expiring_rates' : 0,
+      'monitoring_dashboard' : 0,
+      'cancelled_shipment' : 0,
+      'total' : 0,
+      'previous_day_0' : 100,
+      'previous_day_1' : 100,
+      'previous_day_2' : 100,
+      'previous_day_3' : 100,
+      'previous_day_4' : 100,
+      'previous_day_5' : 100,
+      'previous_day_6' : 100,
+      'weekly_backlog' : 0
     }
 
-    data = get_data(query, statistics)
-    print(data)
+    if not daily_query:
+       return statistics
 
-    return data
+    statistics = get_daily_stats_data(daily_query,statistics)
 
-def get_data(query,statistics):
-    raw_data = json_encoder(list(query.dicts()))
-    statistics['total'] = len(raw_data)
-    for item in raw_data:
-        status = item['status']
-        if status in statistics:
-           statistics[status] += 1
+    if not weekly_query:
+       return statistics
 
-        source = item['source']
-        if source in statistics:
-           statistics[source] += 1
+    statistics = get_weekly_details(weekly_query, statistics)
 
     return statistics
+
+def get_daily_query(sort_by, sort_type):
+  query = AirFreightRateJobs.select().where(
+       (AirFreightRateJobs.created_at > datetime.now()-timedelta(days=1))|
+       (AirFreightRateJobs.status == 'active')
+    )
+  if sort_by:
+      query = query.order_by(eval('AirFreightRateJobs.{}.{}()'.format(sort_by,sort_type)))
+  return query
+
+def get_weekly_query(sort_by, sort_type):
+  query = AirFreightRateJobs.select().where(
+      AirFreightRateJobs.created_at > datetime.now()-timedelta(days=7)
+    )
+  if sort_by:
+    query = query.order_by(eval('AirFreightRateJobs.{}.{}()'.format(sort_by,sort_type)))
+  return query
 
 def apply_indirect_filters(query, filters):
   for key in filters:
@@ -56,10 +81,55 @@ def apply_indirect_filters(query, filters):
     query = eval(f'{apply_filter_function}(query, filters)')
   return query
 
-def get_query(sort_by, sort_type):
-    query = AirFreightRateJobs.select()
+def get_daily_stats_data(query,statistics):
+    raw_data = json_encoder(list(query.dicts()))
+    statistics['total'] = len(raw_data)
+    for item in raw_data:
+      status = item['status']
+      created_at = item['created_at']
 
-    if sort_by:
-        query = query.order_by(eval('AirFreightRateJobs.{}.{}()'.format(sort_by,sort_type)))
-    
-    return query
+      if status == 'active':
+        if datetime.strptime(created_at,"%Y-%m-%dT%H:%M:%S.%fZ") > datetime.now()-timedelta(days=1):
+          statistics['pending'] += 1
+        else:
+          statistics['backlog'] += 1
+      else:
+        if datetime.strptime(created_at,"%Y-%m-%dT%H:%M:%S.%fZ") > datetime.now()-timedelta(days=1):
+          statistics['completed'] += 1
+
+        source = item['source']
+        if source in statistics:
+           statistics[source] += 1
+
+    return statistics
+
+def get_weekly_details(query, statistics):
+  raw_data = json_encoder(list(query.dicts()))
+  total_backlog = 0
+
+  current_date = datetime.now().date()
+
+  backlogs_per_day = Counter()
+  total_tasks_per_day = Counter()
+
+  start_date = current_date - timedelta(days=7)
+
+  for task in raw_data:
+    created_at = datetime.strptime(task['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    if start_date <= created_at.date() <= current_date:
+        days_ago = (current_date - created_at.date()).days
+        total_tasks_per_day[days_ago] += 1
+        if task['status'] == 'active':
+          backlogs_per_day[days_ago] += 1
+
+  for days_ago, count in total_tasks_per_day.items():
+    backlog_count = backlogs_per_day[days_ago]
+    total_backlog += backlog_count
+    print(f"previous_day_{days_ago}: {count} tasks, {backlog_count} backlog tasks")
+
+    if count:
+      statistics['previous_day_' + str(days_ago)] = round((1 - (backlog_count/count))*100)
+
+  statistics['weekly_backlog'] = total_backlog
+
+  return statistics
