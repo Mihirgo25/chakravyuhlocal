b693ac9ba8e6068661d77f60b437ff85b64e71dd
Shiv <109941139+ShivCogoport@users.noreply.github.com>
2023-09-02 20:40:23 +0530

    critical ports pairs fix

--- src/services/envision/schedulers/critical_port_pairs_scheduler.py
+++ src/services/envision/schedulers/critical_port_pairs_scheduler.py
@@ -5,6 +5,9 @@ from services.fcl_freight_rate.interaction.create_fcl_freight_rate_jobs import c
 from configs.fcl_freight_rate_constants import CRITICAL_PORTS_INDIA_VIETNAM, CRITICAL_AIRPORTS_INDIA_VIETNAM
 import datetime
 from playhouse.postgres_ext import ServerSide
+from services.fcl_freight_rate.models.fcl_freight_location_cluster import FclFreightLocationCluster
+from services.air_freight_rate.models.air_freight_location_cluster import AirFreightLocationCluster
+from fastapi.encoders import jsonable_encoder
 from playhouse.shortcuts import model_to_dict
 
 
@@ -19,18 +22,23 @@ REQUIRED_COLUMNS = {
 
 def critical_port_pairs_scheduler():
     services = ['fcl_freight', 'air_freight']
+    fcl_freight_location_cluster = FclFreightLocationCluster.select(FclFreightLocationCluster.base_port_id.distinct())
+    fcl_freight_location_cluster = jsonable_encoder(list(fcl_freight_location_cluster.dicts()))
+    fcl_freight_location_cluster = [i['base_port_id'] for i in fcl_freight_location_cluster]
+
+    air_freight_location_cluster = AirFreightLocationCluster.select(AirFreightLocationCluster.base_airport_id.distinct())
+    air_freight_location_cluster = jsonable_encoder(list(air_freight_location_cluster.dicts()))
+    air_freight_location_cluster = [i['base_airport_id'] for i in air_freight_location_cluster]
 
     for service in services:
         if service == "fcl_freight":
             fcl_query = FclFreightRate.select(*[getattr(FclFreightRate, col) for col in REQUIRED_COLUMNS['fcl_freight']]).where(
-                    ((FclFreightRate.origin_port_id << CRITICAL_PORTS_INDIA_VIETNAM) |
-                    (FclFreightRate.destination_port_id << CRITICAL_PORTS_INDIA_VIETNAM)),
+                    ((FclFreightRate.origin_port_id << CRITICAL_PORTS_INDIA_VIETNAM) & (FclFreightRate.destination_port_id << fcl_freight_location_cluster)) | ((FclFreightRate.origin_port_id << fcl_freight_location_cluster) & (FclFreightRate.destination_port_id << CRITICAL_PORTS_INDIA_VIETNAM)),
                     (FclFreightRate.updated_at < SEVEN_DAYS_AGO)
                 )
         if service == 'air_freight':
             air_query = AirFreightRate.select(*[getattr(AirFreightRate, col) for col in REQUIRED_COLUMNS['air_freight']]).where(
-                    ((AirFreightRate.origin_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM) |
-                    (AirFreightRate.destination_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM)),
+                    ((AirFreightRate.origin_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM) & (AirFreightRate.destination_airport_id << fcl_freight_location_cluster)) | ((AirFreightRate.origin_airport_id << fcl_freight_location_cluster) & (AirFreightRate.destination_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM)),
                     (AirFreightRate.updated_at < SEVEN_DAYS_AGO)
                 )
             
