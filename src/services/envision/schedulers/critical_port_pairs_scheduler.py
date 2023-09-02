ad9ba4c2e9a4637bd701daf2a35457bbd719d1ac
sunazhar <moh.hussain@cogoport.com>
2023-09-02 12:46:25 +0530

    param changes

--- src/services/envision/schedulers/critical_port_pairs_scheduler.py
+++ src/services/envision/schedulers/critical_port_pairs_scheduler.py
@@ -14,7 +14,7 @@ def critical_port_pairs_scheduler():
     required_columns = {
         'fcl_freight': ['id', 'origin_port_id', 'origin_port', 'origin_main_port_id', 'destination_port_id', 'destination_port','destination_main_port_id',
                         'shipping_line_id', 'shipping_line', 'service_provider_id', 'service_provider','container_size', 'container_type', 'commodity', 'rate_type'],
-        'air_freight': ['id', 'origin_airport_id', 'origin_airport','destination_airport_id', 'destination_airport','commodity', 'airline_id', 'service_provider_id', 'length', 'breadth', 'height']
+        'air_freight': ['id', 'origin_airport_id', 'origin_airport','destination_airport_id', 'destination_airport','commodity', 'airline_id', 'service_provider_id', 'commodity_type', 'commodity_sub_type', 'operation_type', 'shipment_type', 'stacking_type', 'price_type']
     }
     
     for service in services:
