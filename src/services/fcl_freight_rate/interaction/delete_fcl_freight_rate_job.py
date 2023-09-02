936b4467c11939cc18b841e10f9d6f7e9133c5e7
Saksham5730 <saksham.sharma@cogoport.com>
2023-09-02 22:08:41 +0530

    fix:

--- src/services/fcl_freight_rate/interaction/delete_fcl_freight_rate_job.py
+++ src/services/fcl_freight_rate/interaction/delete_fcl_freight_rate_job.py
@@ -8,7 +8,7 @@ POSSIBLE_CLOSING_REMARKS = ['not_serviceable', 'rate_not_available', 'no_change_
 
 
 def delete_fcl_freight_rate_job(request):
-    if request.get('closing_remarks') and request.get('closing_remarks')[0] in POSSIBLE_CLOSING_REMARKS:
+    if request.get('closing_remarks') and request.get('closing_remarks') in POSSIBLE_CLOSING_REMARKS:
         update_params = {'status':'aborted'}
     else:
         update_params = {'status':'completed'}
