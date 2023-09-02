2103fffa973a6e3ccb55d67a00705773fc210754
Saksham5730 <saksham.sharma@cogoport.com>
2023-09-02 18:41:27 +0530

    fix:

--- src/services/envision/schedulers/critical_port_pairs_scheduler.py
+++ src/services/envision/schedulers/critical_port_pairs_scheduler.py
@@ -5,27 +5,29 @@ from services.fcl_freight_rate.interaction.create_fcl_freight_rate_jobs import c
 from configs.fcl_freight_rate_constants import CRITICAL_PORTS_INDIA_VIETNAM, CRITICAL_AIRPORTS_INDIA_VIETNAM
 import datetime
 from fastapi.encoders import jsonable_encoder
+from playhouse.postgres_ext import ServerSide
 
 SEVEN_DAYS_AGO = datetime.datetime.now() - datetime.timedelta(days=7)
 
-def critical_port_pairs_scheduler():
-    services = ['fcl_freight', 'air_freight']
-
-    required_columns = {
+REQUIRED_COLUMNS = {
         'fcl_freight': ['id', 'origin_port_id', 'origin_port', 'origin_main_port_id', 'destination_port_id', 'destination_port','destination_main_port_id',
                         'shipping_line_id', 'shipping_line', 'service_provider_id', 'service_provider','container_size', 'container_type', 'commodity', 'rate_type'],
         'air_freight': ['id', 'origin_airport_id', 'origin_airport','destination_airport_id', 'destination_airport','commodity', 'airline_id', 'service_provider_id', 'commodity_type', 'commodity_sub_type', 'operation_type', 'shipment_type', 'stacking_type', 'price_type']
     }
     
+
+def critical_port_pairs_scheduler():
+    services = ['fcl_freight', 'air_freight']
+
     for service in services:
         if service == "fcl_freight":
-            fcl_query = FclFreightRate.select(*[getattr(FclFreightRate, col) for col in required_columns['fcl_freight']]).where(
+            fcl_query = FclFreightRate.select(*[getattr(FclFreightRate, col) for col in REQUIRED_COLUMNS['fcl_freight']]).where(
                     ((FclFreightRate.origin_port_id << CRITICAL_PORTS_INDIA_VIETNAM) |
                     (FclFreightRate.destination_port_id << CRITICAL_PORTS_INDIA_VIETNAM)),
                     (FclFreightRate.updated_at < SEVEN_DAYS_AGO)
                 )
         if service == 'air_freight':
-            air_query = AirFreightRate.select(*[getattr(AirFreightRate, col) for col in required_columns['air_freight']]).where(
+            air_query = AirFreightRate.select(*[getattr(AirFreightRate, col) for col in REQUIRED_COLUMNS['air_freight']]).where(
                     ((AirFreightRate.origin_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM) |
                     (AirFreightRate.destination_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM)),
                     (AirFreightRate.updated_at < SEVEN_DAYS_AGO)
