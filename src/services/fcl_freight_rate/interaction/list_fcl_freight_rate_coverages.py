c7db6053d6a4e7b4d474747f1c097516b0335cd2
Saksham5730 <saksham.sharma@cogoport.com>
2023-09-04 13:00:42 +0530

    fix:

--- src/services/fcl_freight_rate/interaction/list_fcl_freight_rate_coverages.py
+++ src/services/fcl_freight_rate/interaction/list_fcl_freight_rate_coverages.py
@@ -6,36 +6,50 @@ from libs.json_encoder import json_encoder
 from datetime import datetime, timedelta
 
 
-STRING_FORMAT = '%Y-%m-%dT%H:%M:%S.%f%z'
+STRING_FORMAT = "%Y-%m-%dT%H:%M:%S.%f%z"
 
 possible_direct_filters = [
     "origin_port_id",
     "destination_port_id",
     "shipping_line_id",
     "commodity",
-    "status"
+    "status",
+    "serial_id",
 ]
 possible_indirect_filters = ["updated_at", "user_id", "date_range"]
 
 DEFAULT_REQUIRED_FIELDS = [
-    'id',
-    'assigned_to',
-    'closed_by',
-    'closing_remarks',
-    'commodity',
-    'container_size',
-    'created_at',
-    'updated_at',
-    'status',
-    'shipping_line',
-    'service_provider',
-    'origin_port',
-    'destination_port',
-    'container_type',
+    "id",
+    "assigned_to",
+    "closed_by",
+    "closing_remarks",
+    "commodity",
+    "container_size",
+    "created_at",
+    "updated_at",
+    "status",
+    "shipping_line",
+    "shipping_line_id",
+    "service_provider",
+    "service_provider_id",
+    "origin_port",
+    "origin_port_id",
+    "destination_port",
+    "destination_port_id",
+    "serial_id",
+    "container_type",
 ]
 
 
-def list_fcl_freight_rate_coverages(filters = {}, page_limit = 10, page = 1, sort_by = 'updated_at', sort_type = 'desc', generate_csv_url = False, includes = {}):
+def list_fcl_freight_rate_coverages(
+    filters={},
+    page_limit=10,
+    page=1,
+    sort_by="updated_at",
+    sort_type="desc",
+    generate_csv_url=False,
+    includes={},
+):
     query = get_query(sort_by, sort_type, includes)
     if filters:
         if type(filters) != dict:
@@ -56,14 +70,22 @@ def list_fcl_freight_rate_coverages(filters = {}, page_limit = 10, page = 1, sor
 
 def get_statisitcs(query, filters):
     dynamic_statisitcs = {}
-    dynamic_statisitcs['spot_search'] = query.where(FclFreightRateJobs.source == 'spot_search').count() 
-    dynamic_statisitcs['critical_ports'] = query.where(FclFreightRateJobs.source == 'critical_ports').count()
-    dynamic_statisitcs['expiring_rates'] = query.where(FclFreightRateJobs.source == 'expiring_rates').count()
-    dynamic_statisitcs['cancelled_shipments'] = query.where(FclFreightRateJobs.source == 'cancelled_shipments').count() 
-    query = query.where(FclFreightRateJobs.source == filters['source'])
+    dynamic_statisitcs["spot_search"] = query.where(
+        FclFreightRateJobs.source == "spot_search"
+    ).count()
+    dynamic_statisitcs["critical_ports"] = query.where(
+        FclFreightRateJobs.source == "critical_ports"
+    ).count()
+    dynamic_statisitcs["expiring_rates"] = query.where(
+        FclFreightRateJobs.source == "expiring_rates"
+    ).count()
+    dynamic_statisitcs["cancelled_shipments"] = query.where(
+        FclFreightRateJobs.source == "cancelled_shipments"
+    ).count()
+    query = query.where(FclFreightRateJobs.source == filters["source"])
     return dynamic_statisitcs, query
-    
-    
+
+
 def get_data(query):
     return list(query.dicts())
 
@@ -71,10 +93,12 @@ def get_data(query):
 def get_query(sort_by, sort_type, includes):
     if includes:
         fcl_all_fields = list(FclFreightRateJobs._meta.fields.keys())
-        required_fcl_fields =  [a for a in includes.keys() if a in fcl_all_fields]
+        required_fcl_fields = [a for a in includes.keys() if a in fcl_all_fields]
         fcl_fields = [getattr(FclFreightRateJobs, key) for key in required_fcl_fields]
     else:
-        fcl_fields = [getattr(FclFreightRateJobs, key) for key in DEFAULT_REQUIRED_FIELDS]
+        fcl_fields = [
+            getattr(FclFreightRateJobs, key) for key in DEFAULT_REQUIRED_FIELDS
+        ]
     query = FclFreightRateJobs.select(*fcl_fields)
     if sort_by:
         query = query.order_by(
@@ -105,11 +129,17 @@ def apply_date_range_filter(query, filters):
     if not filters["date_range"]["startDate"]:
         start_date = datetime.now() - timedelta(days=7)
     else:
-        start_date = datetime.strptime(filters["date_range"]["startDate"],STRING_FORMAT) + timedelta(hours=5, minutes=30)
+        start_date = datetime.strptime(
+            filters["date_range"]["startDate"], STRING_FORMAT
+        ) + timedelta(hours=5, minutes=30)
     if not filters["date_range"]["endDate"]:
-        
-        end_date = datetime.now() 
+        end_date = datetime.now()
     else:
-        end_date = datetime.strptime(filters["date_range"]["endDate"],STRING_FORMAT) + timedelta(hours=5, minutes=30)
-    query = query.where(FclFreightRateJobs.created_at.cast("date") >= start_date.date(), FclFreightRateJobs.created_at.cast("date") <= end_date.date())
+        end_date = datetime.strptime(
+            filters["date_range"]["endDate"], STRING_FORMAT
+        ) + timedelta(hours=5, minutes=30)
+    query = query.where(
+        FclFreightRateJobs.created_at.cast("date") >= start_date.date(),
+        FclFreightRateJobs.created_at.cast("date") <= end_date.date(),
+    )
     return query
