d72e5b60489be895497ab436979e69de86b6d65d
telkargulshan <gulshan.telkar@cogoport.com>
2023-09-04 13:29:27 +0530

    gri

--- src/services/fcl_freight_rate/helpers/get_critical_ports_extension_parameters.py
+++ src/services/fcl_freight_rate/helpers/get_critical_ports_extension_parameters.py
@@ -1,5 +1,6 @@
 from services.fcl_freight_rate.interaction.get_cluster_extension_gri_worker import get_cluster_extension_gri_worker
 from services.fcl_freight_rate.models.fcl_freight_location_cluster import FclFreightLocationCluster
+from services.fcl_freight_rate.models.fcl_freight_location_cluster_mapping import FclFreightLocationClusterMapping
 import datetime
 
 INDIA_CRITICAL_PORTS = ['eb187b38-51b2-4a5e-9f3c-978033ca1ddf','7aa6ac82-c295-497f-bfe1-90294cdfa7a9','3c843f50-867c-4b07-bb57-e61af97dabfe']
@@ -27,22 +28,29 @@ def generate_combinations(base_port_ids, critical_ports):
             })
     
     return combinations
-
 def get_critical_ports_extension_parameters():
     all_base_port_ids = fetch_all_base_port_ids()
     india_combinations = generate_combinations(all_base_port_ids, INDIA_CRITICAL_PORTS)
     vietnam_combinations = generate_combinations(all_base_port_ids, VIETNAM_CRITICAL_PORTS)
     starttime = datetime.datetime.now()
     
+    location_mappings = (FclFreightLocationClusterMapping
+                             .select(FclFreightLocationClusterMapping.location_id, FclFreightLocationCluster.base_port_id)
+                             .join(FclFreightLocationCluster))
+    
+    
     extension_parameters = []
     for combo in india_combinations + vietnam_combinations:
+        origin_secondary_ports = [str(mapping.location_id) for mapping in location_mappings if str(mapping.base_port_id) == combo["origin_port_id"] ]
+
         request_data = {
-            "start_time": starttime, 
+            "start_time": starttime,
             "origin_port_id": combo["origin_port_id"],
             "destination_port_id": combo["destination_port_id"],
             "container_type": "standard",
-            "commodity": "general" 
+            "commodity": "general",
+            "origin_secondary_ports": origin_secondary_ports,
         }
         extension_parameters.append(request_data)
-    
+
     return extension_parameters
