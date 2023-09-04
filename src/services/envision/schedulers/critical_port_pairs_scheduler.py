f6ccfe519ac1cf2a1e1ee7498de43d2cdb2d22dc
Abhishek <abhishek@cogoport.com>
2023-09-04 18:51:22 +0530

    changes

--- src/services/envision/schedulers/critical_port_pairs_scheduler.py
+++ src/services/envision/schedulers/critical_port_pairs_scheduler.py
@@ -3,16 +3,13 @@ from services.air_freight_rate.models.air_freight_rate import AirFreightRate
 from services.air_freight_rate.interactions.create_air_freight_rate_jobs import create_air_freight_rate_jobs
 from services.fcl_freight_rate.interaction.create_fcl_freight_rate_jobs import create_fcl_freight_rate_jobs
 from configs.fcl_freight_rate_constants import CRITICAL_PORTS_INDIA_VIETNAM, DEFAULT_RATE_TYPE
-from services.air_freight_rate.constants.air_freight_rate_constants import CRITICAL_AIRPORTS_MAPPINGS
+from services.air_freight_rate.constants.air_freight_rate_constants import CRITICAL_AIRPORTS_INDIA_VIETNAM
 import datetime
 from playhouse.postgres_ext import ServerSide
 from services.fcl_freight_rate.models.fcl_freight_location_cluster import FclFreightLocationCluster
 from services.air_freight_rate.models.air_freight_location_cluster import AirFreightLocationCluster
 from fastapi.encoders import jsonable_encoder
 from playhouse.shortcuts import model_to_dict
-import operator
-from functools import reduce
-
 
 SEVEN_DAYS_AGO = datetime.datetime.now().date() - datetime.timedelta(days=7)
 
@@ -27,7 +24,11 @@ def critical_port_pairs_scheduler():
     services = ['fcl_freight', 'air_freight']
     all_fcl_critical_ports = FclFreightLocationCluster.select(FclFreightLocationCluster.base_port_id)
     all_fcl_critical_ports = jsonable_encoder(list(all_fcl_critical_ports.dicts()))
-    fcl_critical_ports_except_in_vn = [str(i['base_port_id']) for i in all_fcl_critical_ports if str(i['base_port_id']) not in CRITICAL_PORTS_INDIA_VIETNAM]
+    fcl_critical_ports_except_in_vn = [str(i['base_port_id']) for i in all_fcl_critical_ports if str(i['base_port_id']) not in CRITICAL_AIRPORTS_INDIA_VIETNAM]
+    
+    all_air_critical_ports = AirFreightLocationCluster.select(AirFreightLocationCluster.base_airport_id)
+    all_air_critical_ports = jsonable_encoder(list(all_air_critical_ports.dicts()))
+    air_critical_ports_except_in_vn = [str(i['base_airport_id']) for i in all_air_critical_ports if str(i['base_airport_id']) not in CRITICAL_AIRPORTS_INDIA_VIETNAM]
 
     for service in services:
         if service == "fcl_freight":
@@ -38,10 +39,11 @@ def critical_port_pairs_scheduler():
                     FclFreightRate.rate_type == DEFAULT_RATE_TYPE
                 )
         if service == 'air_freight':
-            conditions = [((AirFreightRate.origin_airport_id == item["origin_airport_id"]) & (AirFreightRate.destination_airport_id == item["destination_airport_id"]) & (AirFreightRate.airline_id == item["airline_id"])) for item in CRITICAL_AIRPORTS_MAPPINGS]
-            exp = reduce(operator.or_, conditions)
-            
-            air_query = AirFreightRate.select(*[getattr(AirFreightRate, col) for col in REQUIRED_COLUMNS['air_freight']]).where(exp, AirFreightRate.source != 'predicted')
+            air_query  = AirFreightRate.select(*[getattr(AirFreightRate, col) for col in REQUIRED_COLUMNS['air_freight']]).where(
+                    ((AirFreightRate.origin_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM) & (AirFreightRate.destination_airport_id << air_critical_ports_except_in_vn)) | ((AirFreightRate.origin_airport_id << air_critical_ports_except_in_vn) & (AirFreightRate.destination_airport_id << CRITICAL_AIRPORTS_INDIA_VIETNAM)),
+                    (AirFreightRate.updated_at.cast('date') < SEVEN_DAYS_AGO),
+                    AirFreightRate.source != 'predicted'
+                )
             
     for rate in ServerSide(fcl_query):
         rate_data = model_to_dict(rate)
