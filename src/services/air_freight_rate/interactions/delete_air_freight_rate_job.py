2c9177f890f2ed076146222da333d385ffdfedd0
Saksham5730 <saksham.sharma@cogoport.com>
2023-09-04 20:57:10 +0530

    fix:

--- src/services/air_freight_rate/interactions/delete_air_freight_rate_job.py
+++ src/services/air_freight_rate/interactions/delete_air_freight_rate_job.py
@@ -1,17 +1,17 @@
 from database.db_session import db
 from services.air_freight_rate.models.air_freight_rate_jobs import AirFreightRateJobs
 from services.air_freight_rate.models.air_freight_rate_jobs_mapping import AirFreightRateJobsMapping
-from datetime import datetime
-from fastapi import HTTPException
+from database.rails_db import get_user
+
 
 POSSIBLE_CLOSING_REMARKS = ['not_serviceable', 'rate_not_available', 'no_change_in_rate']
 
 
 def delete_air_freight_rate_job(request):
     if request.get('closing_remarks') and request.get('closing_remarks') in POSSIBLE_CLOSING_REMARKS:
-        update_params = {'status':'aborted'}
+        update_params = {'status':'aborted', "closed_by_id": request.get('performed_by_id'), "closed_by": get_user(request.get('performed_by_id'))[0]}
     else:
-        update_params = {'status':'completed'}
+        update_params = {'status':'completed', "closed_by_id": request.get('performed_by_id'), "closed_by": get_user(request.get('performed_by_id'))[0]}
 
     air_freight_rate_job = AirFreightRateJobs.update(update_params).where(AirFreightRateJobs.id == request['id']).execute()
     set_jobs_mapping(request['id'], request)
@@ -23,7 +23,6 @@ def set_jobs_mapping(jobs_id, data):
     audit_id = AirFreightRateJobsMapping.create(
         source_id=data.get("rate_id"),
         job_id= jobs_id,
-        source = 'air_freight_rate',
         performed_by_id = data.get("performed_by_id"),
         data = data.get('data')
     )
--- src/services/fcl_freight_rate/interaction/delete_fcl_freight_rate_job.py
+++ src/services/fcl_freight_rate/interaction/delete_fcl_freight_rate_job.py
@@ -1,17 +1,17 @@
 from database.db_session import db
 from services.fcl_freight_rate.models.fcl_freight_rate_jobs import FclFreightRateJobs
 from services.fcl_freight_rate.models.fcl_freight_rate_jobs_mapping import FclFreightRateJobsMapping
-from datetime import datetime
-from fastapi import HTTPException
+from database.rails_db import get_user
+
 
 POSSIBLE_CLOSING_REMARKS = ['not_serviceable', 'rate_not_available', 'no_change_in_rate']
 
 
 def delete_fcl_freight_rate_job(request):
     if request.get('closing_remarks') and request.get('closing_remarks') in POSSIBLE_CLOSING_REMARKS:
-        update_params = {'status':'aborted'}
+        update_params = {'status':'aborted', "closed_by_id": request.get('performed_by_id'), "closed_by": get_user(request.get('performed_by_id'))[0]}
     else:
-        update_params = {'status':'completed'}
+        update_params = {'status':'completed', "closed_by_id": request.get('performed_by_id'), "closed_by": get_user(request.get('performed_by_id'))[0]}
 
     fcl_freight_rate_job = FclFreightRateJobs.update(update_params).where(FclFreightRateJobs.id == request['id']).execute()
     set_jobs_mapping(request['id'], request)
