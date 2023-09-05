582d6a2557128cbbb720e51a3c98fe3b5a577280
yash2001cp <yash.suryawanshi@cogoport.com>
2023-09-05 18:52:58 +0530

    done

--- src/database/migrations/019_migration_1693907329.py
+++ src/database/migrations/019_migration_1693907329.py
@@ -13,9 +13,9 @@ SQL = pw.SQL
 
 def migrate(migrator: Migrator, database, fake=False, **kwargs):
     """Write your migrations here."""
-    migrator.add_fields(FclFreightRateStatistic, bas_price = FloatField(default = 0),null = True)
-    migrator.add_fields(FclFreightRateStatistic, bas_standard_price = FloatField(default = 0),null = True)
-    migrator.add_fields(FclFreightRateStatistic, bas_currency = CharField(max_length = 3),null = True)
+    migrator.add_fields(FclFreightRateStatistic, bas_price = FloatField(default = 0,null = True))
+    migrator.add_fields(FclFreightRateStatistic, bas_standard_price = FloatField(default = 0,null = True))
+    migrator.add_fields(FclFreightRateStatistic, bas_currency = CharField(max_length = 3,null = True))
 
 
 def rollback(migrator: Migrator, database, fake=False, **kwargs):
