5515ae06a2f4138b34c55ef7977ce059912e5e07
bedadha-Ajay <bedada.kumar@cogoport.com>
2023-09-06 20:27:40 +0530

    min_price_handling

--- src/services/air_freight_rate/interactions/get_air_freight_rate_cards.py
+++ src/services/air_freight_rate/interactions/get_air_freight_rate_cards.py
@@ -278,7 +278,6 @@ def build_freight_object(freight_validity,required_weight,requirements):
         freight_object['is_minimum_threshold_rate'] = True
     line_item['name'] = 'Basic Freight'
     line_item['source'] = 'system'
-    # line_item,freight_object = check_and_update_min_price_line_items(line_item, freight_object,requirements)
     freight_object['line_items'].append(line_item)
     freight_object['chargeable_weight'] = required_weight
     return freight_object
