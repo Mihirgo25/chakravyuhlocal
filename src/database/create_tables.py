3805d978aea09c95eed911ba5d0e2e648ecb874b
samir-cogoport <samir.dupare@cogoport.com>
2023-09-08 22:05:25 +0530

    create table again

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -3,6 +3,7 @@ from services.air_freight_rate.models.air_freight_rate_jobs import AirFreightRat
 from services.air_freight_rate.models.air_freight_rate_jobs_mapping import AirFreightRateJobMapping
 from services.fcl_freight_rate.models.fcl_freight_rate_job_mappings import FclFreightRateJobMapping
 from services.fcl_freight_rate.models.fcl_freight_rate_jobs import FclFreightRateJob
+from services.fcl_freight_rate.models.critical_port_trend_indexes import CriticalPortTrendIndex
 
 class Table:
     def __init__(self) -> None:
@@ -21,6 +22,6 @@ class Table:
 
 
 if __name__ == "__main__":
-    models = [AirFreightRateJob, AirFreightRateJobMapping, FclFreightRateJobMapping, FclFreightRateJob]
+    models = [AirFreightRateJob, AirFreightRateJobMapping, FclFreightRateJobMapping, FclFreightRateJob, CriticalPortTrendIndex]
 
     Table().create_tables(models)
