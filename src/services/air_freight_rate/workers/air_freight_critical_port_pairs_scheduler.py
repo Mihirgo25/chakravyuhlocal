b9de33b7d2a97db9f673a7c356c41e3d83cfed71
Saksham573001 <saksham.sharma@cogoport.com>
2023-09-13 22:39:09 +0530

    fix:

--- src/services/air_freight_rate/workers/air_freight_critical_port_pairs_scheduler.py
+++ src/services/air_freight_rate/workers/air_freight_critical_port_pairs_scheduler.py
@@ -2,9 +2,6 @@ from services.air_freight_rate.models.air_freight_rate import AirFreightRate
 from services.air_freight_rate.interactions.create_air_freight_rate_job import (
     create_air_freight_rate_job,
 )
-from services.air_freight_rate.constants.air_freight_rate_constants import (
-     INDIA_CRITICAL_PORT_PAIR, VIETNAM_CRITICAL_PORT_PAIRS
-)
 import datetime
 from playhouse.postgres_ext import ServerSide
 from playhouse.shortcuts import model_to_dict
@@ -12,8 +9,9 @@ from playhouse.shortcuts import model_to_dict
 SEVEN_DAYS_AGO = datetime.datetime.now().date() - datetime.timedelta(days=7)
 TODAYS_DATE = datetime.datetime.now().date()
 from services.air_freight_rate.constants.air_freight_rate_constants import COGOXPRESS
-
-
+import json
+import os
+from configs.definitions import ROOT_DIR
 
 REQUIRED_COLUMNS = [
     "id",
@@ -46,29 +44,38 @@ def air_freight_critical_port_pairs_scheduler():
     )
     condition = None
 
-    for pairs in INDIA_CRITICAL_PORT_PAIR:
-        current_condition = (
-            (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
-            (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
-        )
-        
-        if condition is None:
-            condition = current_condition
-        else:
-            condition = condition | current_condition
 
-    for pairs in VIETNAM_CRITICAL_PORT_PAIRS:
-        current_condition = (
-            (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
-            (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
-        )
-        
-        if condition is None:
-            condition = current_condition
-        else:
-            condition = condition | current_condition
+    critical_port_pairs_india_path = os.path.join(ROOT_DIR, "libs", "air_freight_critical_port_pairs_india.json")
+    with open(critical_port_pairs_india_path, "r") as json_file:
+        india_critical_port_pairs = json.load(json_file)
 
-    air_query = air_query.where(condition)
+        for pairs in india_critical_port_pairs:
+            current_condition = (
+                (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
+                (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
+            )
+            
+            if condition is None:
+                condition = current_condition
+            else:
+                condition = condition | current_condition
+            
+    critical_port_pairs_vietnam_path = os.path.join(ROOT_DIR, "libs", "air_freight_critical_port_pairs_vietnam.json")
+    with open(critical_port_pairs_vietnam_path, "r") as json_file:
+        vietnam_critical_port_pairs = json.load(json_file)
+
+        for pairs in vietnam_critical_port_pairs:
+            current_condition = (
+                (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
+                (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
+            )
+            
+            if condition is None:
+                condition = current_condition
+            else:
+                condition = condition | current_condition
+    if condition:
+        air_query = air_query.where(condition)
    
     for rate in ServerSide(air_query):
         rate_data = model_to_dict(rate)
--- src/services/air_freight_rate/workers/air_freight_expiring_rates_scheduler.py
+++ src/services/air_freight_rate/workers/air_freight_expiring_rates_scheduler.py
@@ -5,10 +5,10 @@ from services.air_freight_rate.interactions.create_air_freight_rate_job import (
 import datetime
 from playhouse.postgres_ext import ServerSide
 from playhouse.shortcuts import model_to_dict
-from services.air_freight_rate.constants.air_freight_rate_constants import (
-     INDIA_CRITICAL_PORT_PAIR, VIETNAM_CRITICAL_PORT_PAIRS
-)
 from services.air_freight_rate.constants.air_freight_rate_constants import COGOXPRESS
+import json
+import os
+from configs.definitions import ROOT_DIR
 
 DAYS_TO_EXPIRE = datetime.datetime.now().date() + datetime.timedelta(days=2)
 
@@ -44,29 +44,37 @@ def air_freight_expiring_rates_scheduler():
     )
     condition = None
 
-    for pairs in INDIA_CRITICAL_PORT_PAIR:
-        current_condition = (
-            (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
-            (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
-        )
-        
-        if condition is None:
-            condition = current_condition
-        else:
-            condition = condition | current_condition
+    critical_port_pairs_india_path = os.path.join(ROOT_DIR, "libs", "air_freight_critical_port_pairs_india.json")
+    with open(critical_port_pairs_india_path, "r") as json_file:
+        india_critical_port_pairs = json.load(json_file)
 
-    for pairs in VIETNAM_CRITICAL_PORT_PAIRS:
-        current_condition = (
-            (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
-            (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
-        )
-        
-        if condition is None:
-            condition = current_condition
-        else:
-            condition = condition | current_condition
+        for pairs in india_critical_port_pairs:
+            current_condition = (
+                (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
+                (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
+            )
+            
+            if condition is None:
+                condition = current_condition
+            else:
+                condition = condition | current_condition
 
-    air_query = air_query.where(condition)
+    critical_port_pairs_vietnam_path = os.path.join(ROOT_DIR, "libs", "air_freight_critical_port_pairs_vietnam.json")
+    with open(critical_port_pairs_vietnam_path, "r") as json_file:
+        vietnam_critical_port_pairs = json.load(json_file)
+        
+        for pairs in vietnam_critical_port_pairs:
+            current_condition = (
+                (AirFreightRate.origin_airport_id == pairs['origin_airport_id']) |
+                (AirFreightRate.destination_airport_id == pairs['destination_airport_id'])
+            )
+            
+            if condition is None:
+                condition = current_condition
+            else:
+                condition = condition | current_condition
+    if condition:
+        air_query = air_query.where(condition)
     
     for rate in ServerSide(air_query):
         rate_data = model_to_dict(rate)
