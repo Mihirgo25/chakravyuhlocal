9a5a06077352e989cb175abdc797440e46c9d5fc
Saksham573001 <saksham.sharma@cogoport.com>
2023-09-14 19:01:58 +0530

    fix: undo

--- src/services/air_freight_rate/helpers/generate_csv_file_url_for_air.py
+++ src/services/air_freight_rate/helpers/generate_csv_file_url_for_air.py
@@ -5,10 +5,12 @@ from playhouse.shortcuts import model_to_dict
 
 def generate_csv_file_url_for_air(query):
     csv_urls = []
+    rate_count = 0
     final_data = []
 
     for rate in ServerSide(query):
-        if len(final_data) > 5000:
+        if rate_count > 5000:
+            rate_count = 0
             csv_url = get_csv_url("air_freight", final_data)
             csv_urls.append(csv_url)
             final_data = []
@@ -16,6 +18,7 @@ def generate_csv_file_url_for_air(query):
         rate_data = model_to_dict(rate)
         required_coverage_data = get_air_freight_coverage_required_data(rate_data)
         final_data.append(required_coverage_data)
+        rate_count += 1
 
     if final_data:
         csv_url = get_csv_url("air_freight", final_data)
