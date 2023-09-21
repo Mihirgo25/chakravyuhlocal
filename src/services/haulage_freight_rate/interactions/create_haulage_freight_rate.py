d41ca3ad3aab180953e6f74ab9b298e824b3ce0a
Saksham573001 <saksham.sharma@cogoport.com>
2023-09-21 20:59:05 +0530

    fix:

--- src/services/haulage_freight_rate/interactions/create_haulage_freight_rate.py
+++ src/services/haulage_freight_rate/interactions/create_haulage_freight_rate.py
@@ -117,8 +117,10 @@ def create_haulage_freight_rate(request):
 
     haulage_freight_rate.update_platform_prices_for_other_service_providers()
 
-    delay_haulage_functions.apply_async(kwargs={'haulage_object':haulage_freight_rate,'request':request},queue='low')
+    delay_haulage_functions.apply_async(kwargs={'request':request},queue='low')
     get_multiple_service_objects_haulage(haulage_freight_rate, is_new_rate)
+    if not haulage_freight_rate.service_provider:
+        breakpoint()
 
     if request.get('haulage_freight_rate_request_id'):
         update_haulage_freight_rate_request_delay.apply_async(kwargs={'request':{'haulage_freight_rate_request_id': request.get('haulage_freight_rate_request_id'), 'closing_remarks': 'rate_added', 'performed_by_id': request.get('performed_by_id')}},queue='low')
