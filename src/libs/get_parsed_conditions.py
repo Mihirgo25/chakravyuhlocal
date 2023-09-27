01bc18465f7e0197c21c69966a26b4c1056df2f1
Muchipalli Abhishikth <125270594+abhicogoport@users.noreply.github.com>
2023-09-28 00:23:49 +0530

    parse conditions

--- src/libs/get_parsed_conditions.py
+++ src/libs/get_parsed_conditions.py
@@ -2,31 +2,58 @@ from libs.get_applicable_filters import is_valid_uuid
 from micro_services.client import *
 from database.rails_db import get_operators
 
-def process_conditions(condition_value, condition_key):
-    if condition_key in ["origin_country_id", "destination_country_id", "origin_port_id", "destination_port_id", "terminal_id"]:
-        locations_data = maps.list_locations({"filters": {"id": condition_value}})["list"]
-        locations_data = [location["name"] for location in locations_data]
-        return locations_data
-    elif condition_key == "shipping_line_id":
-        shipping_line_data = get_operators(id=condition_value)
-        shipping_line_data = [shipping_line["short_name"] for shipping_line in shipping_line_data]
-        return shipping_line_data
+LOCATION_IDS = ["origin_country_id", "destination_country_id", "origin_port_id", "destination_port_id", "terminal_id"]
 
+def ids_mappings(all_location_ids, all_shipping_line_ids):
+    locations_data  = maps.list_locations({ 'filters': { 'id': all_location_ids }})['list']
+    shipping_lines_data = get_operators(id = all_shipping_line_ids,operator_type = 'shipping_line')
+    location_mappings = {location['id']: location['name'] for location in locations_data}
+    shipping_line_mappings = {shipping_line['id']: shipping_line['short_name'] for shipping_line in shipping_lines_data}
+    return location_mappings, shipping_line_mappings
 
-def get_parsed_conditions_data(line_items):
-    parsed_line_items = []
+def extract_ids(line_items):
+    all_location_ids = []
+    all_shipping_line_ids = []
     for item in line_items:
         if item.get('conditions'):
             values = item["conditions"]["values"]
             for conditions in values:
                 condition_value = conditions["condition_value"]
-                if isinstance(condition_value, str) and  is_valid_uuid(condition_value):
-                    new_condition_value = process_conditions([condition_value], conditions["condition_key"])
-                    conditions['condition_value'] = new_condition_value
-                elif isinstance(condition_value, list):
-                    new_condition_value = process_conditions(condition_value, conditions["condition_key"])
-                    conditions['condition_value'] = new_condition_value
+                condition_key = conditions['condition_key']
+                if condition_key == 'shipping_line_id':
+                    if isinstance(condition_value, str) and is_valid_uuid(condition_value):
+                        all_shipping_line_ids.append(condition_value)
+                    elif isinstance(condition_value, list):
+                        all_shipping_line_ids.extend(condition_value)
+                elif condition_key in LOCATION_IDS:
+                    if isinstance(condition_value, str) and is_valid_uuid(condition_value):
+                        all_location_ids.append(condition_value)
+                    elif isinstance(condition_value, list):
+                        all_location_ids.extend(condition_value)
 
+    return all_location_ids, all_shipping_line_ids
+
+def parse_ids(ids, mappings):
+    if isinstance(ids, str):
+        return [mappings[ids]]
+    elif isinstance(ids, list):
+        return [mappings[value] for value in ids]
+
+def get_parsed_conditions_data(line_items):
+    parsed_line_items = []
+    location_ids, shipping_line_ids = extract_ids(line_items)
+    location_mappings, shipping_line_mappings = ids_mappings(location_ids, shipping_line_ids)
+    
+    for item in line_items:
+        if item.get('conditions'):
+            values = item["conditions"]["values"]
+            for conditions in values:
+                condition_value = conditions["condition_value"]
+                condition_key = conditions["condition_key"]
+                if condition_key == 'shipping_line_id':
+                    conditions['condition_value'] = parse_ids(condition_value, shipping_line_mappings)               
+                elif condition_key in LOCATION_IDS:
+                    conditions['condition_value'] = parse_ids(condition_value, location_mappings)
             parsed_line_items.append(item)
         else:
             parsed_line_items.append(item)
