f7b126922584405db98a91aaa114d1737e562112
Sahil-Cogo <sahil.ghule@cogoport.com>
2023-09-30 11:35:47 +0530

    .

--- src/services/lcl_freight_rate/interactions/delete_lcl_freight_rate_job.py
+++ src/services/lcl_freight_rate/interactions/delete_lcl_freight_rate_job.py
@@ -1,13 +1,13 @@
 from database.db_session import db
 from services.fcl_freight_rate.models.fcl_freight_rate_local_jobs import (
-    FclFreightRateLocalJob,
+    LclFreightRateLocalJob,
 )
 from services.fcl_freight_rate.models.fcl_freight_rate_local_job_mappings import (
-    FclFreightRateLocalJobMapping,
+    LclFreightRateLocalJobMapping,
 )
 from database.rails_db import get_user
 from datetime import datetime
-from services.fcl_freight_rate.models.fcl_services_audit import FclServiceAudit
+from services.fcl_freight_rate.models.fcl_services_audit import LclServiceAudit
 
 POSSIBLE_CLOSING_REMARKS = [
     "not_serviceable",
@@ -16,7 +16,7 @@ POSSIBLE_CLOSING_REMARKS = [
 ]
 
 
-def delete_fcl_freight_rate_local_job(request):
+def delete_lcl_freight_rate_local_job(request):
     if (
         request.get("closing_remarks")
         and request.get("closing_remarks") in POSSIBLE_CLOSING_REMARKS
@@ -37,10 +37,10 @@ def delete_fcl_freight_rate_local_job(request):
         }
 
     fcl_freight_rate_local_job = (
-        FclFreightRateLocalJob.update(update_params)
+        LclFreightRateLocalJob.update(update_params)
         .where(
-            FclFreightRateLocalJob.id == request["id"],
-            FclFreightRateLocalJob.status.not_in(["completed", "aborted"]),
+            LclFreightRateLocalJob.id == request["id"],
+            LclFreightRateLocalJob.status.not_in(["completed", "aborted"]),
         )
         .execute()
     )
@@ -51,10 +51,10 @@ def delete_fcl_freight_rate_local_job(request):
 
 
 def create_audit(jobs_id, data):
-    FclServiceAudit.create(
+    LclServiceAudit.create(
         action_name="delete",
         object_id=jobs_id,
-        object_type="FclFreightRateLocalJob",
+        object_type="LclFreightRateLocalJob",
         data=data.get("data"),
         performed_by_id=data.get("performed_by_id"),
     )
