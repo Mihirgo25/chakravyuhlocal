7811b6f661f38c6e5ea2c86e854fe24ea9802099
samir-cogoport <samir.dupare@cogoport.com>
2023-10-06 12:06:34 +0530

    get coverage from common

--- src/micro_services/common_client.py
+++ src/micro_services/common_client.py
@@ -58,3 +58,9 @@ class CommonApiClient:
     
     def get_all_exchange_rates(self, data = {}):
         return self.client.request('GET','get_all_exchange_rates',data)
+    
+    def get_sailing_schedule_port_pair_coverages(self, data={}):
+        return self.client.request('GET','get_sailing_schedule_port_pair_coverages', data)
+
+    def create_sailing_schedule_port_pair_coverage(self, data={}):
+        return self.client.request('POST','create_sailing_schedule_port_pair_coverage', data)
