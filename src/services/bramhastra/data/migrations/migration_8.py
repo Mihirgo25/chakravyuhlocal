461429b83753ec461dc162d7f5594c63168b826b
yash2001cp <yash.suryawanshi@cogoport.com>
2023-10-08 21:11:23 +0530

    jo

--- src/services/bramhastra/data/migrations/migration_8.py
+++ src/services/bramhastra/data/migrations/migration_8.py
@@ -4,10 +4,10 @@ from services.bramhastra.client import ClickHouse
 def backup():
     client = ClickHouse()
     client.execute(
-        """SELECT * FROM brahmastra.fcl_freight_rate_statistics INTO OUTFILE 'fcl_freight_rate_statistics_1.csv' FORMAT CSVWithNames"""
+        """SELECT * FROM brahmastra.stale_fcl_freight_rate_statistics INTO OUTFILE 'fcl_freight_rate_statistics_1.csv' FORMAT CSVWithNames"""
     )
     client.execute(
-        """SELECT * FROM brahmastra.air_freight_rate_statistics INTO OUTFILE 'air_freight_rate_statistics_1.csv' FORMAT CSVWithNames"""
+        """SELECT * FROM brahmastra.stale_air_freight_rate_statistics INTO OUTFILE 'air_freight_rate_statistics_1.csv' FORMAT CSVWithNames"""
     )
 
 
