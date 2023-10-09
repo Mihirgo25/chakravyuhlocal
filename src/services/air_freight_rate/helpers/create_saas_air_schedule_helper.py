ce176de9c50675ed2d968f3bcfc61bf7d155bd63
bedadha-Ajay <bedada.kumar@cogoport.com>
2023-10-09 17:03:21 +0530

    air serviceability

--- src/services/air_freight_rate/helpers/create_saas_air_schedule_helper.py
+++ src/services/air_freight_rate/helpers/create_saas_air_schedule_helper.py
@@ -7,11 +7,5 @@ def create_saas_air_schedule_airport_pair(air_object,request):
     origin_airport_id=request.get("origin_airport_id")
     destination_airport_id=request.get("destination_airport_id")
     airline_id = request.get('airline_id')
-    air_freight_query = AirFreightRate.select(AirFreightRate.id).where(
-        AirFreightRate.origin_airport_id == origin_airport_id,
-        AirFreightRate.destination_airport_id == destination_airport_id,
-        AirFreightRate.airline_id == airline_id,
-        ~(AirFreightRate.rate_not_available_entry)
-        )
-    
+   
     common.create_saas_air_schedule_airport_pair_coverage({"origin_airport_id":origin_airport_id, "destination_airport_id":destination_airport_id,"airline_id":airline_id,"source":"supply"})
