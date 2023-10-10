06e78095e339c3bc69038964cb58bdade0642adb
yash2001cp <yash.suryawanshi@cogoport.com>
2023-10-10 19:49:25 +0530

    df

--- src/services/bramhastra/models/fcl_freight_action.py
+++ src/services/bramhastra/models/fcl_freight_action.py
@@ -118,7 +118,7 @@ class FclFreightAction(BaseModel):
     
     @classmethod
     def update(cls, *args, **kwargs):
-        kwargs['updated_at'] = datetime.now()
+        kwargs['operation_updated_at'] = datetime.now()
         return super().update(*args, **kwargs)
 
     def refresh(self):
