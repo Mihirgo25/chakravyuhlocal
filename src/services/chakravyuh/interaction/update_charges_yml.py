aec13dc2b377cd910214f805f84d92a8828ec52e
telkargulshan <gulshan.telkar@cogoport.com>
2023-10-11 11:28:04 +0530

    fix

--- src/services/chakravyuh/interaction/update_charges_yml.py
+++ src/services/chakravyuh/interaction/update_charges_yml.py
@@ -1,9 +1,9 @@
-from libs.get_charges_yml import get_charge_from_rd, set_charge_to_rd,get_charge_key
+from libs.get_charges_yml import get_charge_key
 from micro_services.client import common 
 from database.db_session import rd
 
 
 def update_charges_yml(serviceChargeType):
+   
    rd.delete(get_charge_key(serviceChargeType))
-   temp=common.get_charge(serviceChargeType)
    return "Charge data deleted successfully"
