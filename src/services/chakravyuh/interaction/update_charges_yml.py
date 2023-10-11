8fea27f270abf6f6720b3f98010386dc11439471
samir-cogoport <samir.dupare@cogoport.com>
2023-10-11 16:50:33 +0530

    delete extra code

--- src/services/chakravyuh/interaction/update_charges_yml.py
+++
@@ -1,9 +0,0 @@
-from libs.get_charges_yml import get_charge_key
-from micro_services.client import common 
-from database.db_session import rd
-
-
-def update_charges_yml(serviceChargeType):
-   
-   rd.delete(get_charge_key(serviceChargeType))
-   return "Charge data deleted successfully"
