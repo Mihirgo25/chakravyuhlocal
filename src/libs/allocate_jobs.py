b7957695a4bcfae03a2ce68d060eaf4395967223
Saksham573001 <saksham.sharma@cogoport.com>
2023-10-12 18:48:24 +0530

    fix:

--- src/libs/allocate_jobs.py
+++ src/libs/allocate_jobs.py
@@ -1,24 +1,8 @@
 from database.db_session import rd
 from services.air_freight_rate.constants.air_freight_rate_constants import (
     AIR_COVERAGE_USERS,
-    AIR_LOCALS_COVERAGE_USERS
 )
-from configs.fcl_freight_rate_constants import (
-    FCL_COVERAGE_USERS,
-    FCL_LOCALS_COVERAGE_USERS,
-)
-from configs.haulage_freight_rate_constants import HAULAGE_FREIGHT_COVERAGE_USERS
-from configs.fcl_customs_rate_constants import FCL_CUSTOMS_COVERAGE_USERS
-from services.air_customs_rate.air_customs_rate_constants import (
-    AIR_CUSTOMS_COVERAGE_USERS,
-)
-from configs.ltl_freight_rate_constants import LTL_FREIGHT_COVERAGE_USERS
-from configs.ftl_freight_rate_constants import FTL_FREIGHT_COVERAGE_USERS
-from configs.fcl_cfs_rate_constants import FCL_CFS_COVERAGE_USERS
-from configs.lcl_customs_rate_constants import LCL_CUSTOMS_COVERAGE_USERS
-from configs.lcl_freight_rate_constants import LCL_FREIGHT_COVERAGE_USERS
-from micro_services.client import common
-
+from configs.fcl_freight_rate_constants import FCL_COVERAGE_USERS
 
 
 def allocate_jobs(service_type: str) -> str:
@@ -30,58 +14,22 @@ def allocate_jobs(service_type: str) -> str:
     :return: The allocated user
     :rtype: str
     """
-    service_type = service_type.upper()
-    if service_type in ["FCL", "AIR", "HAULAGE_FREIGHT", "FCL_CUSTOMS", "AIR_CUSTOMS", "LCL_CUSTOMS",
-                        "FCL_LOCALS", "AIR_LOCALS", "LTL_FREIGHT", "FTL_FREIGHT", "FCL_CFS", "LCL_FREIGHT"]:
-        users = globals()[f"{service_type}_COVERAGE_USERS"]
-        redis_key = f"last_assigned_user_{service_type.lower()}"
-    else:
-        users = None
-        redis_key = None
-    
+    if service_type.upper() == "FCL":
+        users = FCL_COVERAGE_USERS
+        redis_key = "last_assigned_user_fcl"
+    elif service_type.upper() == "AIR":
+        users = AIR_COVERAGE_USERS
+        redis_key = "last_assigned_user_air"
 
     last_assigned_user = rd.get(redis_key)
     if not last_assigned_user:
-        last_assigned_user = 0
+        last_assigned_user = 1
     else:
         last_assigned_user = int(last_assigned_user)
 
     # Increment and wrap around using modulo
-    next_user = (last_assigned_user + 1) % len(users)
+    next_user = (last_assigned_user % len(users)) + 1
 
     # Store the next user back to Redis
-
-    # Get the user ID for the next user to be assigned
-    next_user_id = users[next_user]
-    agent_filters = {}
-    filters = {"agent_id": users}
-    agent_filters['filters'] = filters
-    online_users = common.list_chat_agents(agent_filters)
-    if online_users:
-        online_users = online_users['list']
-    else:
-        rd.set(redis_key, next_user)
-        return users[next_user]
-
-    user_activity = []
-    for user in online_users:
-        if user['status'] == 'active':
-            user_activity.append(user['agent_id'])
-    # Check if the next user is online and active
-    
-    if next_user_id in user_activity:
-        # Set the allocated user in Redis
-        rd.set(redis_key, next_user)
-        return next_user_id
-    else:
-        # Find the next available online user in a round-robin manner
-        for _ in range(len(users)):
-            next_user = (next_user + 1) % len(users)
-            next_user_id = users[next_user]
-            if next_user_id in user_activity:
-                # Set the allocated user in Redis
-                rd.set(redis_key, next_user)
-                return next_user_id
-    
-    # If no online users are available, return a message
-    return "No online users available for job allocation."
+    rd.set(redis_key, next_user)
+    return users[next_user]
