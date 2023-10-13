ba13b98d08ede9d10bfd081be8e21fbd773cec88
yash2001cp <yash.suryawanshi@cogoport.com>
2023-10-13 11:13:13 +0530

    fd

--- src/services/bramhastra/helpers/shipment_fcl_freight_rate_statistic_helper.py
+++ src/services/bramhastra/helpers/shipment_fcl_freight_rate_statistic_helper.py
@@ -29,7 +29,7 @@ class Shipment:
 
     def set(self):
         shipment = self.request.dict(include={"shipment"})["shipment"]
-        actions = self.__get_actions()
+        actions = self.__get_actions(self.request.shipment.shipment_source_id)
         print(actions, shipment, 'actions')
         if not actions:
             return
@@ -82,9 +82,9 @@ class Shipment:
     def __create(self, model, params):
         model.create(**params)
 
-    def __get_actions(self):
+    def __get_actions(self, source_id):
         actions = FclFreightAction.select().where(
-            FclFreightAction.checkout_id == self.checkout_id
+            FclFreightAction.checkout_id == (self.checkout_id or source_id)
         )
         actions_hash = dict()
         for action in actions:
