8096f7728964b58a51949f96ce78e654e319c716
bedadha-Ajay <bedada.kumar@cogoport.com>
2023-10-16 15:15:31 +0530

    fix

--- src/services/air_freight_rate/helpers/create_saas_air_schedule_helper.py
+++ src/services/air_freight_rate/helpers/create_saas_air_schedule_helper.py
@@ -8,4 +8,4 @@ def create_saas_air_schedule_airport_pair(air_object,request):
     destination_airport_id=request.get("destination_airport_id")
     airline_id = request.get('airline_id')
    
-    common.create_saas_air_schedule_airport_pair_coverage({"origin_airport_id":origin_airport_id, "destination_airport_id":destination_airport_id,"airline_id":airline_id,"source":"supply"})
+    common.create_saas_air_schedule_airport_pair_coverage({"origin_airport_id":origin_airport_id, "destination_airport_id":destination_airport_id,"airline_id":airline_id,"source":"supply","is_verified":True})
