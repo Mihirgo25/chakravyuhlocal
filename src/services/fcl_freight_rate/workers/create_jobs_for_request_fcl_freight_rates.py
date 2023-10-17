fe354732f53afb91efc2a4cb318ab6da3f0822b1
Saksham573001 <saksham.sharma@cogoport.com>
2023-10-18 02:21:01 +0530

    fix:

--- src/services/fcl_freight_rate/workers/create_jobs_for_request_fcl_freight_rates.py
+++
@@ -1,6 +0,0 @@
-from services.fcl_freight_rate.interaction.create_fcl_freight_rate_job import (
-    create_fcl_freight_rate_job,
-)
-def create_jobs_for_request_fcl_freight_rate(requirements):
-    ids = create_fcl_freight_rate_job(requirements, "rate_request")
-    return ids
