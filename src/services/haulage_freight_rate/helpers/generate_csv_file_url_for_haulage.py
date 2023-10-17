cf9f6de0ac0d5cd1be8dca6c88a91e5bad44f462
HarshitCogo22 <harshit.mittal@cogoport.com>
2023-10-17 13:44:25 +0530

    fix:

--- src/services/haulage_freight_rate/helpers/generate_csv_file_url_for_haulage.py
+++ src/services/haulage_freight_rate/helpers/generate_csv_file_url_for_haulage.py
@@ -27,18 +27,14 @@ def generate_csv_file_url_for_haulage(query):
     return {"urls": csv_urls}
 
 
-def get_haulage_freight_coverage_required_data(haulage_coverage_data):
-    list_of_haulage_coverage_data = []
-    for coverage_data in haulage_coverage_data:
-        required_data = {}
-        required_data['origin_location'] = coverage_data['origin_location']['name']
-        required_data['destination_location'] = coverage_data['destination_location']['name']
-        required_data['origin_location_id'] = coverage_data['origin_location']['id']
-        required_data['destination_location_id'] = coverage_data['destination_location']['id']
-        required_data['commodity'] = coverage_data['commodity']
-        required_data['container_type']  = coverage_data['container_type']
-        required_data['container_size'] = coverage_data['container_size']
-        required_data['shipping_line'] = coverage_data['shipping_line']['short_name'] if coverage_data.get('shipping_line') else None
-        required_data['service_provider'] = coverage_data['service_provider']['short_name'] if coverage_data.get('service_provider') else None
-        list_of_haulage_coverage_data.append(required_data)
-    return list_of_haulage_coverage_data
+def get_haulage_freight_coverage_required_data(coverage_data):
+    required_data = {}
+    required_data['origin_location'] = coverage_data['origin_location']['name']
+    required_data['destination_location'] = coverage_data['destination_location']['name']
+    required_data['commodity'] = coverage_data['commodity']
+    required_data['container_type']  = coverage_data['container_type']
+    required_data['container_size'] = coverage_data['container_size']
+    required_data['shipping_line'] = coverage_data['shipping_line']['short_name'] if coverage_data.get('shipping_line') else None
+    required_data['service_provider'] = coverage_data['service_provider']['short_name'] if coverage_data.get('service_provider') else None
+        
+    return required_data
