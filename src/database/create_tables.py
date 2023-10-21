10603f53e9a808762d0a37cdaaa35e7f712b4023
Saksham573001 <saksham.sharma@cogoport.com>
2023-10-21 19:09:33 +0530

    fix: rename migration  number

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -24,18 +24,6 @@ from services.ftl_freight_rate.models.ftl_freight_rate_job_mappings import (
 )
 from services.fcl_cfs_rate.models.fcl_cfs_rate_jobs import FclCfsRateJob
 from services.fcl_cfs_rate.models.fcl_cfs_rate_job_mappings import FclCfsRateJobMapping
-from services.fcl_freight_rate.models.fcl_freight_rate_local_jobs import (
-    FclFreightRateLocalJob,
-)
-from services.fcl_freight_rate.models.fcl_freight_rate_local_job_mappings import (
-    FclFreightRateLocalJobMapping,
-)
-from services.air_freight_rate.models.air_freight_rate_local_jobs import (
-    AirFreightRateLocalJob,
-)
-from services.air_freight_rate.models.air_freight_rate_local_jobs_mapping import (
-    AirFreightRateLocalJobMapping,
-)
 
 from services.lcl_customs_rate.models.lcl_customs_rate_jobs import LclCustomsRateJob
 from services.lcl_customs_rate.models.lcl_customs_rate_job_mappings import (
@@ -74,13 +62,7 @@ class Table:
               CREATE SEQUENCE ftl_freight_rate_jobs_serial_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 0;
             
             
-              CREATE SEQUENCE fcl_cfs_rate_jobs_serial_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 0;
-            
-            
-              CREATE SEQUENCE fcl_freight_rate_local_jobs_serial_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 0;
-
-            
-              CREATE SEQUENCE air_freight_rate_local_jobs_serial_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 0;"""
+              CREATE SEQUENCE fcl_cfs_rate_jobs_serial_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 0;"""
             )
 
             db.create_tables(models)
@@ -104,10 +86,6 @@ if __name__ == "__main__":
         LtlFreightRateAudit,
         FclCfsRateJob,
         FclCfsRateJobMapping,
-        FclFreightRateLocalJob,
-        FclFreightRateLocalJobMapping,
-        AirFreightRateLocalJob,
-        AirFreightRateLocalJobMapping,
         FtlFreightRateJob,
         FtlFreightRateJobMapping,
         LclCustomsRateJob,
