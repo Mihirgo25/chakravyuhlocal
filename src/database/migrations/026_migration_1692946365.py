bb789fc1a6519d9128f90205295c344306fe2113
shreyasnakshatram <shreyas.nakshatram@cogoport.com>
2023-10-31 13:05:28 +0530

    change in migration file

--- src/database/migrations/026_migration_1692946365.py
+++ src/database/migrations/026_migration_1692946365.py
@@ -6,6 +6,7 @@ from peewee import *
 from peewee_migrate import Migrator
 from decimal import ROUND_HALF_EVEN
 from services.fcl_customs_rate.models.fcl_customs_rate import FclCustomsRate
+from services.fcl_customs_rate.models.fcl_customs_rate_feedback import FclCustomsRateFeedback
 
 try:
     import playhouse.postgres_ext as pw_pext
@@ -18,7 +19,7 @@ SQL = pw.SQL
 def migrate(migrator: Migrator, database, fake=False, **kwargs):
     """Write your migrations here."""
     migrator.add_fields(FclCustomsRate, cargo_handling_type = CharField(null=True, index=True))
-
+    migrator.add_fields(FclCustomsRateFeedback, cargo_handling_type = CharField(null=True))
 
 
 def rollback(migrator: Migrator, database, fake=False, **kwargs):
