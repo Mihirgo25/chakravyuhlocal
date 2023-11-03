2a6e4c55f161265f71614f6c71721480f72149ab
Srisha Reddy Tukkani <srisha.tukkani@cogoport.com>
2023-11-03 19:05:54 +0530

    changes

--- src/services/nandi/interactions/validate_dislike_rates.py
+++ src/services/nandi/interactions/validate_dislike_rates.py
@@ -16,7 +16,6 @@ def validate_freight_rate_feedback(request):
         if rate.source in ['predicted','rate_extension']:
             return {}
 
-
 def validate_fcl_freight_unpreferred_operator_function():
     return {}
 
@@ -27,42 +26,52 @@ def validate_air_freight_unpreferred_operator_function(request):
         }
     serviceable_airline_ids = common.get_saas_schedules_airport_pair_coverages(airport_id)
     if not (all(x in serviceable_airline_ids for x in request.get('preferred_airline_ids'))):
-        return {}
+        return {'message': 'Rates for all Shipping lines that are serviceable on this port pair are present'}
+    return {}
     
 def validate_air_customs_unsatisfactory_rate_function(request):
     from services.air_customs_rate.models.air_customs_rate import AirCustomsRate
     air_customs_rate = AirCustomsRate.select(AirCustomsRate.id,AirCustomsRate.updated_at).where(
         AirCustomsRate.id == request.get('rate_id')
-    )
+    ).order_by(AirCustomsRate.updated_at.desc()).first()
+
     if air_customs_rate.updated_at.date() < (datetime.now().date() - timedelta(days=30)):
         return {}
     return {}
+
 def validate_fcl_customs_unsatisfactory_rate_function(request):
     from services.fcl_customs_rate.models.fcl_customs_rate import FclCustomsRate
     fcl_customs_rate = FclCustomsRate.select(FclCustomsRate.id,FclCustomsRate.updated_at).where(
         FclCustomsRate.id == request.get('rate_id')
-    )
+    ).order_by(FclCustomsRate.updated_at.desc()).first()
+
     if fcl_customs_rate.updated_at.date() < (datetime.now().date() - timedelta(days=30)):
         return {}
     return {}
+
 def validate_ltl_freight_unsatisfactory_rate_function(request):
     return {}
+
 def validate_fcl_cfs_unsatisfactory_rate_function(request):
     from services.fcl_cfs_rate.models.fcl_cfs_rate import FclCfsRate
     fcl_cfs_rate = FclCfsRate.select(FclCfsRate.id,FclCfsRate.updated_at).where(
         FclCfsRate.id == request.get('rate_id')
-    )
+    ).order_by(FclCfsRate.updated_at.desc()).first()
+
     if fcl_cfs_rate.updated_at.date() < (datetime.now().date() - timedelta(days=30)):
         return {}
     return {}
+
 def validate_ftl_freight_unsatisfactory_rate_function(request):
     from services.ftl_freight_rate.models.ftl_freight_rate import FtlFreightRate
     ftl_freight_rate = FtlFreightRate.select(FtlFreightRate.id,FtlFreightRate.updated_at).where(
         FtlFreightRate.id == request.get('rate_id')
-    )
+    ).order_by(FtlFreightRate.updated_at.desc()).first()
+
     if ftl_freight_rate.updated_at.date() < (datetime.now().date() - timedelta(days=30)):
         return {}
     return {}
+
 def validate_lcl_freight_unsatisfactory_rate_function(request):
     return {}
     
