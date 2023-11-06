8750b57ac60fd09d1c7dc1b9496d4bcb5349ddbb
samir-cogoport <samir.dupare@cogoport.com>
2023-11-06 16:54:18 +0530

    source

--- src/services/fcl_freight_rate/workers/create_sailing_schedule_port_pair_coverage.py
+++ src/services/fcl_freight_rate/workers/create_sailing_schedule_port_pair_coverage.py
@@ -5,18 +5,24 @@ from micro_services.client import schedule_client
 
 def create_sailing_schedules_port_pair_coverages(request):
 
-    origin_port = request.origin_port
-    destination_port = request.destination_port
+    is_nvocc = request.get('shipping_line', {}).get('is_nvocc', False)
+    if is_nvocc:
+        return 
+    
+    origin_port = request['origin_port']
+    destination_port = request['destination_port']
 
     origin_port_type = 'icd_port' if origin_port.get('is_icd') else 'main_port'
     destination_port_type = 'icd_port' if destination_port.get('is_icd') else 'main_port'
     port_pair_type = f'{origin_port_type}:{destination_port_type}'
 
     data = {
-        "origin_port_id": str(request.origin_port_id),
-        "destination_port_id": str(request.destination_port_id),
-        "shipping_line_id": str(request.shipping_line_id),
-        "port_pair_type": str(port_pair_type)
+        "origin_port_id": str(request['origin_port_id']),
+        "destination_port_id": str(request['destination_port_id']),
+        "shipping_line_id": str(request['shipping_line_id']),
+        "port_pair_type": str(port_pair_type),
+        "is_verified": True,
+        "source": "chakravyuh",
     }
 
     return schedule_client.create_sailing_schedule_port_pair_coverage(data)
