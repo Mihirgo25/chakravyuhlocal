069a9c300275cf6b7bcffd636c9868a31141f950
prashik1845 <prashik.raut@cogoport.com>
2023-11-20 17:04:48 +0530

    minor fixes

--- src/services/fcl_freight_rate/interaction/get_fcl_freight_rate_cards.py
+++ src/services/fcl_freight_rate/interaction/get_fcl_freight_rate_cards.py
@@ -756,8 +756,8 @@ def filter_default_service_provider(freight_rates, are_all_rates_predicted, is_p
                 if val['service_provider_id'] == DEFAULT_SERVICE_PROVIDER_ID:
                     cogofreight_freight_rates_length += 1
             
-            if cogofreight_freight_rates_length != 0 and cogofreight_freight_rates_length != new_freight_rates_length:
-                freight_rates = list(filter(lambda item: item['service_provider_id'] != DEFAULT_SERVICE_PROVIDER_ID, freight_rates))
+            # if cogofreight_freight_rates_length != 0 and cogofreight_freight_rates_length != new_freight_rates_length:
+            #     freight_rates = list(filter(lambda item: item['service_provider_id'] != DEFAULT_SERVICE_PROVIDER_ID, freight_rates))
         else:
                 is_predicted = True
                 
@@ -966,8 +966,15 @@ def get_freight_rates(supply_rates, requirements, serviceable_shipping_lines):
         freight_rates = list(filter(lambda item: item['service_provider_id'] != DEFAULT_SERVICE_PROVIDER_ID, freight_rates))
         return (freight_rates, is_predicted)
 
-    are_all_rates_predicted, available_shipping_lines= all_rates_predicted(freight_rates)
-    if len(freight_rates) == 0 or are_all_rates_predicted:
+    are_all_rates_predicted, available_shipping_lines = all_rates_predicted(freight_rates)
+    
+    temp_serviceable_shipping_lines = serviceable_shipping_lines[0]['shipping_lines']
+
+    temp_serviceable_shipping_lines = [line for line in temp_serviceable_shipping_lines if line not in available_shipping_lines]
+    
+    serviceable_shipping_lines[0]['shipping_lines'] = temp_serviceable_shipping_lines
+    
+    if len(temp_serviceable_shipping_lines) > 0:
         freight_rates, is_predicted = get_cluster_or_predicted_rates(freight_rates, requirements, is_predicted, serviceable_shipping_lines, available_shipping_lines)
         
     freight_rates, is_predicted = filter_default_service_provider(freight_rates, are_all_rates_predicted, is_predicted)
