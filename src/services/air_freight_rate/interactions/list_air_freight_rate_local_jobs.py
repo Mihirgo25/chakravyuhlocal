50c196f21eef517b96d89d2f1a9ad4efcfa2cb34
HarshitCogo22 <harshit.mittal@cogoport.com>
2023-11-22 12:51:58 +0530

    fix

--- src/services/air_freight_rate/interactions/list_air_freight_rate_local_jobs.py
+++ src/services/air_freight_rate/interactions/list_air_freight_rate_local_jobs.py
@@ -202,7 +202,7 @@ def apply_filters(query, filters):
     # applying indirect filters
     query = apply_indirect_filters(query, indirect_filters)
     
-    query = apply_is_visible_filter(query)
+    # query = apply_is_visible_filter(query)
 
     return query
 
