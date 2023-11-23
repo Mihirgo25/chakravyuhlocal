6b2f70034a70de47993ca2d86bb777d6cc28e06d
telkargulshan <gulshan.telkar@cogoport.com>
2023-11-23 11:19:28 +0530

    fix

--- src/services/fcl_freight_rate/interaction/update_schedule_in_fcl_freight_rate.py
+++ src/services/fcl_freight_rate/interaction/update_schedule_in_fcl_freight_rate.py
@@ -26,28 +26,36 @@ def update_schedule_in_fcl_freight_rate(request):
 def execute_transaction_code(request):
     rate_id = request["rate_id"]
     validity_id = request["validity_id"]
-    schedule_id = request["schedule_id"]
+    schedule_id = request.get("schedule_id")
     sourced_by_id = request["performed_by_id"]
     schedule_type = request["schedule_type"]
-
+    
+    deleted=True
+    if schedule_id:
+        deleted=False
+        
     freight = FclFreightRate.select().where(FclFreightRate.id == rate_id).first()
 
     if not freight:
         raise HTTPException(status=404, detail=f"No freight found with id: {rate_id}")
 
     validities = freight.validities
-    new_validities = []
-    validity_found = False
+
+    validity_object_found = None
+
     for validity_object in validities:
         if validity_object["id"] == validity_id:
-            validity_found = True
-            validity_object["schedule_id"] = schedule_id
-            validity_object["schedule_type"] = schedule_type
+            validity_object_found = validity_object
+            break
+            
 
-        new_validities.append(validity_object)
-
-    if validity_found:
-        freight.validities = new_validities
+    if validity_object_found:
+        validity_start=validity_object['validity_start']
+        validity_end=validity_object['validity_end']
+        line_items=validity_object['line_items']
+        schedule_type=schedule_type
+        payment_term=validity_object['payment_term']
+        freight.set_validities(validity_start,validity_end,line_items,schedule_type,deleted,payment_term,{},None,schedule_id)
         freight.sourced_by_id = sourced_by_id
         freight.save()
 
