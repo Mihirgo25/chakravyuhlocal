c118dc912563aff17e054ef7fc099958b5fda720
telkargulshan <gulshan.telkar@cogoport.com>
2023-11-23 16:04:59 +0530

    revert

--- src/services/fcl_freight_rate/interaction/get_fcl_freight_rates_from_clusters.py
+++ src/services/fcl_freight_rate/interaction/get_fcl_freight_rates_from_clusters.py
@@ -21,6 +21,59 @@ from configs.global_constants import CHINA_COUNTRY_ID, INDIA_COUNTRY_ID
 from micro_services.client import common
 
 
+def get_shipping_line_mapping(critical_freight_rates):
+    """Generate a mapping of shipping line IDs to their average prices.
+
+    Args:
+        critical_freight_rates
+
+    Returns:
+        dict: Mapping of shipping line IDs to their average prices.
+    """
+    shipping_line_mapping = {}
+    shipping_line_data = {}
+
+    for rate in critical_freight_rates:
+        validities = rate["validities"]
+        total_price = 0.0
+        count = 0
+        for validity in validities:
+            line_items = validity.get("line_items", [])
+            for line_item in line_items:
+                if line_item["code"] == "BAS":
+                    price = line_item.get("price", 0.0)
+                    if line_item["currency"] != "USD":
+                        price = common.get_money_exchange_for_fcl(
+                            dict(
+                                from_currency=line_item["currency"],
+                                to_currency="USD",
+                                price=price,
+                            )
+                        ).get("price", 0.0)
+                    total_price += float(price)
+                    count += 1
+
+        if count > 0:
+            shipping_line_id = rate["shipping_line_id"]
+
+            if shipping_line_id in shipping_line_data:
+                shipping_line_data[shipping_line_id]["total_price"] += total_price
+                shipping_line_data[shipping_line_id]["count"] += count
+            else:
+                shipping_line_data[shipping_line_id] = {
+                    "total_price": total_price,
+                    "count": count,
+                }
+
+    shipping_line_mapping = {}
+    for shipping_line_id, data in shipping_line_data.items():
+        total_price = data["total_price"]
+        count = data["count"]
+        avg_price = float(total_price / count)
+        shipping_line_mapping[shipping_line_id] = {"sl_avg": avg_price}
+
+    return shipping_line_mapping
+
 
 def get_current_median(
     request,
